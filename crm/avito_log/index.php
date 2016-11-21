92aa28e32d0bd82f3c53609d155131bf55859793
Andrew Abalakov <a.s.abalakov@ucre.ru>
2016-11-21 11:03:01 +0500

    Добовлена загрузка самого лога, загрузка данных по объвлениям и количественная информация об автозагрузке

--- crm/avito_log/index.php
+++ crm/avito_log/index.php
@@ -44,10 +44,11 @@ if ($USER::GetID() == 24) {
       $DB->PrepareFields("ucre_avito_log");
       $_params = $xpath->query("/html/body/div[@class='width']/div[@class='form-section form-section_blue']/fieldset[@class='form-fieldset is-readonly']");
       $arFields = array(//Наполняем поля данными
-        'UF_AVITO_ID' =>  "'".$avitoid."'",
-        'UF_STATUS'   =>  "'".utf8_decode($_params->item(0)->childNodes->item(2)->nodeValue)."'",
-        'UF_LINK'     =>  "'http://avito.ru".$_params->item(1)->childNodes->item(2)->childNodes->item(1)->getAttributeNode("href")->nodeValue."'",
-        'UF_TIME'     =>  $DB->CharToDateFunction($_params->item(2)->childNodes->item(2)->nodeValue)
+        'UF_AVITO_ID' =>  "'".$avitoid."'", //ID Лога на Авито
+        'UF_STATUS'   =>  "'".utf8_decode($_params->item(0)->childNodes->item(2)->nodeValue)."'", //Общий статус загрузки
+        'UF_LINK'     =>  "'http://avito.ru".$_params->item(1)->childNodes->item(2)->childNodes->item(1)->getAttributeNode("href")->nodeValue."'", //Ссылка xml фмд загриузки
+        'UF_LOG_LINK' =>  "'".$_POST["log_link"]."'",
+        'UF_TIME'     =>  $DB->CharToDateFunction($_params->item(2)->childNodes->item(2)->nodeValue)  //Время обработки фида
       );
       $DB->StartTransaction();
       if ($ID > 0) {
@@ -59,6 +60,74 @@ if ($USER::GetID() == 24) {
       if (strlen($strError)<=0) {
         $DB->Commit();
       } else $DB->Rollback();
+      
+      $_res = $xpath->query("/html/body/div[@class='width']/table[@class='table table__items']/tbody/tr");
+      $uf_processed = 0;
+      $uf_success = 0;
+      $uf_problems = 0;
+      $uf_errors = 0;
+      $uf_deleted = 0;
+      foreach ($_res as $row){
+        $children = $row->childNodes;
+        if ($children->length == 10){
+          $arElementFields = array(
+            'UF_AVITO_LOG_ID' =>  "'".$avitoid."'",
+            'UF_CRM_ID'       =>  "'".trim($children->item(2)->childNodes->item(1)->nodeValue)."'",
+            'UF_AVITO_LINK'   =>  "'".$children->item(2)->childNodes->item(3)->nodeValue."'",
+            'UF_STATUS'       =>  "'".utf8_decode($children->item(4)->childNodes->item(1)->nodeValue)."'",
+            'UF_STATUS_MORE'  =>  "'".utf8_decode($children->item(4)->childNodes->item(4)->nodeValue)."'",
+            'UF_TILL'         =>  $DB->CharToDateFunction($children->item(6)->childNodes->item(1)->nodeValue),
+            'UF_MESSAGE'      =>  "'".utf8_decode($children->item(8)->nodeValue)."'"
+          );
+        }
+        if ($children->length == 8){
+          $arElementFields = array(
+            'UF_AVITO_LOG_ID' =>  "'".$avitoid."'",
+            'UF_CRM_ID'       =>  "'".trim($children->item(2)->childNodes->item(1)->nodeValue)."'",
+            'UF_AVITO_LINK'   =>  "'".$children->item(2)->childNodes->item(3)->nodeValue."'",
+            'UF_STATUS'       =>  "'".utf8_decode($children->item(4)->childNodes->item(1)->nodeValue)."'",
+            'UF_STATUS_MORE'  =>  "'".utf8_decode($children->item(4)->childNodes->item(4)->nodeValue)."'",
+            'UF_TILL'         =>  "",
+            'UF_MESSAGE'      =>  "'".utf8_decode($children->item(6)->nodeValue)."'"
+          );
+        }
+        $query = 'SELECT * FROM ucre_avito_log_element WHERE UF_AVITO_LOG_ID="'.$avitoid.'" AND UF_CRM_ID="'.trim($children->item(2)->childNodes->item(1)->nodeValue).'"';
+        $rsData = $DB->Query($query);
+        if ($avitologelement = $rsData->Fetch()){ //Если запись с таким AVITO_LOG_ID есть
+          $ID_EL = $avitologelement['ID']; // то запоминаем id этой записи, будем обновлять даннеы в ней
+        } else {
+          $ID_EL = 0;// в противном случае будем добавлять новую запись
+        }
+        $DB->StartTransaction();
+        if ($ID_EL > 0) {
+          $DB->Update("ucre_avito_log_element", $arElementFields, "WHERE ID='".$ID_EL."'", $err_mess.__LINE__);
+        } else {
+          $ID = $DB->Insert("ucre_avito_log_element", $arElementFields, $err_mess.__LINE__);
+        }
+        $ID_EL = intval($ID_EL);
+        if (strlen($strError)<=0) {
+          $DB->Commit();
+        } else $DB->Rollback();
+        
+        if (utf8_decode($children->item(4)->childNodes->item(1)->nodeValue)=="Успешно опубликовано") $uf_success++;
+        if (utf8_decode($children->item(4)->childNodes->item(1)->nodeValue)=="Опубликовано с проблемами") $uf_problems++;
+        if (utf8_decode($children->item(4)->childNodes->item(1)->nodeValue)=="Не удалось опубликовать") $uf_errors++;
+        if (utf8_decode($children->item(4)->childNodes->item(1)->nodeValue)=="Удалены из файла") $uf_deleted++;
+        $uf_processed++;
+      }
+      $arFields = array(
+        'UF_PROCESSED'  =>  $uf_processed,
+        'UF_SUCCESS'    =>  $uf_success,
+        'UF_PROBLEMS'   =>  $uf_problems,
+        'UF_ERRORS'     =>  $uf_errors,
+        'UF_DELETED'    =>  $uf_deleted
+      );
+      $DB->StartTransaction();
+      $DB->Update("ucre_avito_log", $arFields, "WHERE ID='".$ID."'", $err_mess.__LINE__);
+      if (strlen($strError)<=0) {
+        $DB->Commit();
+      } else $DB->Rollback();
+      echo "Всего - ".$uf_processed.", успешно - ".$uf_success.", с проблемами - ".$uf_problems.", ошибки - ".$uf_errors.", удалено - ".$uf_deleted;
     } else {
       $log_message = utf8_decode($_error->item(0)->nodeValue);
     }
