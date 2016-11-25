5c1f2a9571e73717aacb0935f57c505663b78f42
Andrew Abalakov <a.s.abalakov@ucre.ru>
2016-11-25 10:25:02 +0500

    Исправил формат вывода больших чисел: убрал запятую как разделитель целых разрядов

--- pub/ucre_export.php
+++ pub/ucre_export.php
@@ -52,25 +52,25 @@ if(CModule::IncludeModule('iblock') && CModule::IncludeModule("crm")) {
     $tmp_appointment = CIBlockPropertyEnum::GetByID($aRes["PROPERTY_238"]);
     switch ($aRes['PROPERTY_210']){
       case 381:
-        $name = mb_strtoupper(substr($tmp_type["VALUE"],0,1)).substr($tmp_type["VALUE"],1)." ".number_format($aRes['PROPERTY_228'],2)." кв.м., ".$aRes["PROPERTY_217"].", ".$aRes["PROPERTY_218"];
+        $name = mb_strtoupper(substr($tmp_type["VALUE"],0,1)).substr($tmp_type["VALUE"],1)." ".number_format($aRes['PROPERTY_228'],2,".","")." кв.м., ".$aRes["PROPERTY_217"].", ".$aRes["PROPERTY_218"];
         break;
       case 382:
-        $name = intval($aRes["PROPERTY_229"])."-к ".$tmp_type["VALUE"]." ".number_format($aRes['PROPERTY_224'],2)." кв.м., ".$aRes["PROPERTY_217"].", ".$aRes["PROPERTY_218"];
+        $name = intval($aRes["PROPERTY_229"])."-к ".$tmp_type["VALUE"]." ".number_format($aRes['PROPERTY_224'],2,".","")." кв.м., ".$aRes["PROPERTY_217"].", ".$aRes["PROPERTY_218"];
         break;
       case 383:
-        $name = intval($aRes["PROPERTY_229"])."-к ".$tmp_type["VALUE"]." ".number_format($aRes['PROPERTY_224'],2)." кв.м., ".$aRes["PROPERTY_217"];
+        $name = intval($aRes["PROPERTY_229"])."-к ".$tmp_type["VALUE"]." ".number_format($aRes['PROPERTY_224'],2,".","")." кв.м., ".$aRes["PROPERTY_217"];
         break;
       case 384:
-        $name = intval($aRes["PROPERTY_229"])."-к ".$tmp_type["VALUE"]." ".number_format($aRes['PROPERTY_224'],2)." кв.м., ".$aRes["PROPERTY_217"];
+        $name = intval($aRes["PROPERTY_229"])."-к ".$tmp_type["VALUE"]." ".number_format($aRes['PROPERTY_224'],2,".","")." кв.м., ".$aRes["PROPERTY_217"];
         break;
       case 385:
-        $name = mb_strtoupper(substr($tmp_type["VALUE"],0,1)).substr($tmp_type["VALUE"],1)." ".number_format($aRes['PROPERTY_292'],2)." сот., ".$aRes["PROPERTY_217"];
+        $name = mb_strtoupper(substr($tmp_type["VALUE"],0,1)).substr($tmp_type["VALUE"],1)." ".number_format($aRes['PROPERTY_292'],2,".","")." сот., ".$aRes["PROPERTY_217"];
         break;
       case 386:
-        $name = mb_strtoupper(substr($tmp_type["VALUE"],0,1)).substr($tmp_type["VALUE"],1)." ".number_format($aRes['PROPERTY_292'],2)." сот., ".$aRes["PROPERTY_217"];
+        $name = mb_strtoupper(substr($tmp_type["VALUE"],0,1)).substr($tmp_type["VALUE"],1)." ".number_format($aRes['PROPERTY_292'],2,".","")." сот., ".$aRes["PROPERTY_217"];
         break;
       case 387:
-        $name = mb_strtoupper(substr($tmp_appointment["VALUE"],0,1)).substr($tmp_appointment["VALUE"],1)." ".number_format($aRes['PROPERTY_224'],2)." кв.м., ".$aRes["PROPERTY_217"].", ".$aRes["PROPERTY_218"];
+        $name = mb_strtoupper(substr($tmp_appointment["VALUE"],0,1)).substr($tmp_appointment["VALUE"],1)." ".number_format($aRes['PROPERTY_224'],2,".","")." кв.м., ".$aRes["PROPERTY_217"].", ".$aRes["PROPERTY_218"];
         break;
     }
     $json_ro[] = array('ID'               => $aRes['ID'],
@@ -84,16 +84,16 @@ if(CModule::IncludeModule('iblock') && CModule::IncludeModule("crm")) {
                        'ADDRESS'          => ($aRes['TYPE']==381 || $aRes['PROPERTY_210']==382)? $aRes['PROPERTY_217'].", ".$aRes['PROPERTY_218']:$aRes['PROPERTY_217'],
                        'LATITUDE'				  => $aRes['PROPERTY_298'],
                        'LONGITUDE'			  => $aRes['PROPERTY_299'],
-                       'SQUARE'           => number_format($aRes['PROPERTY_224'],2),
-                       'LIVING'					  => number_format($aRes['PROPERTY_225'],2),
-                       'KITCHEN'				  => number_format($aRes['PROPERTY_226'],2),
+                       'SQUARE'           => number_format($aRes['PROPERTY_224'],2,".",""),
+                       'LIVING'					  => number_format($aRes['PROPERTY_225'],2,".",""),
+                       'KITCHEN'				  => number_format($aRes['PROPERTY_226'],2,".",""),
                        'ROOMS'            => intval($aRes['PROPERTY_229']),
                        'FLOOR'            => $aRes['PROPERTY_221'],
                        'FLOORS'           => $aRes['PROPERTY_222'],
                        'TYPE_HOUSE'       => $housetype[$aRes['PROPERTY_243']],
                        'TYPE'             => $aRes['PROPERTY_300'],
                        'APPOINTMENT'      => $appointment[$aRes['PROPERTY_238']],
-                       'PLOT'             => number_format($aRes['PROPERTY_292'],2),
+                       'PLOT'             => number_format($aRes['PROPERTY_292'],2,".",""),
                        'ASSIGNED_BY'      => $aRes['PROPERTY_313'],
                        'PRICE'            => $deal['UF_CRM_579897C010103'],//Цену берем из заявки
                        'DESCRIPTION'      => $deal['COMMENTS']." Номер в базе: ".$aRes['ID']/*$aRes['DETAIL_TEXT']*/,
