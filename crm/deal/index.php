f978007b3f16688bb5d1bef8d5216500dd52e30b
Andrew Abalakov <a.s.abalakov@ucre.ru>
2017-01-05 13:49:10 +0500

    Добавлен js-скрипт для заполнения нештатной вкладки Сделки/Заявки, процедура добавления вкладки и код взяты из статьи Антона Долганинапо ссылке, указанной внутри index.php

--- crm/deal/index.php
+++ crm/deal/index.php
@@ -2,7 +2,50 @@
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 IncludeModuleLangFile($_SERVER["DOCUMENT_ROOT"]."/bitrix/modules/intranet/public/crm/deal/index.php");
 $APPLICATION->SetTitle(GetMessage("CRM_TITLE"));
-?><?$APPLICATION->IncludeComponent(
+?>
+<!--
+	Код взят из блога Антона Долганина
+  http://blog.d-it.ru/crm/add-the-tab-to-the-box-bitrix24-crm/
+-->
+<script type="text/javascript">
+	var arTabLoading = []; 
+	BX.ready(function(){
+		//обработка открытия вкладки 
+		BX.addCustomEvent('BX_CRM_INTERFACE_FORM_TAB_SELECTED', BX.delegate(function(self, name, tab_id){
+			//вкладка остатков поставщиков 
+			if (!arTabLoading[tab_id] && self.oTabsMeta[tab_id].name.toLowerCase().indexOf('авито') !== -1) { 
+				var innerTab = BX('inner_tab_'+tab_id), 
+						dealId = 0, matches = null, 
+						waiter = BX.showWait(innerTab); 
+				if (matches = window.location.href.match(/\/crm\/deal\/show\/([\d]+)\//i)) { 
+					var dealId = parseInt(matches[1]); 
+				} 
+				if (dealId > 0) { 
+					//чтобы не грузить повторно 
+					arTabLoading[tab_id] = true; 
+					BX.ajax({ 
+						url: '/ajax/upload_avito_log.php', 
+						method: 'POST', 
+						dataType: 'html', 
+						data: { 
+							id: dealId 
+						}, 
+						onsuccess: function(data) 
+						{ 
+							innerTab.innerHTML = data; 
+							BX.closeWait(innerTab, waiter); //$this <-> innerTab. в противном случае вызывал ошибку дебагера Битрикс
+						}, 
+						onfailure: function(data) 
+						{ 
+							BX.closeWait(innerTab, waiter); 
+						} 
+					}); 
+				} 
+			}
+		})); 
+	});
+</script>
+<?$APPLICATION->IncludeComponent(
 	"bitrix:crm.deal",
 	"",
 	Array(
