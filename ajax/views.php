06d332df862618a7a82ae8f83c6c62c779df56e5
Andrew Abalakov <a.s.abalakov@ucre.ru>
2017-01-12 16:37:35 +0500

    Добавлен вывод таблицы просмотров (предварительный вариант)

--- ajax/views.php
+++ ajax/views.php
@@ -16,13 +16,43 @@ if (isset($_POST['id'])){
 			}
 			if($aRes = $rsData->GetNext()){
 				$avitId = substr(strrchr($aRes['UF_AVITO_LINK'], "_"), 1);//'https://www.avito.ru/items/stat/860175284?step=0'
-				echo "Ссылка на статистику: https://www.avito.ru/items/stat/".$avitId."?step=".$_POST['step']."<br>";
+				//echo "Ссылка на статистику: https://www.avito.ru/items/stat/".$avitId."?step=".$_POST['step']."<br>";
 				if( $curl = curl_init() ) {
 					curl_setopt($curl, CURLOPT_URL, 'https://www.avito.ru/items/stat/'.$avitId.'?step='.$_POST['step']);
 					curl_setopt($curl, CURLOPT_RETURNTRANSFER,true);
 					$out = curl_exec($curl);
-					echo $out;
+					//echo $out;
 					curl_close($curl);
+					$dom = new DomDocument();
+					$dom->loadHTML($out);
+					$xpath = new DomXPath($dom);
+					$stat = $xpath->query("///div[@class='item-stats__chart js-item-stats-chart']");
+					$all = $xpath->query("///div[@class='item-stats-legend']");
+					$startdate = $xpath->query("///div[@class='item-stats__date']");
+					$stat_array = json_decode(utf8_decode($stat->item(0)->getAttribute("data-chart")));
+					$seens = (array) $stat_array;
+					$stat_table = array(array(),array());
+					
+					foreach ($seens['columns'][1] as $value){
+						$stat_table[1][] = $value;
+					}
+					foreach ($seens['dates'] as $value){
+						$stat_table[0][] = $value;
+					}
+					$stat_table[0][0] = "Даты";
+					$stat_table[1][0] = "Просмотры";
+					echo "<br>".utf8_decode($startdate->item(0)->nodeValue)."<br><br>";
+					echo "<table style='width:100%; border: 1px solid black; border-collapse: collapse;'><tr>";
+					foreach($stat_table[0] as $value){
+						echo "<td style='border: 1px solid black;'>".$value."</td>";
+					}
+					echo "</tr><tr>";
+					foreach($stat_table[1] as $value){
+						echo "<td style='border: 1px solid black;'>".$value."</td>";
+					}
+					echo "</tr></table>";
+
+					echo "<br>Всего: ".utf8_decode($all->item(0)->nodeValue);
 				}
 			}else{
 				echo "<br><h2>Выгрузка объекта ".$tempFields['UF_CRM_1469534140']." не производилась!</h2>";
@@ -33,7 +63,7 @@ if (isset($_POST['id'])){
 	}
 	
 }
-print_r($_POST);
+//print_r($_POST);
 ?>
 <form width="100%" style="background-color: #f4f0d2" method="POST" id="formx" action="javascript:void(null);">
 	<input type="hidden" name="id" value="<?=$_POST['id']?>">
