d3f53d167a13cee0905360723a3e77dee25f91e3
Andrew Abalakov <a.s.abalakov@ucre.ru>
2017-01-18 16:56:06 +0500

    Сделал вывод в блок типа недвижки, кол-во комнат, площади для квартиры/дома и площадь уч-ка

--- bitrix/components/bitrix/crm.deal.show/templates/.default/template.php
+++ bitrix/components/bitrix/crm.deal.show/templates/.default/template.php
@@ -208,26 +208,51 @@ $rsDeal = CCrmDeal::GetListEx(
 $mainDeal = $rsDeal->Fetch();
 if ($mainDeal["CATEGORY_ID"] == 0 || $mainDeal["CATEGORY_ID"] == 4){
 	if ($mainDeal["UF_CRM_1469534140"]){
-		if ($USER->GetID() == 24){
+		$rsObject = CIBlockElement::GetById($mainDeal["UF_CRM_1469534140"]);
+		$mainObject = $rsObject->GetNextElement();
+		$objectFields = $mainObject->GetFields();
+		$objectFields['PROPERTIES'] = $mainObject->GetProperties();
 ?>
 <table class="crm-offer-info-table crm-offer-main-info-text">
 	<tbody>
-		<tr><td colspan="5"><div class="crm-offer-title">Об объекте</div></td></tr>
+		<tr><td colspan="5"><div class="crm-offer-title">Характеристики объекта по адресу: <?=$objectFields['PROPERTIES']['ADDRESS']['VALUE']?></div></td></tr>
 		<tr class="crm-offer-row">
 			<td class="crm-offer-info-drg-btn"></td>
 			<td class="crm-offer-info-left">
-				<div class="crm-offer-info-label-wrap">Метка:</div>
+				<div class="crm-offer-info-label-wrap"><span class="crm-offer-info-label">Тип недвижимости:</span></div>
 			</td>
-			<td class="crm-offer-info-right">Значение</td>
+			<td class="crm-offer-info-right">
+				<div class="crm-offer-info-label-wrap"><span class="crm-offer-info-label"><?=$objectFields['PROPERTIES']['TYPE']['VALUE']?></span></div>
+			</td>
 			<td class="crm-offer-info-left">
-				<div class="crm-offer-info-label-wrap">Метка:</div>
+				<div class="crm-offer-info-label-wrap"><span class="crm-offer-info-label">Кол-во комнат:</span></div>
 			</td>
-			<td class="crm-offer-info-right">Значение</td>
+			<td class="crm-offer-info-right">
+				<div class="crm-offer-info-label-wrap"><span class="crm-offer-info-label"><?=($objectFields['PROPERTIES']['ROOMS']['VALUE'])?$objectFields['PROPERTIES']['ROOMS']['VALUE']:"нет данных"?></span></div>
+			</td>
+		</tr>
+		<tr class="crm-offer-row">
+			<td class="crm-offer-info-drg-btn"></td>
+			<td class="crm-offer-info-left">
+				<div class="crm-offer-info-label-wrap"><span class="crm-offer-info-label">Площади (общ./жил./кух.):</span></div>
+			</td>
+			<td class="crm-offer-info-right">
+				<div class="crm-offer-info-label-wrap">
+					<span class="crm-offer-info-label">
+						<?=($objectFields['PROPERTIES']['TOTAL_AREA']['VALUE'])?$objectFields['PROPERTIES']['TOTAL_AREA']['VALUE']:"нет данных"?>/<?=($objectFields['PROPERTIES']['LIVE_AREA']['VALUE'])?$objectFields['PROPERTIES']['LIVE_AREA']['VALUE']:"нет данных"?>/<?=($objectFields['PROPERTIES']['KITCHEN_AREA']['VALUE'])?$objectFields['PROPERTIES']['KITCHEN_AREA']['VALUE']:"нет данных"?>
+					</span>
+				</div>
+			</td>
+			<td class="crm-offer-info-left">
+				<div class="crm-offer-info-label-wrap"><span class="crm-offer-info-label">Площадь участка:</span></div>
+			</td>
+			<td class="crm-offer-info-right">
+				<div class="crm-offer-info-label-wrap"><span class="crm-offer-info-label"><?=($objectFields['PROPERTIES']['PLOT_AREA']['VALUE'])?$objectFields['PROPERTIES']['PLOT_AREA']['VALUE']:"нет данных"?></span></div>
+			</td>
 		</tr>
 	</tbody>
 </table>
 <?
-}
 	} else {
 ?>
 <table class="crm-offer-info-table crm-offer-main-info-text">
