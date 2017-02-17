c0d9fd2f437fdcd3693e8f24a224bdd99f0daf18
Andrew Abalakov <a.s.abalakov@ucre.ru>
2017-02-17 10:06:28 +0500

    Исправление косяков обновления Битрикс

--- bitrix/components/bitrix/crm.deal.show/templates/.default/template.php
+++ bitrix/components/bitrix/crm.deal.show/templates/.default/template.php
@@ -24,7 +24,7 @@ if(CCrmActivity::GetDefaultStorageTypeID() === StorageType::Disk)
 	CJSCore::Init(array('uploader', 'file_dialog'));
 }
 
-$arResult['CRM_CUSTOM_PAGE_TITLE'] = GetMessage(
+$arResult['CRM_CUSTOM_PAGE_TITLE'] = DealCategory::getName($arResult['CATEGORY_ID']).": ".GetMessage(
 	'CRM_DEAL_SHOW_TITLE',
 	array(
 		'#ID#' => $arResult['ELEMENT']['ID'],
