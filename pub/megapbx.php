75ee9392bfa9b09ca7d470272e512029f12608bd
Andrew Abalakov <a.s.abalakov@ucre.ru>
2017-03-15 14:21:36 +0500

    Основной скрипт интеграции с ВАТС МЕГАФОН (прием запросов)

--- pub/megapbx.php
+++ pub/megapbx.php
@@ -2,8 +2,9 @@
 require ($_SERVER['DOCUMENT_ROOT'] . "/bitrix/modules/main/include/prolog_before.php");
 $megapbx = json_decode(file_get_contents($_SERVER['DOCUMENT_ROOT']."/../megapbx_params"));
 $postdata = file_get_contents("php://input");
-
-if ($postdata!=''){
+if ($_POST['crm_token'] == $megapbx->crm_key){
+  echo "Yes";
+  $DB->Query("INSERT INTO b_megapbx_log VALUES ('', NOW(),'".serialize($_POST)."')");
   $megapbx_log = fopen('megapbx.log', 'a');
   fwrite( $megapbx_log, $postdata ."\r\n");
   fclose( $megapbx_log );
