923ceb5a4fae88ad9013492fbe510f604f6a3ca2
Andrew Abalakov <a.s.abalakov@ucre.ru>
2017-03-17 15:47:30 +0500

    Добавлен вывод краткой всплывающей карточки на входящий звонок. Добавлена фукнция поиска сотрудника по короткому внутреннму номеру, на выходе false или id сотрудника, для определения кому показывать картучку, накого заводить звонок и сущности CRM

--- pub/megapbx.php
+++ pub/megapbx.php
@@ -10,6 +10,14 @@ function isEmployee ($phone){
     return $arUser['ID'];
   }
 }
+function getUserByExt($ext){
+  $rsUsers = CUser::GetList(($by="ID"), ($order="desc"), array('UF_PHONE_INNER' => $ext));
+  if ($arUser = $rsUsers->Fetch()){
+    return $arUser['ID'];
+  }else{
+    return false;
+  }
+}
 require ($_SERVER['DOCUMENT_ROOT'] . "/bitrix/modules/main/include/prolog_before.php");
 include('../mts/connector/functions.php');
 CModule::IncludeModule('pull');
@@ -18,10 +26,37 @@ $megapbx = json_decode(file_get_contents($_SERVER['DOCUMENT_ROOT']."/../megapbx_
 if ($_POST['crm_token'] == $megapbx->crm_key){
   $DB->Query("INSERT INTO b_megapbx_mess VALUES ('', NOW(),'".$_POST['callid']."','".$_POST['cmd']."','".$_POST['phone']."','".$_POST['type']."','".$_POST['user']."','".$_POST['ext']."','".$_POST['telnum']."','".$_POST['diversion']."','".$_POST['duration']."','".$_POST['link']."','".$_POST['status']."')");
   if ($_POST['cmd'] == 'event'){
-    echo "";
+    $user = getUserByExt($_POST['ext']);
+    if ($_POST['type'] == 'INCOMING'){
+      CPullStack::AddByUser(
+        $user,
+        array(
+          'module_id' => 'voximplant',
+          'command' => 'showExternalCall',
+          'params' => array('callId' => $_POST['callid'], 'toUserId' => $user,'phoneNumber' => $_POST['phone'], 'crm' => ''), 
+          'push' => ''
+        )
+      );
+    }
+    if ($_POST['type'] == 'ACCEPTED'){
+      
+    }
+
+    if ($_POST['type'] == 'COMPLETED' || $_POST['type'] == 'CANCELLED'){
+      CPullStack::AddByUser(
+        $user,
+        array(
+          'module_id' => 'voximplant',
+          'command' => 'hideExternalCall',
+          'params' => array('callId' => $_POST['callid']),
+          'push' => ''
+        )
+      );
+    }
   }
   if ($_POST['cmd'] == 'history'){
-    echo "";
+    if ($_POST['type'] == 'in'){}
+    if ($_POST['type'] == 'out'){}
   }
 }else {
   echo "<center><img style='margin: 0 auto;' src='images/away.jpg'></center>";
