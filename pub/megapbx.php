9784b700ee55d049ff2bf3db260ddabbc9e574d4
Andrew Abalakov <a.s.abalakov@ucre.ru>
2017-03-22 16:05:58 +0500

    Правка косяков

--- pub/megapbx.php
+++ pub/megapbx.php
@@ -1,4 +1,10 @@
 <?php
+require ($_SERVER['DOCUMENT_ROOT'] . "/bitrix/modules/main/include/prolog_before.php");
+include('../mts/connector/functions.php');
+CModule::IncludeModule('pull');
+CModule::IncludeModule('voximplant');
+CModule::IncludeModule("im");
+CModule::IncludeModule("timeman");
 function isEmployee ($phone){
   global $DB;
   $temp_phone = substr($phone,1);
@@ -11,11 +17,19 @@ function isEmployee ($phone){
   }
 }
 function getUserByExt($ext){
-  $rsUsers = CUser::GetList(($by="ID"), ($order="desc"), array('UF_PHONE_INNER' => $ext));
-  if ($arUser = $rsUsers->Fetch()){
+  $rsUsers = CUser::GetList(($by="ID"), ($order="desc"), array('ACTIVE' => 'Y','GROUPS_ID' => array(12), 'UF_MEGAPBX' => $ext));
+  if ($rsUsers->SelectedRowsCount() == 0) return false;
+  if ($rsUsers->SelectedRowsCount() == 1) {
+    $arUser = $rsUsers->Fetch();
     return $arUser['ID'];
-  }else{
-    return false;
+  }
+  if ($rsUsers->SelectedRowsCount() > 1) {
+    $results = array();
+    while ($arUser = $rsUsers->Fetch()){
+      $TimemanUser = new CTimeManUser($arUser['ID']);
+      if ($TimemanUser->State()=='OPEN') $results[] = $arUser['ID'];
+    }
+    return $results;
   }
 }
 function getExtByOperName($name, $pbx_params){
@@ -33,17 +47,14 @@ function getExtByOperName($name, $pbx_params){
     return false;
   }
 }
-require ($_SERVER['DOCUMENT_ROOT'] . "/bitrix/modules/main/include/prolog_before.php");
-include('../mts/connector/functions.php');
-CModule::IncludeModule('pull');
-CModule::IncludeModule('voximplant');
+
 $megapbx = json_decode(file_get_contents($_SERVER['DOCUMENT_ROOT']."/../megapbx_params"));
 if ($_POST['crm_token'] == $megapbx->crm_key){
   $DB->Query("INSERT INTO b_megapbx_mess VALUES ('', NOW(),'".trim($_POST['callid'])."','".trim($_POST['cmd'])."','".trim($_POST['phone'])."','".trim($_POST['type'])."','".trim($_POST['user'])."','".trim($_POST['ext'])."','".trim($_POST['telnum'])."','".trim($_POST['diversion'])."','".trim($_POST['duration'])."','".trim($_POST['link'])."','".trim($_POST['status'])."')");
   if ($_POST['cmd'] == 'event'){
     $user = getUserByExt($_POST['ext']);
     if ($_POST['type'] == 'INCOMING'){
-      CPullStack::AddByUser(//Выводим карточку звонка
+      /*CPullStack::AddByUser(//Выводим карточку звонка
         $user,
         array(
           'module_id' => 'voximplant',
@@ -51,10 +62,10 @@ if ($_POST['crm_token'] == $megapbx->crm_key){
           'params' => array('callId' => $_POST['callid'], 'toUserId' => $user,'phoneNumber' => $_POST['phone'], 'crm' => ''), 
           'push' => ''
         )
-      );
+      );*/
     }
     if ($_POST['type'] == 'ACCEPTED'){
-      CPullStack::AddByUser(//Убираем карточку звонка
+      /*CPullStack::AddByUser(//Убираем карточку звонка
         $user,
         array(
           'module_id' => 'voximplant',
@@ -63,10 +74,25 @@ if ($_POST['crm_token'] == $megapbx->crm_key){
           'push' => ''
         )
       );
+      $crm_data = CVoxImplantCrmHelper::GetDataForPopup($_POST['callid'],$_POST['phone'],$user);
+      CPullStack::AddByUser(//Выводим карточку звонка c данными поиска CRM
+        $user,
+        array(
+          'module_id' => 'voximplant',
+          'command' => 'showExternalCall',
+          'params' => array('callId' => $_POST['callid'], 'toUserId' => $user,'phoneNumber' => $_POST['phone'], 'crm' => $crm_data), 
+          'push' => ''
+        )
+      );*/
     }
     if ($_POST['type'] == 'OUTGOING'){
-      $ext_out = getExtByOperName(trim($_POST['user']), $megapbx);
+      /*$ext_out = getExtByOperName(trim($_POST['user']), $megapbx);
       $user_out = getUserByExt($ext_out);
+      
+       $test_pbx = fopen('test_pbx.log', 'a');
+       fwrite( $test_pbx, serialize(array('user'=>trim($_POST['user']), 'ext' => $ext_out, 'user_id' => $user_out)) ."\r\n");
+       fclose( $test_pbx );
+      
       CPullStack::AddByUser(//Выводим карточку звонка
         $user_out,
         array(
@@ -75,10 +101,10 @@ if ($_POST['crm_token'] == $megapbx->crm_key){
           'params' => array('callId' => $_POST['callid'], 'fromUserId' => $user_out,'phoneNumber' => $_POST['phone'], 'crm' => ''), 
           'push' => ''
         )
-      );
+      );*/
     }
     if ($_POST['type'] == 'COMPLETED' || $_POST['type'] == 'CANCELLED'){
-      CPullStack::AddByUser(//Убираем карточку 
+      /*CPullStack::AddByUser(//Убираем карточку 
         $user,
         array(
           'module_id' => 'voximplant',
@@ -86,13 +112,46 @@ if ($_POST['crm_token'] == $megapbx->crm_key){
           'params' => array('callId' => $_POST['callid']),
           'push' => ''
         )
-      );
+      );*/
     }
   }
   if ($_POST['cmd'] == 'history'){
-    if ($_POST['type'] == 'in'){}
+    if ($_POST['type'] == 'in'){
+      $crm_data = CVoxImplantCrmHelper::GetDataForPopup($_POST['callid'],$_POST['phone']);
+      if ($crm_data['FOUND'] == 'N'){//Нет записей этого телефона в лида/контактах
+        $employee = isEmployee($_POST['phone']);
+        if ($employee){//Служебный телефон сотрудника
+          $rsUser = CUser::GetByID($employee);
+          $arUser = $rsUser->Fetch();
+          //Сообщение сотруднику во избежании повторных звонков на номер автосекретаря
+          $arMessageFields = array(
+            "TO_USER_ID" => $employee, // получатель
+            "FROM_USER_ID" => 0,// отправитель (может быть >0)
+            "NOTIFY_TYPE" => IM_NOTIFY_SYSTEM, // тип уведомления
+            "NOTIFY_MODULE" => "im", // модуль запросивший отправку уведомления
+            "NOTIFY_TAG" => "IM_CONFIG_NOTICE", // символьный тэг для группировки (будет выведено только одно сообщение), если это не требуется - не задаем параметр
+            "NOTIFY_MESSAGE" => '[b]Предупреждение:[/b] '.$arUser['LAST_NAME'].' '.$arUser['NAME'].', вы позвонили на публичный номер компании +'.(($_POST['diversion'])?$_POST['diversion']:$_POST['user']).', что строжайше запрещено, для общения с сотрудниками, необходимо набирать прямой номер!'// текст уведомления на сайте (доступен html и бб-коды)
+          );
+          CIMNotify::Add($arMessageFields);
+          //Сообщение руководителю
+          $arMessageFields = array(
+            "TO_USER_ID" => 1, // получатель
+            "FROM_USER_ID" => 0,// отправитель (может быть >0)
+            "NOTIFY_TYPE" => IM_NOTIFY_SYSTEM, // тип уведомления
+            "NOTIFY_MODULE" => "im", // модуль запросивший отправку уведомления
+            "NOTIFY_TAG" => "IM_CONFIG_NOTICE", // символьный тэг для группировки (будет выведено только одно сообщение), если это не требуется - не задаем параметр
+            "NOTIFY_MESSAGE" => '[b]Сообщение:[/b] Сотрудник: [url=/company/personal/user/'.$employee.'/]'.$arUser['LAST_NAME'].' '.$arUser['NAME'].'[/url], позвонил на публичный номер компании +'.(($_POST['diversion'])?$_POST['diversion']:$_POST['user']).', что строжайше запрещено, для общения с сотрудниками, необходимо набирать прямой номер!'// текст уведомления на сайте (доступен html и бб-коды)
+          );
+          CIMNotify::Add($arMessageFields);
+        }else{
+          if (trim($_POST['status']) == 'Succsess'){}
+          if (trim($_POST['status']) == 'missed'){}
+        }
+      }
+      if ($crm_data['FOUND'] == 'Y'){}
+    }
     if ($_POST['type'] == 'out'){
-      CPullStack::AddByUser(//Убираем карточку 
+      /*CPullStack::AddByUser(//Убираем карточку 
         $user_out,
         array(
           'module_id' => 'voximplant',
@@ -100,7 +159,7 @@ if ($_POST['crm_token'] == $megapbx->crm_key){
           'params' => array('callId' => $_POST['callid']),
           'push' => ''
         )
-      );
+      );*/
 
     }
   }
