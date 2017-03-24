e88c9b8d5c53e10461ab5a63f8438bf2669090b4
Andrew Abalakov <a.s.abalakov@ucre.ru>
2017-03-24 12:03:57 +0500

    Функция для поиск сущности CRM по номеру телефона в формате 79877959999, выдет на выходе массив с элементами [FOUND]=> Y|N, в случае успешного поиска добавляется второйф элемент массива [CONTACT|LEAD|EMPLOYEE] с говорящей структурой полей, думблирует стандартную йункция модуля CRM, но исключает лиды при существующих контактах, убирает закрытые лиды, в случае множественного результата выдает самую свежую сущность

---
+++ include/megapbx/functions.php
@@ -0,0 +1,77 @@
+<?
+CModule::IncludeModule('crm');
+function findByPhoneNumber($number, $params = array()){
+  if (!is_string($number)){
+    throw new \Bitrix\Main\ArgumentTypeException('number', 'string');
+  }
+  if ($number === ''){
+    throw new \Bitrix\Main\ArgumentException('Is empty', 'number');
+  }
+  if (!is_array($params)){
+    $params = array();
+  }
+  $dups = array();
+  $criterion = new \Bitrix\Crm\Integrity\DuplicateCommunicationCriterion('PHONE', $number);
+  $entityTypes = array(CCrmOwnerType::Contact, CCrmOwnerType::Company, CCrmOwnerType::Lead);
+  foreach ($entityTypes as $entityType){
+    $duplicate = $criterion->find($entityType);
+    if ($duplicate !== null){
+      $dups[] = $duplicate;
+    }
+  }
+  $entityByType = array();
+  foreach ($dups as &$dup){
+    /** @var \Bitrix\Crm\Integrity\Duplicate $dup */
+    $entities = $dup->getEntities();
+    if (!(is_array($entities) && !empty($entities))){
+      continue;
+    }
+    //Each entity type limited by 50 items
+    foreach ($entities as &$entity){
+      /** @var \Bitrix\Crm\Integrity\DuplicateEntity $entity */
+      $entityTypeID = $entity->getEntityTypeID();
+      $entityID = $entity->getEntityID();
+      
+      $fields = CCrmSipHelper::getEntityFields($entityTypeID, $entityID, $params);
+      if(!is_array($fields)) continue;
+      $entityTypeName = CCrmOwnerType::ResolveName($entityTypeID);
+      if (!isset($entityByType[$entityTypeName])){
+        $entityByType[$entityTypeName] = array($fields);
+      }elseif (!in_array($entityID, $entityByType[$entityTypeName], true)){
+        $entityByType[$entityTypeName][] = $fields;
+      }
+    }
+  }
+  unset($dup);
+  
+  if (isset($entityByType['CONTACT'])) unset($entityByType['LEAD']);//Если есть контакт, то лиды игнорируем
+  if (isset($entityByType['LEAD'])){//Если есть лид (значит контакта нет)
+    foreach($entityByType['LEAD'] as $key=>$lead){//удаляем инфу о закрытых лидах (способ закрытия неважен)
+      $curLead = CCrmLead::GetByID($lead['ID'],false);
+      if(CCrmLead::IsStatusFinished($curLead['STATUS_ID'])) unset($entityByType['LEAD'][$key]);
+    }
+  }
+  if (count($entityByType)){
+    if (isset($entityByType['CONTACT'])) return array('FOUND' => 'Y', 'CONTACT' => end($entityByType['CONTACT']));//Выдаем в результат полений по ID контакт, если найдено несколько
+    if (isset($entityByType['LEAD'])) return array('FOUND' => 'Y', 'LEAD' => end($entityByType['LEAD']));//Выдаем в результат полений по ID лид, если найдено несколько не закрытых
+  }else {
+    global $DB;
+    $temp_phone = substr($number,1);
+    $result = $DB->Query("SELECT ID,NAME,SECOND_NAME,LAST_NAME,EMAIL FROM b_user WHERE ACTIVE='Y' AND (REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(PERSONAL_PHONE,'+',''),' ',''),'(',''),')',''),'-','') LIKE '%".$temp_phone."' OR REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(PERSONAL_MOBILE,'+',''),' ',''),'(',''),')',''),'-','') LIKE '%".$temp_phone."' OR REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(WORK_PHONE,'+',''),' ',''),'(',''),')',''),'-','') LIKE '%".$temp_phone."');");
+    if ($result->SelectedRowsCount() == 0) {
+      return array ('FOUND' => 'N');
+    }else {
+      $arUser = $result->Fetch();
+      return array('FOUND'    => 'Y', 
+                   'EMPLOYEE' =>  array('ID'          =>  $arUser['ID'],
+                                        'NAME'        =>  $arUser['NAME'],
+                                        'SECOND_NAME' =>  $arUser['SECOND_NAME'],
+                                        'LAST_NAME'   =>  $arUser['LAST_NAME'],
+                                        'EMAIL'       =>  $arUser['EMAIL'],
+                                        'URL'         =>  '/company/personal/user/'.$arUser['ID'].'/'
+                                       )
+                  );
+    }    
+  }
+}
+?>
