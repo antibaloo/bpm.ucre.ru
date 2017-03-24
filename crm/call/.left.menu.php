9f59587616e8c8974bdd2ba175360f251360479d
Andrew Abalakov <a.s.abalakov@ucre.ru>
2017-03-24 13:17:35 +0500

    Добавил внутренне меню раздела

---
+++ crm/call/.left.menu.php
@@ -0,0 +1,16 @@
+<?
+$aMenuLinks = array(
+	array(
+		"Запросы к ВАТС Мегафон", 
+		"index.php", 
+		array(), 
+		array(), 
+	),
+	array(
+		"Эмулятор ВАТС Мегафон", 
+		"emulation.php", 
+		array(), 
+		array(), 
+	)
+);
+?>
