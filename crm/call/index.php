63f29e9c0c3db51a4b100c975e3ff1cdd7187de1
Andrew Abalakov <a.s.abalakov@ucre.ru>
2017-03-27 14:10:06 +0500

    Добавлена команда makeCall от CRM к ВАТС Мегафон, пока на определенный номер

--- crm/call/index.php
+++ crm/call/index.php
@@ -3,7 +3,11 @@ require_once($_SERVER['DOCUMENT_ROOT']."/bitrix/header.php");
 $APPLICATION->SetTitle("Запросы к ВАТС Мегафон");
 $megapbx = json_decode(file_get_contents($_SERVER['DOCUMENT_ROOT']."/../megapbx_params"));
 if (count($_POST)){
-  $postdata = http_build_query(array('cmd' => $_POST['cmd'],'token' => $_POST['token']));
+  if ($_POST['cmd'] == 'accounts'){
+    $postdata = http_build_query(array('cmd' => $_POST['cmd'],'token' => $_POST['token']));
+  } elseif ($_POST['cmd'] == 'makeCall'){
+    $postdata = http_build_query(array('cmd' => $_POST['cmd'],'phone'=>'+79877955786','user'=>'703','token' => $_POST['token']));
+  }
   $opts = array('http' =>array('method'  => 'POST','header'  => 'Content-type: application/x-www-form-urlencoded','content' => $postdata));
   $context  = stream_context_create($opts); 
   $result = file_get_contents($megapbx->pbx_url, false, $context);
@@ -14,6 +18,7 @@ if (count($_POST)){
   cmd
   <select name="cmd">
     <option value="accounts">accounts</option>
+    <option value="makeCall">makeCall</option>
   </select>
   <input name="token" type="hidden" value="<?=$megapbx->pbx_key?>">
   <input type="submit" value="Отправить">
