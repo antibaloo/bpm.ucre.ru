31075d4d943f7b44088a03647d37c6eba2b7a059
Andrew Abalakov <a.s.abalakov@ucre.ru>
2017-03-30 14:57:57 +0500

    Исправлена страшная ошибкаgit add pub/ro.phpgit add pub/ro.php Вместо CIBlockElemen::Update на до использовать CIBlockElement::SetPropertyValuesEx иначе перезатираются все пользовательские поля инфоблока, кроме введенных

--- pub/ro.php
+++ pub/ro.php
@@ -1,13 +1,7 @@
 <?
 require ($_SERVER['DOCUMENT_ROOT'] . "/bitrix/modules/main/include/prolog_before.php");
-if (isset($_GET['id']) && $_GET['id']>0 && isset($_GET['link']) && !empty($_GET['link'])){
-  $arFields['PROPERTY_VALUES'][301] = $_GET['link'];;
-  $object = new CIBlockElement;
-  if ($object->Update($_GET['id'], $arFields)){
-    echo "Updated!";
-  } else {
-    $object->LAST_ERROR;
-  }
+if (isset($_GET['id']) && $_GET['id']>0 && isset($_GET['link']) && !empty($_GET['link'])){  
+  CIBlockElement::SetPropertyValuesEx($_GET['id'], false, array('LINK' => $_GET['link']));
 }else {
   echo "<center><img style='margin: 0 auto;' src='images/away.jpg'></center>";
 }
