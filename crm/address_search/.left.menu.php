39da6859e595544f8a456b172963de20d5fa9f3c
Andrew Abalakov <a.s.abalakov@ucre.ru>
2017-04-07 12:37:15 +0500

    Меню для дополнительных инструментов поиска адреса

---
+++ crm/address_search/.left.menu.php
@@ -0,0 +1,22 @@
+<?
+$aMenuLinks = array(
+	array(
+		"Поиск адреса по КЛАДР", 
+		"index.php", 
+		array(), 
+		array(), 
+	),
+	array(
+		"Адрес по карте", 
+		"address_click.php", 
+		array(), 
+		array(), 
+	),
+	array(
+		"Автозаполнение формы адреса (КЛАДР)", 
+		"autocomplete.php", 
+		array(), 
+		array(), 
+	)
+);
+?>
