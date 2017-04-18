ed1993bd12bc69cc49646087edf0d96295d4c440
Andrew Abalakov <a.s.abalakov@ucre.ru>
2017-04-18 16:26:42 +0500

    Компонент для приема сообщений от обработчика запросов ВАТС Мегафон

---
+++ bitrix/components/ucre/pull.megapbx/component.php
@@ -0,0 +1,13 @@
+<?
+if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();
+$currentUserId = $USER->GetId();
+CPullWatch::Add($currentUserId, 'PULL_MEGAPBX');
+?>
+<script type="text/javascript">
+BX.ready(function(){
+	BX.addCustomEvent("onPullEvent", function(module_id,command,time, params) {
+    if (module_id == "megapbx") 	console.log(module_id,command,time,params);
+	});
+	BX.PULL.extendWatch('PULL_MEGAPBX');
+});
+</script>
