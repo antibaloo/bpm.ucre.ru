06bbb67fcdfb82b857c87c1791f78b5d8a563026
Andrew Abalakov <a.s.abalakov@ucre.ru>
2017-04-18 16:24:45 +0500

    Тестовая страница с компонентом дял приема сообщений от обработчика запросов ВАТС Мегафон

--- crm/pull/index.php
+++ crm/pull/index.php
@@ -1,8 +1,8 @@
 <?
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
-$APPLICATION->SetTitle("Push & Pull");
+$APPLICATION->SetTitle("Сообщения от ВАТС Мегафон");
 ?>
 <?
-$APPLICATION->IncludeComponent("ucre:pull.test", '');
+$APPLICATION->IncludeComponent("ucre:pull.megapbx", '');
 ?>
 <?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
