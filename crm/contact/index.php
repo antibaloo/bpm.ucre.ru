4f06c923f5f8f68d235d469049ba4f5052a69bf5
Andrew Abalakov <a.s.abalakov@ucre.ru>
2017-04-18 11:48:03 +0500

    Убрал компонент shum.crm.maskphone из лидов, контактов и компаний

---
+++ crm/contact/index.php
@@ -0,0 +1,34 @@
+<?
+require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
+IncludeModuleLangFile($_SERVER["DOCUMENT_ROOT"]."/bitrix/modules/intranet/public/crm/contact/index.php");
+$APPLICATION->SetTitle(GetMessage("CRM_TITLE"));
+?><?$APPLICATION->IncludeComponent(
+	"bitrix:crm.contact", 
+	".default", 
+	array(
+		"SEF_MODE" => "Y",
+		"PATH_TO_LEAD_SHOW" => "/crm/lead/show/#lead_id#/",
+		"PATH_TO_LEAD_EDIT" => "/crm/lead/edit/#lead_id#/",
+		"PATH_TO_LEAD_CONVERT" => "/crm/lead/convert/#lead_id#/",
+		"PATH_TO_COMPANY_SHOW" => "/crm/company/show/#company_id#/",
+		"PATH_TO_COMPANY_EDIT" => "/crm/company/edit/#company_id#/",
+		"PATH_TO_DEAL_SHOW" => "/crm/deal/show/#deal_id#/",
+		"PATH_TO_DEAL_EDIT" => "/crm/deal/edit/#deal_id#/",
+		"PATH_TO_INVOICE_SHOW" => "/crm/invoice/show/#invoice_id#/",
+		"PATH_TO_INVOICE_EDIT" => "/crm/invoice/edit/#invoice_id#/",
+		"PATH_TO_USER_PROFILE" => "/company/personal/user/#user_id#/",
+		"ELEMENT_ID" => $_REQUEST["contact_id"],
+		"SEF_FOLDER" => "/crm/contact/",
+		"COMPONENT_TEMPLATE" => ".default",
+		"NAME_TEMPLATE" => "#LAST_NAME# #NAME# #SECOND_NAME#",
+		"SEF_URL_TEMPLATES" => array(
+			"index" => "index.php",
+			"list" => "list/",
+			"edit" => "edit/#contact_id#/",
+			"show" => "show/#contact_id#/",
+			"service" => "service/",
+			"import" => "import/",
+		)
+	),
+	false
+);?><?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
---
+++ crm/lead/index.php
@@ -0,0 +1,43 @@
+<?
+require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
+IncludeModuleLangFile($_SERVER["DOCUMENT_ROOT"]."/bitrix/modules/intranet/public/crm/lead/index.php");
+$APPLICATION->SetTitle(GetMessage("CRM_TITLE"));
+?><?$APPLICATION->IncludeComponent(
+	"bitrix:crm.lead", 
+	"", 
+	Array(
+	"SEF_MODE" => "Y",	// Включить поддержку ЧПУ
+		"PATH_TO_CONTACT_SHOW" => "/crm/contact/show/#contact_id#/",
+		"PATH_TO_CONTACT_EDIT" => "/crm/contact/edit/#contact_id#/",
+		"PATH_TO_COMPANY_SHOW" => "/crm/company/show/#company_id#/",
+		"PATH_TO_COMPANY_EDIT" => "/crm/company/edit/#company_id#/",
+		"PATH_TO_DEAL_SHOW" => "/crm/deal/show/#deal_id#/",
+		"PATH_TO_DEAL_EDIT" => "/crm/deal/edit/#deal_id#/",
+		"PATH_TO_USER_PROFILE" => "/company/personal/user/#user_id#/",
+		"PATH_TO_PRODUCT_EDIT" => "/crm/product/edit/#product_id#/",
+		"PATH_TO_PRODUCT_SHOW" => "/crm/product/show/#product_id#/",
+		"ELEMENT_ID" => $_REQUEST["lead_id"],	// ID лида
+		"SEF_FOLDER" => "/crm/lead/",	// Каталог ЧПУ (относительно корня сайта)
+		"SEF_URL_TEMPLATES" => array(
+			"index" => "index.php",
+			"list" => "list/",
+			"edit" => "edit/#lead_id#/",
+			"show" => "show/#lead_id#/",
+			"convert" => "convert/#lead_id#/",
+			"import" => "import/",
+			"service" => "service/",
+			"dedupe" => "dedupe/",
+		),
+		"VARIABLE_ALIASES" => array(
+			"index" => "",
+			"list" => "",
+			"edit" => "",
+			"show" => "",
+			"convert" => "",
+			"import" => "",
+			"service" => "",
+			"dedupe" => "",
+		)
+	),
+	false
+);?><?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
