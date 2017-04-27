e93f9678b9af51fd69c41f24f83f8f7b43275998
Andrew Abalakov <a.s.abalakov@ucre.ru>
2017-04-27 12:20:10 +0500

    В компонент ucre:crm.deal.ro добавлены шаблоны для комнаты и квартиры. Начат шаблон дома

--- bitrix/components/ucre/crm.deal.ro/component.php
+++ bitrix/components/ucre/crm.deal.ro/component.php
@@ -14,28 +14,55 @@ if ($mainDeal["CATEGORY_ID"] == 0 || $mainDeal["CATEGORY_ID"] == 4){
 	if ($mainDeal["UF_CRM_1469534140"]){
     $rsObject = CIBlockElement::GetById($mainDeal["UF_CRM_1469534140"]);
     $mainObject = $rsObject->GetNextElement();
-    $arResult['RO'] = $mainObject->GetFields();
-    $arResult['RO']['PROPERTIES'] = $mainObject->GetProperties();
-    switch ($arResult['RO']['PROPERTIES']['TYPE']['VALUE']) {
-      case 'комната':
+    $objectFields = $mainObject->GetFields();
+    $objectProperties = $mainObject->GetProperties();
+    $arResult['ADDRESS'] = ($objectProperties['ADDRESS']['VALUE'])?$objectProperties['ADDRESS']['VALUE']:'<span style="color: red">нет данных</span>';
+    $arResult['LINK'] = ($objectProperties['LINK']['VALUE'])?'<a href="'.$objectProperties['LINK']['VALUE'].'" traget="_blank">'.$objectProperties['LINK']['VALUE'].'</a>':'<span style="color: red">нет данных</span>';
+    switch ($objectProperties['TYPE']['VALUE']) {
+			case 'комната':
+				if ($objectProperties['ISHOSTEL']['VALUE'] == 'да'){
+					$arResult['TYPE'] = $objectProperties['TYPE']['VALUE']." в общежитии";
+				}elseif ($objectProperties['ISHOSTEL']['VALUE'] == 'нет'){
+					$rooms = ($objectProperties['ROOMS']['VALUE'])?$objectProperties['ROOMS']['VALUE']."-к ":'<span style="color: red">?-к </span>';
+					$arResult['TYPE'] = $objectProperties['TYPE']['VALUE']." в ".$rooms." квартире";
+				}else {
+					$arResult['TYPE'] = $objectProperties['TYPE']['VALUE']." неизвестно где";
+				}
+				$arResult['ROOM_AREA'] = ($objectProperties['ROOM_AREA']['VALUE'])?$objectProperties['ROOM_AREA']['VALUE']." м<sup>2</sup>":'<span style="color: red">нет данных</span>';
+				$arResult['HOUSE_TYPE'] = ($objectProperties['HOUSE_TYPE']['VALUE'])?$objectProperties['HOUSE_TYPE']['VALUE']:'<span style="color: red">нет данных</span>';
+				$arResult['FLOOR'] = ($objectProperties['FLOOR']['VALUE'])?$objectProperties['FLOOR']['VALUE']:'<span style="color: red">нет данных</span>';
+        $arResult['FLOORALL'] = ($objectProperties['FLOORALL']['VALUE'])?$objectProperties['FLOORALL']['VALUE']:'<span style="color: red">нет данных</span>';
         $componentPage = 'room';
         break;
       case 'квартира':
+        $rooms = ($objectProperties['ROOMS']['VALUE'])?$objectProperties['ROOMS']['VALUE']."-к ":'<span style="color: red">?-к </span>';
+        $arResult['TYPE'] = $rooms.$objectProperties['TYPE']['VALUE'];
+        $arResult['TOTAL_AREA'] = ($objectProperties['TOTAL_AREA']['VALUE'])?$objectProperties['TOTAL_AREA']['VALUE']." м<sup>2</sup>":'<span style="color: red">нет данных</span>';
+        $arResult['LIVE_AREA'] = ($objectProperties['LIVE_AREA']['VALUE'])?$objectProperties['LIVE_AREA']['VALUE']." м<sup>2</sup>":'<span style="color: red">нет данных</span>';
+        $arResult['KITCHEN_AREA'] = ($objectProperties['KITCHEN_AREA']['VALUE'])?$objectProperties['KITCHEN_AREA']['VALUE']." м<sup>2</sup>":'<span style="color: red">нет данных</span>';
+        $arResult['HOUSE_TYPE'] = ($objectProperties['HOUSE_TYPE']['VALUE'])?$objectProperties['HOUSE_TYPE']['VALUE']:'<span style="color: red">нет данных</span>';
+        $arResult['FLOOR'] = ($objectProperties['FLOOR']['VALUE'])?$objectProperties['FLOOR']['VALUE']:'<span style="color: red">нет данных</span>';
+        $arResult['FLOORALL'] = ($objectProperties['FLOORALL']['VALUE'])?$objectProperties['FLOORALL']['VALUE']:'<span style="color: red">нет данных</span>';
         $componentPage = 'flat';
         break;
       case 'дом':
+        $arResult['TYPE'] = $objectProperties['TYPE']['VALUE'];
         $componentPage = 'house';
         break;
       case 'таунхаус':
+        $arResult['TYPE'] = $objectProperties['TYPE']['VALUE'];
         $componentPage = 'townhouse';
         break;
       case 'дача':
+        $arResult['TYPE'] = $objectProperties['TYPE']['VALUE'];
         $componentPage = 'dacha';
         break;
       case 'участок':
+        $arResult['TYPE'] = $objectProperties['TYPE']['VALUE'];
         $componentPage = 'plot';
         break;
       case 'коммерческий':
+        $arResult['TYPE'] = $objectProperties['TYPE']['VALUE'];
         $componentPage = 'comm';
         break;
     }
---
+++ bitrix/components/ucre/crm.deal.ro/templates/.default/flat.php
@@ -0,0 +1,64 @@
+<?php
+if(!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED!==true) die();
+?>
+<table class="crm-offer-info-table crm-offer-main-info-text">
+  <tbody>
+    <tr><td colspan="5"><div class="crm-offer-title">Характеристики объекта по адресу: <?=$arResult['ADDRESS']?></div></td></tr>
+    <tr class="crm-offer-row">
+      <td class="crm-offer-info-drg-btn"></td>
+      <td class="crm-offer-info-left">
+        <div class="crm-offer-info-label-wrap"><span class="crm-offer-info-label">Тип недвижимости:</span></div>
+      </td>
+      <td class="crm-offer-info-right">
+        <div class="crm-offer-info-label-wrap"><span class="crm-offer-info-label"><?=$arResult['TYPE']?></span></div>
+      </td>
+      <td class="crm-offer-info-left">
+        <div class="crm-offer-info-label-wrap"><span class="crm-offer-info-label">Ссылка на сайте:</span></div>
+      </td><td class="crm-offer-info-right">
+      <div class="crm-offer-info-label-wrap"><span class="crm-offer-info-label"><?=$arResult['LINK']?></span></div>
+      </td>
+    </tr>
+    <tr class="crm-offer-row">
+      <td class="crm-offer-info-drg-btn"></td>
+      <td class="crm-offer-info-left">
+        <div class="crm-offer-info-label-wrap"><span class="crm-offer-info-label">Общая плошадь:</span></div>
+      </td>
+      <td class="crm-offer-info-right">
+        <div class="crm-offer-info-label-wrap"><span class="crm-offer-info-label"><?=$arResult['TOTAL_AREA']?></span></div>
+      </td>
+      <td class="crm-offer-info-left">
+        <div class="crm-offer-info-label-wrap"><span class="crm-offer-info-label">Тип дома:</span></div>
+      </td><td class="crm-offer-info-right">
+      <div class="crm-offer-info-label-wrap"><span class="crm-offer-info-label"><?=$arResult['HOUSE_TYPE']?></span></div>
+      </td>
+    </tr>
+    <tr class="crm-offer-row">
+      <td class="crm-offer-info-drg-btn"></td>
+      <td class="crm-offer-info-left">
+        <div class="crm-offer-info-label-wrap"><span class="crm-offer-info-label">Жилая площадь:</span></div>
+      </td>
+      <td class="crm-offer-info-right">
+        <div class="crm-offer-info-label-wrap"><span class="crm-offer-info-label"><?=$arResult['LIVE_AREA']?></span></div>
+      </td>
+      <td class="crm-offer-info-left">
+        <div class="crm-offer-info-label-wrap"><span class="crm-offer-info-label">Этаж:</span></div>
+      </td><td class="crm-offer-info-right">
+      <div class="crm-offer-info-label-wrap"><span class="crm-offer-info-label"><?=$arResult['FLOOR']?></span></div>
+      </td>
+    </tr>
+    <tr class="crm-offer-row">
+      <td class="crm-offer-info-drg-btn"></td>
+      <td class="crm-offer-info-left">
+        <div class="crm-offer-info-label-wrap"><span class="crm-offer-info-label">Площадь кухни:</span></div>
+      </td>
+      <td class="crm-offer-info-right">
+        <div class="crm-offer-info-label-wrap"><span class="crm-offer-info-label"><?=$arResult['KITCHEN_AREA']?></span></div>
+      </td>
+      <td class="crm-offer-info-left">
+        <div class="crm-offer-info-label-wrap"><span class="crm-offer-info-label">Этажность:</span></div>
+      </td><td class="crm-offer-info-right">
+      <div class="crm-offer-info-label-wrap"><span class="crm-offer-info-label"><?=$arResult['FLOORALL']?></span></div>
+      </td>
+    </tr>
+  </tbody>
+</table>
