29416dc7db641d91b9f89f04ad7281991e05b421
Andrew Abalakov <a.s.abalakov@ucre.ru>
2017-04-27 13:48:25 +0500

    Блок вывода информации по связанному объекту недвижимости: гора кода заменена на лаконичный вывод компонента, в которм четко разделены логика вызова и шаблон вывода :)

--- bitrix/components/bitrix/crm.deal.show/templates/.default/template.php
+++ bitrix/components/bitrix/crm.deal.show/templates/.default/template.php
@@ -328,98 +328,6 @@ if ($mainDeal['UF_CRM_589C63CD96E82'] && !$mainDeal["UF_CRM_1469534140"]){
 <?	
 }
 /*-----------------------------------------------------------------------------------*/
-/*---------Блок информации о связанном объекте----------*/
-$rsDeal = CCrmDeal::GetListEx(
-	array(),
-	array("ID" => $element['ID']),
-	false,
-	false,
-	array("CATEGORY_ID", "UF_CRM_1469534140"),
-	array()
-);
-$mainDeal = $rsDeal->Fetch();		
-if ($mainDeal["CATEGORY_ID"] == 0 || $mainDeal["CATEGORY_ID"] == 4){		
-	if ($mainDeal["UF_CRM_1469534140"]){		
-		$rsObject = CIBlockElement::GetById($mainDeal["UF_CRM_1469534140"]);		
-		$mainObject = $rsObject->GetNextElement();		
-		$objectFields = $mainObject->GetFields();		
-		$objectFields['PROPERTIES'] = $mainObject->GetProperties();		
-?>		
-<table class="crm-offer-info-table crm-offer-main-info-text">		
-	<tbody>		
-		<tr><td colspan="5"><div class="crm-offer-title">Характеристики объекта по адресу: <?=$objectFields['PROPERTIES']['ADDRESS']['VALUE']?></div></td></tr>		
-	<tr class="crm-offer-row">		
-			<td class="crm-offer-info-drg-btn"></td>		
-			<td class="crm-offer-info-left">		
-				<div class="crm-offer-info-label-wrap"><span class="crm-offer-info-label">Тип недвижимости:</span></div>		
-			</td>		
-			<td class="crm-offer-info-right">		
-				<div class="crm-offer-info-label-wrap"><span class="crm-offer-info-label"><?=$objectFields['PROPERTIES']['TYPE']['VALUE']?></span></div>		
-			</td>		
-			<td class="crm-offer-info-left">		
-				<div class="crm-offer-info-label-wrap"><span class="crm-offer-info-label">Кол-во комнат:</span></div>		
-			</td>		
-			<td class="crm-offer-info-right">		
-				<div class="crm-offer-info-label-wrap"><span class="crm-offer-info-label"><?=($objectFields['PROPERTIES']['ROOMS']['VALUE'])?$objectFields['PROPERTIES']['ROOMS']['VALUE']:"нет данных"?></span></div>		
-			</td>		
-		</tr>		
-		<tr class="crm-offer-row">		
-			<td class="crm-offer-info-drg-btn"></td>		
-			<td class="crm-offer-info-left">		
-				<div class="crm-offer-info-label-wrap"><span class="crm-offer-info-label">Площади (общ./жил./кух.):</span></div>		
-			</td>		
-			<td class="crm-offer-info-right">		
-				<div class="crm-offer-info-label-wrap">		
-					<span class="crm-offer-info-label">		
-						<?=($objectFields['PROPERTIES']['TOTAL_AREA']['VALUE'])?$objectFields['PROPERTIES']['TOTAL_AREA']['VALUE']:"нет данных"?>/<?=($objectFields['PROPERTIES']['LIVE_AREA']['VALUE'])?$objectFields['PROPERTIES']['LIVE_AREA']['VALUE']:"нет данных"?>/<?=($objectFields['PROPERTIES']['KITCHEN_AREA']['VALUE'])?$objectFields['PROPERTIES']['KITCHEN_AREA']['VALUE']:"нет данных"?>		
-					</span>		
-				</div>		
-			</td>		
-			<td class="crm-offer-info-left">		
-				<div class="crm-offer-info-label-wrap"><span class="crm-offer-info-label">Площадь участка:</span></div>		
-			</td>		
-			<td class="crm-offer-info-right">		
-				<div class="crm-offer-info-label-wrap"><span class="crm-offer-info-label"><?=($objectFields['PROPERTIES']['PLOT_AREA']['VALUE'])?$objectFields['PROPERTIES']['PLOT_AREA']['VALUE']." сот.":"нет данных"?></span></div>		
-			</td>		
-		</tr>		
-		<tr class="crm-offer-row">		
-			<td class="crm-offer-info-drg-btn"></td>		
-			<td class="crm-offer-info-left">		
-				<div class="crm-offer-info-label-wrap"><span class="crm-offer-info-label">Этаж/Этажность:</span></div>		
-			</td>		
-			<td class="crm-offer-info-right">		
- 				<div class="crm-offer-info-label-wrap"><span class="crm-offer-info-label"><?=($objectFields['PROPERTIES']['FLOOR']['VALUE'])?$objectFields['PROPERTIES']['FLOOR']['VALUE']:"нет данных"?>/<?=($objectFields['PROPERTIES']['FLOORALL']['VALUE'])?$objectFields['PROPERTIES']['FLOORALL']['VALUE']:"нет данных"?></span></div>		
- 			</td>		
- 			<td class="crm-offer-info-left">		
- 				<div class="crm-offer-info-label-wrap"><span class="crm-offer-info-label">Ссылка на сайте:</span></div>		
- 			</td>		
- 			<td class="crm-offer-info-right">		
-				<div class="crm-offer-info-label-wrap">
-					<span class="crm-offer-info-label">
-						<?if ($objectFields['PROPERTIES']['LINK']['VALUE']){?>
-						<a href="<?=$objectFields['PROPERTIES']['LINK']['VALUE']?>" target="_blank">
-							<?=$objectFields['PROPERTIES']['LINK']['VALUE']?>
-						</a>
-						<?} else {?>
-						нет данных
-						<?}?>
-					</span>
-				</div>		
- 			</td>		
- 		</tr>		
- 	</tbody>		
- </table>		
- <?		
- 	} else {		
- ?>		
- <table class="crm-offer-info-table crm-offer-main-info-text">		
- 	<tbody><tr><td colspan="5"><div class="crm-offer-title">Нет связанного объекта</div></td></tr></tbody>		
- </table>		
- <?				
- 	}		
- }		
-/*------Конец блока информации о связанном объекте----------*/
-
 /*Общий компонент для отображения данных связанного объекта*/
 if ($USER->GetID() == 24) {
 	$APPLICATION->IncludeComponent(
@@ -429,7 +337,6 @@ if ($USER->GetID() == 24) {
 	);
 }
 /*----------------------------------------------------------*/
-
 $APPLICATION->IncludeComponent(
 	'bitrix:crm.interface.form',
 	'show',
