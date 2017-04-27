a08c50dee02d8ad6770d76f0030932645dd7922c
Andrew Abalakov <a.s.abalakov@ucre.ru>
2017-04-27 18:03:58 +0500

    Компонент вывода информации об объявлении Авито для заявок созданных из лидов парсера Авито

---
+++ bitrix/components/ucre/crm.deal.avito/component.php
@@ -0,0 +1,46 @@
+<?
+CModule::IncludeModule('crm');
+if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED!==true) die();//Запрет вызова из адресной строки браузера
+$rsDeal = CCrmDeal::GetListEx(
+	array(),
+	array("ID" => $arParams['DEAL_ID']),
+	false,
+	false,
+	array(
+		'STAGE_ID', 						//статус заявки
+		'UF_CRM_1469534140',		//связанный объект
+		'UF_CRM_589C63CD96E82',	//id авито
+		'UF_CRM_589C63CE03874',	//ссылка
+		'UF_CRM_58A2B07090172',	//профиль
+		'UF_CRM_58A2B9F26548F',	//фото авито
+		'UF_CRM_58958B52BA439',	//общая
+		'UF_CRM_58958B52D6C9B',	//жилая
+		'UF_CRM_58958B52F2BAC',	//площадь кухни
+		'UF_CRM_5895994ED0C7B',	//этаж
+		'UF_CRM_58958B51C2F36',	//этажность
+		'UF_CRM_58958B529E628',	//кол-во комнат
+		'UF_CRM_5895994EB2646',	//адрес
+		'UF_CRM_58958B5207D0C'	//тип дома
+	),
+	array()
+);
+$mainDeal = $rsDeal->Fetch();
+$arResult['LINK'] = '<a href="'.$mainDeal['UF_CRM_589C63CE03874'].'" target="_blank">'.$mainDeal['UF_CRM_589C63CE03874'].'</a>';
+$arResult['PROFILE'] = '<a href="'.$mainDeal['UF_CRM_58A2B07090172'].'" target="_blank">'.$mainDeal['UF_CRM_58A2B07090172'].'</a>';
+$arResult['PHOTO'] = "";
+foreach (unserialize($mainDeal['UF_CRM_58A2B9F26548F']) as $avitolink){
+  $arResult['PHOTO'] .= "<a class='fancybox' rel='image_gallery' href='".$avitolink."'><img style='margin-right: 10px; border:1px solid #cccccc;' src='".$avitolink."' width = 'auto' height ='50'/></a>";
+}
+$total = ($mainDeal['UF_CRM_58958B52BA439'])?$mainDeal['UF_CRM_58958B52BA439']:"-";
+$live = ($mainDeal['UF_CRM_58958B52D6C9B'])?$mainDeal['UF_CRM_58958B52D6C9B']:"-";
+$kitchen = ($mainDeal['UF_CRM_58958B52F2BAC'])?$mainDeal['UF_CRM_58958B52F2BAC']:"-";
+$arResult['SQUARE'] = $total."/".$live."/".$kitchen;
+$floor = ($mainDeal['UF_CRM_5895994ED0C7B'])?$mainDeal['UF_CRM_5895994ED0C7B']:"-";
+$floorall = ($mainDeal['UF_CRM_58958B51C2F36'])?$mainDeal['UF_CRM_58958B51C2F36']:"-";
+$arResult['FLOORS'] = $floor."/".$floorall;
+$arResult['ADDRESS'] = $mainDeal['UF_CRM_5895994EB2646'];
+$arResult['ID'] = $mainDeal['ID'];
+if ($mainDeal['UF_CRM_589C63CD96E82'] && !$mainDeal["UF_CRM_1469534140"]){
+  $this->IncludeComponentTemplate();
+}
+?>
--- bitrix/components/ucre/crm.deal.avito/templates/.default/template.php
+++ bitrix/components/ucre/crm.deal.avito/templates/.default/template.php
@@ -1,3 +1,83 @@
 <?php
 if(!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED!==true) die();
 ?>
+<table class="crm-offer-info-table crm-offer-main-info-text">
+  <tbody>
+    <tr><td colspan="5"><div class="crm-offer-title">Заявка создана из лида по данным парсера Авито-Недвижимость</div></td></tr>
+    <tr class="crm-offer-row">
+			<td class="crm-offer-info-drg-btn"></td>
+			<td class="crm-offer-info-left">
+				<div class="crm-offer-info-label-wrap"><span class="crm-offer-info-label">Ссылка на объявление:</span></div>
+			</td>
+			<td class="crm-offer-info-right">
+				<div class="crm-offer-info-label-wrap"><span class="crm-offer-info-label"><?=$arResult['LINK']?></span></div>
+			</td>
+			<td class="crm-offer-info-left">
+				<div class="crm-offer-info-label-wrap"><span class="crm-offer-info-label">Профиль Авито:</span></div>
+			</td>
+			<td class="crm-offer-info-right">
+				<div class="crm-offer-info-label-wrap"><span class="crm-offer-info-label"><?=$arResult['PROFILE']?></span></div>
+			</td>
+		</tr>
+    <tr class="crm-offer-row">
+			<td class="crm-offer-info-drg-btn"></td>
+			<td class="crm-offer-info-left">
+				<div class="crm-offer-info-label-wrap"><span class="crm-offer-info-label">Фотографии с Авито:</span></div>
+			</td>
+			<td class="crm-offer-info-right" colspan="3">
+				<div class="crm-offer-info-label-wrap" style="text-align: center;">
+          <?=$arResult['PHOTO']?>
+				</div>
+			</td>
+		</tr>
+    <tr class="crm-offer-row">
+			<td class="crm-offer-info-drg-btn"></td>
+			<td class="crm-offer-info-left">
+				<div class="crm-offer-info-label-wrap"><span class="crm-offer-info-label">Площади (общ./жил./кух.):</span></div>
+			</td>
+			<td class="crm-offer-info-right">
+				<div class="crm-offer-info-label-wrap">
+					<span class="crm-offer-info-label">
+						<?=$arResult['SQUARE']?>
+					</span>
+				</div>
+			</td>
+			<td class="crm-offer-info-left">
+				<div class="crm-offer-info-label-wrap"><span class="crm-offer-info-label">Этаж/Этажность:</span></div>
+			</td>
+			<td class="crm-offer-info-right">
+				<div class="crm-offer-info-label-wrap">
+					<span class="crm-offer-info-label">
+						<?=$arResult['FLOORS']?>
+					</span>
+				</div>
+			</td>
+		</tr>
+    <tr class="crm-offer-row">
+			<td class="crm-offer-info-drg-btn"></td>
+			<td class="crm-offer-info-left">
+				<div class="crm-offer-info-label-wrap"><span class="crm-offer-info-label">Адрес:</span></div>
+			</td>
+			<td class="crm-offer-info-right">
+				<div class="crm-offer-info-label-wrap"><span class="crm-offer-info-label"><?=$arResult['ADDRESS']?></span></div>
+			</td>
+			<td class="crm-offer-info-left">
+				<div class="crm-offer-info-label-wrap">
+					<span class="crm-offer-info-label">
+						<form id="object">
+							<input type="hidden" name="deal_id" value="<?=$arResult['ID']?>">
+							<input type="button" id="create" value="Создать объект" <?=$arResult['BUTTON']?>/>
+						</form>
+					</span>
+				</div>
+			</td>
+			<td class="crm-offer-info-right">
+				<div class="crm-offer-info-label-wrap">
+					<span class="crm-offer-info-label">
+						<div id="result"><?=$arResult['RESULT']?></div>
+					</span>
+				</div>
+			</td>
+		</tr>
+  </tbody>
+</table>
