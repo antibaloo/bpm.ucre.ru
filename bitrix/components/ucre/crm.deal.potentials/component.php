2d2a8b409613af74a8dbdd3a80fda64cbf52b6a2
Andrew Abalakov <a.s.abalakov@ucre.ru>
2017-04-28 12:24:01 +0500

    Инициализация компонента ucre:crm.deal.potentials

---
+++ bitrix/components/ucre/crm.deal.potentials/component.php
@@ -0,0 +1,6 @@
+<?
+CModule::IncludeModule('crm');
+if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED!==true) die();//Запрет вызова из адресной строки браузера
+
+$this->IncludeComponentTemplate();
+?>
