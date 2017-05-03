3334ee5c1ed3437fdbb64fab5a125b6b8d7cb872
Andrew Abalakov <a.s.abalakov@ucre.ru>
2017-05-03 17:24:19 +0500

    Компонент ucre:crm.deal.potentials вызыввается с параметрами ID - ID заявки, CATEGORY - категория заявки, TABID - id  div для вызова ajax, FILTER - каие пары показывать, по умолчанию new

--- ajax/potentials.php
+++ ajax/potentials.php
@@ -1,9 +1,13 @@
 <?php
 require_once($_SERVER['DOCUMENT_ROOT'] . "/bitrix/modules/main/include/prolog_before.php");
-$APPLICATION->IncludeComponent(
-  "ucre:crm.deal.potentials",
-  "",
-  array('ID' => $_GET['id'], 'CATEGORY' => $_GET['category']),
-  false
-);
+if ($USER->GetID() == 24){
+  $APPLICATION->IncludeComponent(
+    "ucre:crm.deal.potentials",
+    "",
+    array('ID' => $_POST['id'], 'CATEGORY' => $_POST['category'], 'TABID' =>$_POST['tabid'], 'FILTER' => ($_POST['filter'])?$_POST['filter']:"new"),
+    false
+  );
+}else{
+  echo "<br><center><img style='margin: 0 auto;' src='/ajax/images/construction.png'></center>";
+}
 ?>
