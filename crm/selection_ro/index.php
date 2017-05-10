f7e779b63e28f9ec5eca8b008f7c665eaa2f3b5c
Andrew Abalakov <a.s.abalakov@ucre.ru>
2017-05-10 12:09:06 +0500

    Добовлен поиск по полю 'Район' по подстроке, добавлен поиск по полю 'Тэги' по логике 'ИЛИ'

--- crm/selection_ro/index.php
+++ crm/selection_ro/index.php
@@ -45,6 +45,7 @@ $rsUsers = CUser::GetList(($by="id"), ($order="asc"), array("GROUPS_ID"=> array(
       ?>
     </select>
     <input name="withfired" type="checkbox" <?=($_POST['withfired'])?"checked":""?> onchange="toggle_fired(this)"> с уволенными
+    <hr> Тэги (через запятую) <input name="tags" type="text" size="100" value="<?=$_POST['tags']?>">
     <div id="buy" <?=($_POST['goal']!='buy'? 'style="display:none"':'')?>>
       <fieldset>
         <legend>
@@ -104,6 +105,7 @@ $rsUsers = CUser::GetList(($by="id"), ($order="asc"), array("GROUPS_ID"=> array(
         S <sub>кух.</sub> от <input type="number" min="1" name="kitchen_s" style="width: 4em;" value="<?=$_POST['kitchen_s']?>">,
         не 1-й<input type="checkbox" name="nfirst" <?=($_POST['nfirst'])?"checked":""?>>, 
         не последний <input type="checkbox" name="nlast" <?=($_POST['nlast'])?"checked":""?>>,
+        район <input type="text" name="locality" size="40" value="<?=$_POST['locality']?>">, 
         улица <input type="text" name="street" size="40" value="<?=$_POST['street']?>">
       </fieldset>
     </div>
@@ -123,6 +125,7 @@ $rsUsers = CUser::GetList(($by="id"), ($order="asc"), array("GROUPS_ID"=> array(
   $map_data['yandex_scale'] = "10";
   
   if ($_POST['goal']=='buy') {
+    
     $rsQuery = "SELECT b_crm_deal.ID, b_crm_deal.TITLE, b_crm_deal.ASSIGNED_BY_ID, b_uts_crm_deal.UF_CRM_5895BC940ED3F,b_uts_crm_deal.UF_CRM_58CFC7CDAAB96,b_uts_crm_deal.UF_CRM_58958B529E628,b_uts_crm_deal.UF_CRM_58958B52BA439,b_uts_crm_deal.UF_CRM_58958B52F2BAC,b_uts_crm_deal.UF_CRM_58958B51B667E, b_uts_crm_deal.UF_CRM_58958B576448C, b_uts_crm_deal.UF_CRM_58958B5751841 FROM b_crm_deal INNER JOIN b_uts_crm_deal ON b_crm_deal.ID=b_uts_crm_deal.VALUE_ID WHERE b_crm_deal.CATEGORY_ID = 2 AND b_crm_deal.STAGE_ID = 'C2:PROPOSAL'";
     //Фильтр по рынку
     if ($_POST['market'] == "Первичный") $rsQuery.=" AND UF_CRM_5895BC940ED3F LIKE '%828%'";
@@ -143,6 +146,16 @@ $rsUsers = CUser::GetList(($by="id"), ($order="asc"), array("GROUPS_ID"=> array(
     if ($_POST['price'] > 0) $rsQuery.=" AND UF_CRM_58958B576448C<=".$_POST['price']." AND UF_CRM_58958B5751841>=".$_POST['price'];
     //Фильтр по ответственному
     if ($_POST['assigned'] !='') $rsQuery.=" AND ASSIGNED_BY_ID=".$_POST['assigned'] ;
+    //Фильтр по тэгам
+    if ($_POST['tags']){
+      $tags = explode(",",$_POST["tags"]);
+      $rsQuery.= " AND (";
+      foreach ($tags as $key=>$tag){
+        $rsQuery.= "UF_CRM_1494396942 LIKE '%".trim($tag)."%'";
+        if ($key != count($tags)-1) $rsQuery.= " OR ";
+      }
+      $rsQuery.= ")";
+    }
     $rsQuery .= " ORDER BY b_crm_deal.ID DESC";
     $rsData = $DB->Query($rsQuery);
     $count = $rsData->SelectedRowsCount();
@@ -219,7 +232,7 @@ $rsUsers = CUser::GetList(($by="id"), ($order="asc"), array("GROUPS_ID"=> array(
 //    echo $count."=".$pages." по ".$rows;
   }
   if ($_POST['goal']=='sell') {
-    $rsQuery = "SELECT b_crm_deal.ID,b_crm_deal.CATEGORY_ID, b_crm_deal.TITLE, b_crm_deal.ASSIGNED_BY_ID, b_uts_crm_deal.UF_CRM_1469534140,b_uts_crm_deal.UF_CRM_58958B5734602, b_iblock_element.NAME, b_iblock_element_prop_s42.PROPERTY_210, b_iblock_element_prop_s42.PROPERTY_209, b_iblock_element_prop_s42.PROPERTY_229, b_iblock_element_prop_s42.PROPERTY_224, b_iblock_element_prop_s42.PROPERTY_226, b_iblock_element_prop_s42.PROPERTY_221, b_iblock_element_prop_s42.PROPERTY_222, b_iblock_element_prop_s42.PROPERTY_292, b_iblock_element_prop_s42.PROPERTY_298, b_iblock_element_prop_s42.PROPERTY_299, b_iblock_element_prop_s42.PROPERTY_301 FROM b_crm_deal INNER JOIN b_uts_crm_deal ON b_crm_deal.ID=b_uts_crm_deal.VALUE_ID INNER JOIN b_iblock_element ON b_uts_crm_deal.UF_CRM_1469534140 = b_iblock_element.ID INNER JOIN b_iblock_element_prop_s42 ON b_iblock_element.ID = b_iblock_element_prop_s42.IBLOCK_ELEMENT_ID WHERE";
+    $rsQuery = "SELECT b_crm_deal.ID,b_crm_deal.CATEGORY_ID, b_crm_deal.TITLE, b_crm_deal.ASSIGNED_BY_ID, b_uts_crm_deal.UF_CRM_1469534140,b_uts_crm_deal.UF_CRM_58958B5734602, b_iblock_element.NAME, b_iblock_element_prop_s42.PROPERTY_210, b_iblock_element_prop_s42.PROPERTY_216, b_iblock_element_prop_s42.PROPERTY_209, b_iblock_element_prop_s42.PROPERTY_229, b_iblock_element_prop_s42.PROPERTY_224, b_iblock_element_prop_s42.PROPERTY_226, b_iblock_element_prop_s42.PROPERTY_221, b_iblock_element_prop_s42.PROPERTY_222, b_iblock_element_prop_s42.PROPERTY_292, b_iblock_element_prop_s42.PROPERTY_298, b_iblock_element_prop_s42.PROPERTY_299, b_iblock_element_prop_s42.PROPERTY_301 FROM b_crm_deal INNER JOIN b_uts_crm_deal ON b_crm_deal.ID=b_uts_crm_deal.VALUE_ID INNER JOIN b_iblock_element ON b_uts_crm_deal.UF_CRM_1469534140 = b_iblock_element.ID INNER JOIN b_iblock_element_prop_s42 ON b_iblock_element.ID = b_iblock_element_prop_s42.IBLOCK_ELEMENT_ID WHERE";
     //Фильтр по стадии заявки
     if ($_POST['stage_id'] == 'А+П') {
       $rsQuery.=" (b_crm_deal.STAGE_ID = 'PROPOSAL' or b_crm_deal.STAGE_ID = '8' or b_crm_deal.STAGE_ID = 'C4:1' or b_crm_deal.STAGE_ID = 'C4:PROPOSAL')";
@@ -246,10 +259,22 @@ $rsUsers = CUser::GetList(($by="id"), ($order="asc"), array("GROUPS_ID"=> array(
     if ($_POST['nfirst']) $rsQuery.=" AND PROPERTY_221<>1";
     //Не последний
     if ($_POST['nlast']) $rsQuery.=" AND PROPERTY_221<>PROPERTY_222";
+    //Фильтр по району н.п.
+    if ($_POST['locality']) $rsQuery.=" AND PROPERTY_216 LIKE '%".$_POST['locality']."%'";
     //Фильтр по улице
     if ($_POST['street']) $rsQuery.=" AND PROPERTY_217 LIKE '%".$_POST['street']."%'";
     //Фильтр по ответственному
     if ($_POST['assigned'] !='') $rsQuery.=" AND ASSIGNED_BY_ID=".$_POST['assigned'] ;
+    //Фильтр по тэгам
+    if ($_POST['tags']){
+      $tags = explode(",",$_POST["tags"]);
+      $rsQuery.= " AND (";
+      foreach ($tags as $key=>$tag){
+        $rsQuery.= "UF_CRM_1494396942 LIKE '%".trim($tag)."%'";
+        if ($key != count($tags)-1) $rsQuery.= " OR ";
+      }
+      $rsQuery.= ")";
+    }
     $rsQuery .= " ORDER BY b_uts_crm_deal.UF_CRM_58958B5734602 ASC";
     $rsData = $DB->Query($rsQuery);
     $count = $rsData->SelectedRowsCount();
@@ -310,7 +335,6 @@ $rsUsers = CUser::GetList(($by="id"), ($order="asc"), array("GROUPS_ID"=> array(
   ?>
     </center>
   </div>
-  <!--<a href="toprint.php?sql=<?=htmlspecialchars(serialize($_POST),ENT_QUOTES)?>" target="_blank">Версия для печати</a>-->
   <?
   }
   $arUser =$USER->GetById($USER->GetID())->Fetch();
