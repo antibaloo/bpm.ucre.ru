5fe4e212a6e0b9fa4d90aec961de8e12e74254bd
Andrew Abalakov <a.s.abalakov@ucre.ru>
2017-05-18 11:15:01 +0500

    Мелкие правки

--- bitrix/components/bitrix/crm.lead.show/templates/.default/script.js
+++ bitrix/components/bitrix/crm.lead.show/templates/.default/script.js
@@ -1,7 +1,5 @@
 $(document).ready(function() {
-		$("a.fancybox").fancybox();
-	});
-$(document).ready(function() {
+	$("a.fancybox").fancybox();
   $('#sync').on('click', function () {
     var data = $('#avito').serialize();
     $.ajax({
