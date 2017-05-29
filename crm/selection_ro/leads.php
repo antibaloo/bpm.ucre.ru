0a1116d98e0cd07eff7ad0ed7b252fe6f9df4af5
Andrew Abalakov <a.s.abalakov@ucre.ru>
2017-05-29 17:38:10 +0500

    Точка входа в парсер Авито-Недвижимость

--- crm/selection_ro/leads.php
+++ crm/selection_ro/leads.php
@@ -1,5 +1,5 @@
-
 <?
+$start = microtime(true);//Засекаем время выполнения скрипта
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 $APPLICATION->SetTitle("Поиск объектов на Авито");
 global $APPLICATION;
@@ -10,23 +10,29 @@ if(SITE_TEMPLATE_ID === 'bitrix24')
 	$APPLICATION->SetAdditionalCSS("/bitrix/themes/.default/bitrix24/crm-entity-show.css");
 }
 
-if (in_array($USER->GetID(), array(1,24))){
+if (in_array($USER->GetID(), array(1,24,203,204))){
 ?>
+<link href="custom.css?<?=time();?>" rel="stylesheet">
 <link rel="stylesheet" href="/bitrix/js/baloo/fancyapps/source/jquery.fancybox.css" type="text/css" media="screen" />
 <script type="text/javascript" src="/bitrix/js/baloo/fancyapps/source/jquery.fancybox.pack.js"></script>
 <form id="avito" name="avito">
   <input type="hidden" name="base_url" value="https://www.avito.ru/orenburg">
   <input type="radio" name="type" value="kvartiry" checked> Квартиры
-  <input type="radio" name="type" value="doma_dachi_kottedzhi"> Дома, дачи, коттеджи
+  <!--<input type="radio" name="type" value="doma_dachi_kottedzhi"> Дома, дачи, коттеджи
   <input type="radio" name="type" value="zemelnye_uchastki"> Земельные участк
   <input type="radio" name="type" value="kommercheskaya_nedvizhimost"> Коммерческая недвижимость
   <input type="radio" name="type" value="komnaty"> Комнаты
   <input type="radio" name="type" value="garazhi_i_mashinomesta"> Гаражи и машиноместа
-  <input type="radio" name="type" value="nedvizhimost_za_rubezhom"> Недвижимость за рубежом
+  <input type="radio" name="type" value="nedvizhimost_za_rubezhom"> Недвижимость за рубежом-->
   <input type="hidden" name="operation" value="prodam">
   <div id="kvartiry" style="display:block">
+		<select name="user">
+			<!--<option value="">Все</option>-->
+			<option value="1">Частные</option>
+			<option value="2">Агентства</option>
+		</select>
     <select name="rooms">
-      <option value="">(кол-во комнат)</option>
+      <!--<option value="">(кол-во комнат)</option>-->
       <option value="studii">Студии</option>
       <option value="1-komnatnye">1-комнатные</option>
       <option value="2-komnatnye">2-комнатные</option>
@@ -40,21 +46,22 @@ if (in_array($USER->GetID(), array(1,24))){
       <option value="mnogokomnatnye">многокомнатные</option>
     </select>
     <select name="market">
-      <option value="">(выберите рынок)</option>
+      <!--<option value="">(выберите рынок)</option>-->
       <option value="vtorichka">Вторичка</option>
       <option value="novostroyka">Новостройка</option>
     </select>
     <select name="type_house">
-      <option value="">(тип дома)</option>
+      <!--<option value="">(тип дома)</option>-->
       <option value="kirpichnyy_dom">Кирпичный</option>
       <option value="panelnyy_dom">Панельный</option>
       <option value="blochnyy_dom">Блочный</option>
       <option value="monolitnyy_dom">Монолитный</option>
       <option value="derevyannyy_dom">Деревяный</option>
     </select>
-    Этаж <input type="number" min="1" max="31" step="1" name="floor">
+		<input type="text" name="content" size="100" placeholder="ключевые слова">
+    <!--Этаж <input type="number" min="1" max="31" step="1" name="floor">
     <span style="border: 1px solid black"><input type="checkbox" name="notfirst"> не последний </span>
-    Этажей <input type="number" min="1" max="31" step="1" name="floors">
+    Этажей <input type="number" min="1" max="31" step="1" name="floors">-->
   </div>
   <div id="doma_dachi_kottedzhi" style="display:none"></div>
   <div id="zemelnye_uchastki" style="display:none"></div>
@@ -68,13 +75,45 @@ if (in_array($USER->GetID(), array(1,24))){
   
 </div>
 <?
+$time = microtime(true) - $start;
+//echo "<br>Скрипт отработал за ".$time." секунд.";
 }else{
   echo "Вам сюда пока нельзя!";
 }
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");
 ?>
 <script>
- function send_form() {
+  function set_active(object){
+    if(!object.classList.contains('active')){
+      var el = document.getElementById("page"+object.innerHTML);
+      var a_page = document.getElementsByClassName("page active");
+      var a_pages = document.getElementsByClassName("pages active");
+      a_page[0].classList.remove('active');
+      a_pages[0].classList.remove('active');
+      el.classList.add('active');
+      object.classList.add('active');
+    }
+  }
+	function show_ad(object) {
+		//document.getElementById('avitoAd').innerHTML += "<br>"+object.getAttribute("link");
+		//document.getElementById('avitoAd').style.display = 'block';
+		var adParams = object.getAttribute("adParams");
+		$.ajax({
+			type: "POST",
+			url:"./ajax/avito_parse.php",
+			dataType: "html",
+			data: {adParams: adParams},
+			success: function (html){
+				$("#avitoAd").html(html);
+				$("#avitoAd").show();
+			},
+			error: function (html){
+				$("#avitoAd").html("Технические неполадки! В ближайшее время все будет исправлено!");
+				$("#avitoAd").show();
+			},
+		});
+	}
+	function send_form() {
     var data = $('#avito').serialize();
     $.ajax({
       type: "POST",
@@ -89,6 +128,22 @@ require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");
       },
     });
   }
+	function convertToLead(id){
+		var data = $('#toLead_'+id).serialize();
+		$.ajax({
+      type: "POST",
+      url: "./ajax/converttolead.php",
+      dataType: "text",
+      data: data,
+      success: function (html) {
+        $("#avitoAd").html(html);
+				$("tr#"+id).detach();
+      },
+      error: function (html) {
+        $("#avitoAd").html("Технические неполадки! В ближайшее время все будет исправлено!");
+      },
+    });
+	}
   var rad = document.avito.type;
 	var prev = rad[0]; //Вкладка квартир включена по-умолчанию
   for(var i = 0; i < rad.length; i++) {
