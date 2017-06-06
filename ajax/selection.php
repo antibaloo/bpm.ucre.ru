38148ca774d51cd339b611af9d2bd4617a859db7
Andrew Abalakov <a.s.abalakov@ucre.ru>
2017-06-06 09:17:50 +0500

    Добавлены вызовы новых компонентов для поиска встречных(соответствующих) заявок. Поиск соответствий дял покупки/продажи разделен на разные компоненты дял упрощения внутренней логики компонентов

--- ajax/selection.php
+++ ajax/selection.php
@@ -1,9 +1,31 @@
 <?php
 require_once($_SERVER['DOCUMENT_ROOT'] . "/bitrix/modules/main/include/prolog_before.php");
-$APPLICATION->IncludeComponent(
-  "ucre:select.request",
-  "",
-  array('ID' => $_GET['id'], 'CATEGORY' => $_GET['category']),
-  false
-);
+if ($USER->GetID()!=24){
+  $APPLICATION->IncludeComponent(
+    "ucre:select.request",
+    "",
+    array('ID' => $_GET['id'], 'CATEGORY' => $_GET['category']),
+    false
+  );
+}else{
+ if ($_GET['category'] == 0 || $_GET['category'] == 4) {
+   echo "<h2>Встречные для продажи и новостроек!</h2>";
+   $APPLICATION->IncludeComponent(
+     "ucre:select.relevant.for_sell",
+     "",
+     array('ID' => $_GET['id']),
+     false
+   );
+ }elseif ($_GET['category'] == 2){
+   echo "<h2>Встречные для заявок на покупку!</h2>";
+   $APPLICATION->IncludeComponent(
+     "ucre:select.relevant.for_buy",
+     "",
+     array('ID' => $_GET['id']),
+     false
+   );
+ }else {
+   echo "<h2>Для это категории заявок поиск встречных не производится!</h2>";
+ }
+}
 ?>
