4d256ba228b001fdb48fd5977e80bd12cbb04588
Andrew Abalakov <a.s.abalakov@ucre.ru>
2017-07-07 10:19:01 +0500

    Обновление

--- bitrix/modules/crm/classes/general/crm_view_helper.php
+++ bitrix/modules/crm/classes/general/crm_view_helper.php
@@ -1118,7 +1118,8 @@ class CCrmViewHelper
 								'VALUE_TYPE_ID' => $valueTypeID,
 								'VALUE_TYPE' => $valueType,
 								'VALUE' => $value
-							)
+							),
+							$arOptions
 						)
 					),
 					'type' => htmlspecialcharsbx(
@@ -1809,7 +1810,7 @@ class CCrmViewHelper
 
 			if($processed > 0)
 			{
-				echo '<span class="bx-br-separator">&nbsp;</span>';
+				echo '<span class="bx-br-separator"><br/></span>';
 			}
 
 			echo '<span class="fields files">';
@@ -1818,7 +1819,7 @@ class CCrmViewHelper
 
 			if ($file->IsImage($fileInfo['ORIGINAL_NAME'], $fileInfo['CONTENT_TYPE']))
 			{
-				echo '<a class="fancybox" rel="image_gallery" href="https://bpm.ucre.ru'.$fileInfo['SRC'].'" title=""><img src="https://bpm.ucre.ru'.$fileInfo['SRC'].'" width = "auto" height ="50" alt="" /></a>';
+				echo $file->ShowImage($fileInfo, $fileMaxWidth, $fileMaxHeight, '', '', true, false, 0, 0, $fileUrlTemplate);
 			}
 			else
 			{
