939755558dc9be6a4216ea64bb915f03ae915af6
Andrew Abalakov <a.s.abalakov@ucre.ru>
2017-07-11 10:35:36 +0500

    Правка перевода

---
+++ bitrix/components/bitrix/crm.deal/templates/.default/lang/ru/widget.php
@@ -0,0 +1,26 @@
+<?
+$MESS["CRM_DEAL_WGT_PAGE_TITLE"] = "Аналитические отчёты по заявкам";
+$MESS["CRM_DEAL_WGT_PAGE_TITLE_SHORT"] = "Отчёты по заявкам";
+$MESS["CRM_DEAL_WGT_FUNNEL"] = "Воронка продаж по заявкам";
+$MESS["CRM_DEAL_WGT_SUM_DEAL_OVERALL"] = "Общая сумма заявок";
+$MESS["CRM_DEAL_WGT_SUM_DEAL_WON"] = "Сумма выигранных заявок";
+$MESS["CRM_DEAL_WGT_SUM_DEAL_IN_WORK"] = "Сумма заявок в работе";
+$MESS["CRM_DEAL_WGT_RATING"] = "Рейтинг по сумме выигранных заявок";
+$MESS["CRM_DEAL_WGT_SUM_INVOICE_OVERALL"] = "Сумма выставленных счетов";
+$MESS["CRM_DEAL_WGT_SUM_INVOICE_OWED"] = "Сумма невыставленных счетов";
+$MESS["CRM_DEAL_WGT_PAYMENT_CONTROL"] = "Контроль оплат выигранных заявок";
+$MESS["CRM_DEAL_WGT_DEAL_IN_WORK"] = "Заявки в работе";
+$MESS["CRM_DEAL_WGT_QTY_DEAL_IN_WORK"] = "Количество заявок в работе";
+$MESS["CRM_DEAL_WGT_QTY_CALL"] = "Количество звонков";
+$MESS["CRM_DEAL_WGT_QTY_ACTIVITY"] = "Количество дел";
+$MESS["CRM_DEAL_WGT_QTY_DEAL_IDLE"] = "Количество забытых заявок";
+$MESS["CRM_DEAL_WGT_EMPLOYEE_DEAL_IN_WORK"] = "Заявки в работе по сотрудникам";
+$MESS["CRM_DEAL_WGT_QTY_MEETING"] = "Количество встреч";
+$MESS["CRM_DEAL_WGT_QTY_EMAIL"] = "Количество писем";
+$MESS["CRM_DEAL_WGT_QTY_DEAL_WON"] = "Количество выигранных заявок";
+$MESS["CRM_DEAL_WGT_DEMO_TITLE"] = "Это демо-представление, вы можете скрыть его и получить аналитику по вашим заявкам.";
+$MESS["CRM_DEAL_WGT_DEMO_CONTENT"] = "Если у вас еще нет заявок, создайте <a href=\"#URL#\" class=\"#CLASS_NAME#\">первую заявку</a> прямо сейчас!";
+$MESS["CRM_DEAL_WGT_DEAL_CATEGORY"] = "Направление";
+$MESS["CRM_DEAL_WGT_NO_SELECTED"] = "не выбрано";
+$MESS["CRM_DEAL_WGT_CURRENT"] = "[текущее направление]";
+?>
