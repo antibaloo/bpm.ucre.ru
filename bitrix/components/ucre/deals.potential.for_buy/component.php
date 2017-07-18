49590436aa3b3e9c4a4a19a427f0708d9f4d6020
Andrew Abalakov <a.s.abalakov@ucre.ru>
2017-07-18 16:32:38 +0500

    Базовая структура компонента 'Потенциальные сделки для заявок на покупку'

---
+++ bitrix/components/ucre/deals.potential.for_buy/component.php
@@ -0,0 +1,5 @@
+<?php
+if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED!==true) die();
+CModule::IncludeModule("crm");
+$this->IncludeComponentTemplate();
+?>
---
+++ bitrix/components/ucre/deals.potential.for_buy/templates/.default/template.php
@@ -0,0 +1,5 @@
+<link href="/include/custom_css/custom_paging.css?<?=time();?>" rel="stylesheet">
+<?php
+if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED!==true)die();
+echo "Потенциальные сделки для заявок на покупку!";
+?>
