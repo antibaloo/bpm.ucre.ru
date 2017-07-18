fbccd560fa60be4265e709ef917f146207deb55d
Andrew Abalakov <a.s.abalakov@ucre.ru>
2017-07-18 15:04:17 +0500

    Изменены параметры вызова компонентов для отрисовки вкладки 'Потенциальные сделки'

--- ajax/potentials.php
+++ ajax/potentials.php
@@ -1,13 +1,27 @@
 <?php
 require_once($_SERVER['DOCUMENT_ROOT'] . "/bitrix/modules/main/include/prolog_before.php");
-if ($USER->GetID() == 24){
-  $APPLICATION->IncludeComponent(
-    "ucre:crm.deal.potentials",
-    "",
-    array('ID' => $_POST['id'], 'CATEGORY' => $_POST['category'], 'TABID' =>$_POST['tabid'], 'FILTER' => ($_POST['filter'])?$_POST['filter']:"new"),
-    false
-  );
-}else{
+if ($USER->GetID()!=24 && $USER->GetID()!=1){
   echo "<br><center><img style='margin: 0 auto;' src='/ajax/images/construction.png'></center>";
+}else{
+  echo "<pre>";
+  print_r($_POST);
+  echo "</pre>";
+  if ($_POST['category'] == 0 || $_POST['category'] == 4) {
+    $APPLICATION->IncludeComponent(
+      "ucre:select.potential.for_sell",
+      "",
+      array('ID' => $_POST['id']),
+      false
+    );
+  }elseif ($_POST['category'] == 2){
+    $APPLICATION->IncludeComponent(
+      "ucre:select.potential.for_buy",
+      "",
+      array('ID' => $_POST['id']),
+      false
+    );
+ }else {
+   echo "<h2>Для это категории заявок поиск встречных не производится!</h2>";
+ }
 }
 ?>
