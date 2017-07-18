997732d773fd6408e9b6f70d8208a418f3000613
Andrew Abalakov <a.s.abalakov@ucre.ru>
2017-07-18 15:22:22 +0500

    Меслкие правки

--- ajax/potentials.php
+++ ajax/potentials.php
@@ -3,19 +3,16 @@ require_once($_SERVER['DOCUMENT_ROOT'] . "/bitrix/modules/main/include/prolog_be
 if ($USER->GetID()!=24 && $USER->GetID()!=1){
   echo "<br><center><img style='margin: 0 auto;' src='/ajax/images/construction.png'></center>";
 }else{
-  echo "<pre>";
-  print_r($_POST);
-  echo "</pre>";
   if ($_POST['category'] == 0 || $_POST['category'] == 4) {
     $APPLICATION->IncludeComponent(
-      "ucre:select.potential.for_sell",
+      "ucre:deals.potential.for_sell",
       "",
       array('ID' => $_POST['id']),
       false
     );
   }elseif ($_POST['category'] == 2){
     $APPLICATION->IncludeComponent(
-      "ucre:select.potential.for_buy",
+      "ucre:deals.potential.for_buy",
       "",
       array('ID' => $_POST['id']),
       false
