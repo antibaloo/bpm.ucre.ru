d7e7fbc8ec543abc2b1fa6a0bc3f4b16ac158e2d
Andrew Abalakov <a.s.abalakov@ucre.ru>
2017-07-19 12:21:18 +0500

    Мелкие правки

--- bitrix/templates/bitrix24/header.php
+++ bitrix/templates/bitrix24/header.php
@@ -479,23 +479,28 @@ if(\Bitrix\Main\ModuleManager::isModuleInstalled('bitrix24'))
 										<?
 										$APPLICATION->ShowViewContent("above_pagetitle");
 										$APPLICATION->IncludeComponent(
-											"bitrix:menu",
-											"top_horizontal",
-											array(
-												"ROOT_MENU_TYPE" => "left",
-												"MENU_CACHE_TYPE" => "N",
-												"MENU_CACHE_TIME" => "604800",
-												"MENU_CACHE_USE_GROUPS" => "N",
-												"MENU_CACHE_USE_USERS" => "Y",
-												"CACHE_SELECTED_ITEMS" => "N",
-												"MENU_CACHE_GET_VARS" => array(),
-												"MAX_LEVEL" => "1",
-												"USE_EXT" => "Y",
-												"DELAY" => "N",
-												"ALLOW_MULTI_SELECT" => "N"
-											),
-											false
-										);
+	"bitrix:menu", 
+	"top_horizontal", 
+	array(
+		"ROOT_MENU_TYPE" => "left",
+		"MENU_CACHE_TYPE" => "A",
+		"MENU_CACHE_TIME" => "604800",
+		"MENU_CACHE_USE_GROUPS" => "N",
+		"MENU_CACHE_USE_USERS" => "Y",
+		"CACHE_SELECTED_ITEMS" => "N",
+		"MENU_CACHE_GET_VARS" => array(
+		),
+		"MAX_LEVEL" => "1",
+		"USE_EXT" => "Y",
+		"DELAY" => "N",
+		"ALLOW_MULTI_SELECT" => "N",
+		"COMPONENT_TEMPLATE" => "top_horizontal",
+		"CHILD_MENU_TYPE" => "left",
+		"COMPOSITE_FRAME_MODE" => "A",
+		"COMPOSITE_FRAME_TYPE" => "AUTO"
+	),
+	false
+);
 										?>
 
 										<div class="pagetitle-wrap">
