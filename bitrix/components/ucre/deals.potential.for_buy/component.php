383b2d512836e29bc9c52ce727d3e761ab05f86a
Andrew Abalakov <a.s.abalakov@ucre.ru>
2017-07-25 10:54:42 +0500

    Добавлены параметры id заявки на покупку и пусть до папки компонента

--- bitrix/components/ucre/deals.potential.for_buy/component.php
+++ bitrix/components/ucre/deals.potential.for_buy/component.php
@@ -1,5 +1,7 @@
 <?php
 if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED!==true) die();
 CModule::IncludeModule("crm");
+$arResult['ID'] = $arParams['ID'];
+$arResult['COMPONENT_PATH'] = $this->GetPath();
 $this->IncludeComponentTemplate();
 ?>
