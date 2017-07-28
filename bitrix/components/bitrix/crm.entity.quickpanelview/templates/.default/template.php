0659757d33a80db7351dacebbcfce69732735c77
Andrew Abalakov <a.s.abalakov@ucre.ru>
2017-07-28 17:57:01 +0500

    Всплывающая подсказка с именем автора заявки, необходима при смене ответственного

--- bitrix/components/bitrix/crm.entity.quickpanelview/templates/.default/template.php
+++ bitrix/components/bitrix/crm.entity.quickpanelview/templates/.default/template.php
@@ -407,7 +407,7 @@ if(!function_exists('__CrmQuickPanelViewRenderSection'))
 						$title = $arResult['HEAD_TITLE'];
 						$headerConfig['TITLE'] = array('fieldId' => $arResult['HEAD_TITLE_FIELD_ID']);
 						?><div id="<?="{$guid}_title"?>" class="crm-lead-header-title">
-						 <?if ($entityTypeID === CCrmOwnerType::Deal) echo "<span style='color: blue; font-size: 18px; vertical-align: middle;'>".DealCategory::getName($entityFields['CATEGORY_ID']).": </span>";?>
+						 <?if ($entityTypeID === CCrmOwnerType::Deal) echo "<span style='color: blue; font-size: 18px; vertical-align: middle;' title='Заявку создал: ".$entityFields['CREATED_BY_NAME']." ".$entityFields['CREATED_BY_LAST_NAME']."'>".DealCategory::getName($entityFields['CATEGORY_ID']).": </span>";?>
 							<span class="crm-lead-header-title-text"><?=$title?></span>
 						<span class="crm-lead-header-title-edit-wrapper" style="display: none;"></span>
 							<div class="crm-lead-header-title-edit"></div>
