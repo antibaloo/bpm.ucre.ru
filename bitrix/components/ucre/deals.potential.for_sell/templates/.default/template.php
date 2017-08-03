7177bbfa4bbcd9dcebe769166de8ca983743d314
Andrew Abalakov <a.s.abalakov@ucre.ru>
2017-08-03 10:09:38 +0500

    Мелкие правки

--- bitrix/components/ucre/deals.potential.for_sell/templates/.default/template.php
+++ bitrix/components/ucre/deals.potential.for_sell/templates/.default/template.php
@@ -1,5 +1,5 @@
 <link href="/include/custom_css/custom_paging.css?<?=time();?>" rel="stylesheet">
 <?php
 if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED!==true)die();
-echo "Потенциальные сделки для заявок на продажу!";
 ?>
+<h2>Компонент потенциальных сделок для заявок на продажу и новостроек находится в разработке!</h2>
