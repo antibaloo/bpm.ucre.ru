08de6ed5dc0a25e76540947876bc4844a560adcd
Andrew Abalakov <a.s.abalakov@ucre.ru>
2017-08-03 10:08:50 +0500

    Подключены новые компоненты по встречным заявкам и потенциальным сделкам

--- ajax/potentials.php
+++ ajax/potentials.php
@@ -1,24 +1,20 @@
 <?php
 require_once($_SERVER['DOCUMENT_ROOT'] . "/bitrix/modules/main/include/prolog_before.php");
-if ($USER->GetID()!=24 && $USER->GetID()!=1){
-  echo "<br><center><img style='margin: 0 auto;' src='/ajax/images/construction.png'></center>";
-}else{
-  if ($_POST['category'] == 0 || $_POST['category'] == 4) {
-    $APPLICATION->IncludeComponent(
-      "ucre:deals.potential.for_sell",
-      "",
-      array('ID' => $_POST['id']),
-      false
-    );
-  }elseif ($_POST['category'] == 2){
-    $APPLICATION->IncludeComponent(
-      "ucre:deals.potential.for_buy",
-      "",
-      array('ID' => $_POST['id']),
-      false
-    );
- }else {
-   echo "<h2>Для это категории заявок поиск встречных не производится!</h2>";
- }
+if ($_POST['category'] == 0 || $_POST['category'] == 4) {
+  $APPLICATION->IncludeComponent(
+    "ucre:deals.potential.for_sell",
+    "",
+    array('ID' => $_POST['id']),
+    false
+  );
+}elseif ($_POST['category'] == 2){
+  $APPLICATION->IncludeComponent(
+    "ucre:deals.potential.for_buy",
+    "",
+    array('ID' => $_POST['id']),
+    false
+  );
+}else {
+  echo "<h2>Для это категории заявок поиск встречных не производится!</h2>";
 }
 ?>
--- ajax/selection.php
+++ ajax/selection.php
@@ -1,29 +1,20 @@
 <?php
 require_once($_SERVER['DOCUMENT_ROOT'] . "/bitrix/modules/main/include/prolog_before.php");
-if ($USER->GetID()!=24 && $USER->GetID()!=1){
+if ($_GET['category'] == 0 || $_GET['category'] == 4) {
   $APPLICATION->IncludeComponent(
-    "ucre:select.request",
+    "ucre:select.relevant.for_sell",
     "",
-    array('ID' => $_GET['id'], 'CATEGORY' => $_GET['category']),
+    array('ID' => $_GET['id']),
     false
   );
-}else{
- if ($_GET['category'] == 0 || $_GET['category'] == 4) {
-   $APPLICATION->IncludeComponent(
-     "ucre:select.relevant.for_sell",
-     "",
-     array('ID' => $_GET['id']),
-     false
-   );
- }elseif ($_GET['category'] == 2){
-   $APPLICATION->IncludeComponent(
-     "ucre:select.relevant.for_buy",
-     "",
-     array('ID' => $_GET['id']),
-     false
-   );
- }else {
-   echo "<h2>Для это категории заявок поиск встречных не производится!</h2>";
- }
+}elseif ($_GET['category'] == 2){
+  $APPLICATION->IncludeComponent(
+    "ucre:select.relevant.for_buy",
+    "",
+    array('ID' => $_GET['id']),
+    false
+  );
+}else {
+  echo "<h2>Для это категории заявок поиск встречных не производится!</h2>";
 }
 ?>
