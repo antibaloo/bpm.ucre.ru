5b62f846779f808a89a2848b5862439c252acf39
Andrew Abalakov <a.s.abalakov@ucre.ru>
2017-08-03 12:49:57 +0500

    Переделано отображение параметров поиска встречных заявок

--- bitrix/components/ucre/select.relevant.for_buy/templates/.default/template.php
+++ bitrix/components/ucre/select.relevant.for_buy/templates/.default/template.php
@@ -5,19 +5,45 @@ if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED!==true)die();
 ?>
 <br>
 <form id="select_relevant_to_buy">
-  Рынок поиска: <?=$arResult['SELECT_PARAMS']['MARKET']?>&nbsp;
-  Тип объекта: <?=$arResult['SELECT_PARAMS']['TYPE']?>&nbsp;
-  N<sub>комнат</sub> от <?=$arResult['SELECT_PARAMS']['ROOMS']?>&nbsp;
-  S<sub>общ.</sub> от <?=$arResult['SELECT_PARAMS']['TOTAL_AREA']?>&nbsp;
-  Этажи: <?=($arResult['SELECT_PARAMS']['FIRST'])?$arResult['SELECT_PARAMS']['FIRST']:"можно первый"?>&nbsp;<?=($arResult['SELECT_PARAMS']['LAST'])?$arResult['SELECT_PARAMS']['LAST']:"можно последний"?>&nbsp;
-  Цена от <?=($arResult['SELECT_PARAMS']['MINPRICE'])?$arResult['SELECT_PARAMS']['MINPRICE']:"-"?> до <?=($arResult['SELECT_PARAMS']['MAXPRICE'])?$arResult['SELECT_PARAMS']['MAXPRICE']:"-"?> &nbsp;
-  <input id="submit" type="button" value="Искать">
+  <table style="width:100%;border-collapse: collapse;margin-bottom:15px;">
+    <tr style="border: 1px solid lightgrey">
+      <td style="text-align:right;width:25%"><b>Рынок поиска:</b></td>
+      <td style="text-align:left;width:25%"><?=$arResult['SELECT_PARAMS']['MARKET']?></td>
+      <td style="text-align:right;width:25%"><b>Тип объекта:</b></td>
+      <td style="text-align:left;width:25%"><?=$arResult['SELECT_PARAMS']['TYPE']?></td>
+    </tr>
+    <tr style="border: 1px solid lightgrey">
+      <td style="text-align:right;width:25%"><b>N<sub>комнат</sub> от:</b></td>
+      <td style="text-align:left;width:25%"><?=$arResult['SELECT_PARAMS']['ROOMS']?></td>
+      <td style="text-align:right;width:25%"><b>S<sub>общ.</sub> от:</b></td>
+      <td style="text-align:left;width:25%"><?=$arResult['SELECT_PARAMS']['TOTAL_AREA']?></td>
+    </tr>
+    <tr style="border: 1px solid lightgrey">
+      <td style="text-align:right;width:25%"><b>S<sub>кух.</sub> от:</b></td>
+      <td style="text-align:left;width:25%"><?=$arResult['SELECT_PARAMS']['KITCHEN_AREA']?></td>
+      <td style="text-align:right;width:25%"></td>
+      <td style="text-align:left;width:25%"></td>
+    </tr>
+    <tr style="border: 1px solid lightgrey">
+      <td style="text-align:right;width:25%"><b>1-й этаж:</b></td>
+      <td style="text-align:left;width:25%"><?=($arResult['SELECT_PARAMS']['FIRST'])?$arResult['SELECT_PARAMS']['FIRST']:"можно первый"?></td>
+      <td style="text-align:right;width:25%"><b>N-й этаж:</b></td>
+      <td style="text-align:left;width:25%"><?=($arResult['SELECT_PARAMS']['LAST'])?$arResult['SELECT_PARAMS']['LAST']:"можно последний"?></td>
+    </tr>
+    <tr style="border: 1px solid lightgrey">
+      <td style="text-align:right;width:25%"><b>Цена от:</b></td>
+      <td style="text-align:left;width:25%"><?=$arResult['SELECT_PARAMS']['MINPRICE']?></td>
+      <td style="text-align:right;width:25%"><b>Цена до:</b></td>
+      <td style="text-align:left;width:25%"><?=$arResult['SELECT_PARAMS']['MAXPRICE']?></td>
+    </tr>
+  </table>
+  <center><input id="submit" type="button" value="Искать"></center>
   
   <input type="hidden" name="sql" value="<?=bin2hex($arResult['SQL_STRING'])?>">
   <input type="hidden" name="deal_id" value="<?=$arResult['ID']?>">
   <input type="hidden" name="assigned_by_id" value="<?=$arResult['ASSIGNED_BY_ID']?>">
 </form>
-
+<hr>
 <div id="resultGrid">
 <?
   $currentUserCount = $DB->Query("select * from b_crm_relevant_search where deal_id=".$arResult['ID']." AND user_id=".$USER->GetID())->SelectedRowsCount();
