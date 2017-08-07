22256bb82e73320aad865ef19ab493164120489a
Andrew Abalakov <a.s.abalakov@ucre.ru>
2017-08-07 09:23:50 +0500

    В резюме объекта типа дом или таунхаус добавлено кол-во комнат

--- bitrix/components/ucre/deals.potential.for_buy/ajax.php
+++ bitrix/components/ucre/deals.potential.for_buy/ajax.php
@@ -3,7 +3,7 @@ require_once($_SERVER['DOCUMENT_ROOT'] . "/bitrix/modules/main/include/prolog_be
 if (strripos ($_SERVER['HTTP_REFERER'], 'bpm.ucre.ru')!==false){
   $type_s = array(
       '383' =>  "Дом",
-      '384' =>  "Таунхаус",
+      '384' =>  "таунхаус",
       '385' =>  "Дача",
     );
   $rsData = $DB->Query("select b_crm_potential_deals.sell_deal_id, b_crm_potential_deals.price, b_crm_potential_deals.object, b_crm_potential_deals.access, b_crm_potential_deals.yard, b_crm_potential_deals.infra, b_crm_potential_deals.comment, b_uts_crm_deal.UF_CRM_58958B5734602, b_iblock_element_prop_s42.PROPERTY_210, b_iblock_element_prop_s42.PROPERTY_215, b_iblock_element_prop_s42.PROPERTY_217, b_iblock_element_prop_s42.PROPERTY_218, b_iblock_element_prop_s42.PROPERTY_221, b_iblock_element_prop_s42.PROPERTY_222, b_iblock_element_prop_s42.PROPERTY_224, b_iblock_element_prop_s42.PROPERTY_225, b_iblock_element_prop_s42.PROPERTY_226, b_iblock_element_prop_s42.PROPERTY_229, b_iblock_element_prop_s42.PROPERTY_243 from b_crm_potential_deals inner join b_uts_crm_deal ON b_crm_potential_deals.sell_deal_id=b_uts_crm_deal.VALUE_ID INNER JOIN b_iblock_element ON b_uts_crm_deal.UF_CRM_1469534140 = b_iblock_element.ID INNER JOIN b_iblock_element_prop_s42 ON b_iblock_element.ID = b_iblock_element_prop_s42.IBLOCK_ELEMENT_ID where buy_deal_id=".$_POST['id']." AND result='".$_POST['filter']."'");
@@ -93,6 +93,8 @@ if (strripos ($_SERVER['HTTP_REFERER'], 'bpm.ucre.ru')!==false){
             break;
           case 383:
           case 384:
+            $resume = number_format($aRes['PROPERTY_229'],0)."-к ".$type_s[$aRes['PROPERTY_210']]." площадью ".number_format($aRes['PROPERTY_224'],2)." м<sup>2</sup> на участке в ".number_format($aRes['PROPERTY_292'],2)." сот, ".$wallsTypeValue.", ".$city.", ".$street;
+            break;
           case 385:
             $resume = $type_s[$aRes['PROPERTY_210']]." площадью ".number_format($aRes['PROPERTY_224'],2)." м<sup>2</sup> на участке в ".number_format($aRes['PROPERTY_292'],2)." сот, ".$wallsTypeValue.", ".$city.", ".$street;
             break;
