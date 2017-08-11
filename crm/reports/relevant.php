863d046dc79203deba50f387ea9d7038eb6ffe35
Andrew Abalakov <a.s.abalakov@ucre.ru>
2017-08-11 15:01:57 +0500

    Отчет по количеству вызовов 'Встречных заявок' сотрудниками отдела продаж

--- crm/reports/relevant.php
+++ crm/reports/relevant.php
@@ -1,5 +1,37 @@
+<link href="/include/custom_css/custom_paging.css?<?=time();?>" rel="stylesheet">
 <?
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
+CModule::IncludeModule('intranet');
+require($_SERVER["DOCUMENT_ROOT"]."/include/reports/functions.php");
 $APPLICATION->SetTitle("Отчет по встречным заявкам");
+$departments = explode("|", GetSubStructure(55));
+$emploees = array();
+$structure = CIntranetUtils::GetStructure();
+if (is_array($departments)){
+  foreach ($departments as $department){
+    $emploees = array_merge($emploees, $structure['DATA'][$department]['EMPLOYEES']);
+  }
+}else {
+  $emploees = $structure['DATA'][$departments]['EMPLOYEES'];
+}
 ?>
+<div class="page active">
+  <table>
+    <tr>
+      <th>id</th>
+      <th>Фио</th>
+      <th>Вызовы</th>
+    </tr>
+<?    
+foreach ($emploees as $emploee){
+  $arUser = $USER->GetById($emploee)->Fetch();
+?>
+    <tr class="row">
+      <td><?=$arUser['ID']?></td>
+      <td><?=$arUser['LAST_NAME']." ".$arUser['NAME']." ".$arUser['SECOND_NAME']?></td>
+      <td><?=$DB->Query("select * from b_crm_relevant_search where user_id=".$arUser['ID'])->SelectedRowsCount();?></td>
+    </tr>  
+<?}?>
+  </table>
+</div>
 <?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
