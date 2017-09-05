a7a008bda1b458a7231c63b7ea0ad31d43f74fd7
Andrew Abalakov <a.s.abalakov@ucre.ru>
2017-09-05 11:36:05 +0500

    Интерактивный тест-юнит динамической формы ввода дял новых лидов

--- crm/lead_ucre/index.php
+++ crm/lead_ucre/index.php
@@ -1,44 +1,36 @@
 <?
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
+IncludeModuleLangFile($_SERVER["DOCUMENT_ROOT"]."/bitrix/modules/intranet/public/crm/reports/index.php");
+$APPLICATION->SetTitle("Новые лиды");
 ?>
-<?$APPLICATION->IncludeComponent(
-	"ucre:crm.lead", 
-	"", 
-	Array(
-	"SEF_MODE" => "Y",	// Включить поддержку ЧПУ
-		"PATH_TO_CONTACT_SHOW" => "/crm/contact/show/#contact_id#/",
-		"PATH_TO_CONTACT_EDIT" => "/crm/contact/edit/#contact_id#/",
-		"PATH_TO_COMPANY_SHOW" => "/crm/company/show/#company_id#/",
-		"PATH_TO_COMPANY_EDIT" => "/crm/company/edit/#company_id#/",
-		"PATH_TO_DEAL_SHOW" => "/crm/deal/show/#deal_id#/",
-		"PATH_TO_DEAL_EDIT" => "/crm/deal/edit/#deal_id#/",
-		"PATH_TO_USER_PROFILE" => "/company/personal/user/#user_id#/",
-		"PATH_TO_PRODUCT_EDIT" => "/crm/product/edit/#product_id#/",
-		"PATH_TO_PRODUCT_SHOW" => "/crm/product/show/#product_id#/",
-		"ELEMENT_ID" => $_REQUEST["lead_id"],	// ID лида
-		"SEF_FOLDER" => "/crm/lead_ucre/",	// Каталог ЧПУ (относительно корня сайта)
-		"SEF_URL_TEMPLATES" => array(
-			"index" => "index.php",
-			"list" => "list/",
-			"edit" => "edit/#lead_id#/",
-			"show" => "show/#lead_id#/",
-			"convert" => "convert/#lead_id#/",
-			"import" => "import/",
-			"service" => "service/",
-			"dedupe" => "dedupe/",
-		),
-		"VARIABLE_ALIASES" => array(
-			"index" => "",
-			"list" => "",
-			"edit" => "",
-			"show" => "",
-			"convert" => "",
-			"import" => "",
-			"service" => "",
-			"dedupe" => "",
-		)
-	),
-	false
-);
-?>
-<?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
+	<style>
+	</style>
+	<div style="margin:0 auto; width: 50%; padding: 5px; background-color: #eeeeee;">
+		<form>
+			<div id="leadGoal">
+
+			</div>
+			<div id="roType">
+				
+			</div>
+			<div id="roProperty">
+				
+			</div>
+			<button id="resetForm" type="button">
+				Сбросить форму
+			</button>
+		</form>
+	</div>
+<script>
+	$(document).ready(function(){
+		$("#leadGoal").load("./templates/leadgoal.html");
+		$("#roType").load("./templates/rotype.html");
+		$("#roProperty").load("./templates/roproperty.html");
+	});
+	$("#resetForm").click(function(){
+		$("#leadGoal").load("./templates/leadgoal.html");
+		$("#roType").load("./templates/rotype.html");
+		$("#roProperty").load("./templates/roproperty.html");
+	});
+</script>
+	<?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
