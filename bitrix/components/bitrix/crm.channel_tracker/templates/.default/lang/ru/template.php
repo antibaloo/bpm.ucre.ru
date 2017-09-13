8b88c6d4bfd5cf4c3f74b681bdbc542c40bce838
Andrew Abalakov <a.s.abalakov@ucre.ru>
2017-09-13 17:24:05 +0500

    Обновление

--- bitrix/components/bitrix/crm.channel_tracker/templates/.default/lang/ru/template.php
+++ bitrix/components/bitrix/crm.channel_tracker/templates/.default/lang/ru/template.php
@@ -1,14 +1,35 @@
 <?
-$MESS["CRM_CH_TRACKER_MINIMIZE"] = "Свернуть";
-$MESS["CRM_CH_TRACKER_MAXIMIZE"] = "Развернуть";
-$MESS["CRM_CH_TRACKER_CONNECT"] = "Подключить";
 $MESS["CRM_CH_TRACKER_WGT_DEMO_TITLE"] = "Это демо-представление, вы можете скрыть его и получить аналитику по вашим данным.";
-$MESS["CRM_CH_TRACKER_WGT_ACTIVITY_QUANTITY"] = "Количество дел";
-$MESS["CRM_CH_TRACKER_WGT_DEAL_SUCCESS_SUM"] = "Сумма выигранных заявок";
-$MESS["CRM_CH_TRACKER_WGT_DEAL_PROCESS_SUM"] = "Сумма сдезаявоклок в работе";
+$MESS["CRM_CH_TRACKER_WGT_ACTIVITY_DYNAMIC"] = "Контроль коммуникаций";
+$MESS["CRM_CH_TRACKER_WGT_COMMUNICATION_DYNAMIC"] = "Динамика коммуникационной нагрузки";
+$MESS["CRM_CH_TRACKER_WGT_COMMUNICATION_DYNAMIC_TELEPHONY"] = "Телефония";
+$MESS["CRM_CH_TRACKER_WGT_COMMUNICATION_DYNAMIC_EMAIL"] = "E-mail";
+$MESS["CRM_CH_TRACKER_WGT_COMMUNICATION_DYNAMIC_OPENLINES"] = "Мессенджеры и соц. сети";
+$MESS["CRM_CH_TRACKER_WGT_COMMUNICATION_DYNAMIC_FORM"] = "CRM - формы";
+$MESS["CRM_CH_TRACKER_WGT_PERSONAL_LOAD"] = "Нагрузка по менеджерам";
+$MESS["CRM_CH_TRACKER_WGT_PERSONAL_LOAD_TELEPHONY"] = "Телефония";
+$MESS["CRM_CH_TRACKER_WGT_PERSONAL_LOAD_EMAIL"] = "E-mail";
+$MESS["CRM_CH_TRACKER_WGT_PERSONAL_LOAD_OPENLINES"] = "Мессенджеры и соц. сети";
+$MESS["CRM_CH_TRACKER_WGT_PERSONAL_LOAD_FORM"] = "CRM - формы";
+$MESS["CRM_CH_TRACKER_WGT_DEAL_SUCCESS_SUM_PER_MONTH"] = "Продажи за месяц";
+$MESS["CRM_CH_TRACKER_WGT_DEAL_SUCCESS_SUM_PER_DAY"] = "Продажи за сегодня";
+$MESS["CRM_CH_TRACKER_WGT_DEAL_SUCCESS_SUM"] = "Сумма продаж";
+$MESS["CRM_CH_TRACKER_WGT_NEW_CLIENTS"] = "Новые клиенты";
+$MESS["CRM_CH_TRACKER_WGT_NEW_CLIENTS_PER_DAY"] = "Новые клиенты за сегодня";
+$MESS["CRM_CH_TRACKER_WGT_MANAGERS_FAILS"] = "Счетчики по менеджерам";
+$MESS["CRM_CH_TRACKER_WGT_MANAGERS_SUCCESSES"] = "Вклад менеджеров в продажи";
+$MESS["CRM_CH_TRACKER_WGT_DEAL_PROCESS_SUM"] = "Сделки в работе";
 $MESS["CRM_CH_TRACKER_WGT_AMOUNT_OF_SALE"] = "Сумма продаж";
-$MESS["CRM_CH_TRACKER_WGT_DEAL_IN_WORK_BY_EMPLOYEE"] = "Заявки в работе по сотрудникам";
-$MESS["CRM_CH_TRACKER_WGT_RATING_BY_SUCCESSFUL_DEALS"] = "Рейтинг по сумме выигранных заявок";
-$MESS["CRM_CH_TRACKER_HELP_POPUP_TITLE"] = "Контроль продаж компании";
-$MESS["CRM_CH_TRACKER_HELP_POPUP_CONTENT"] = "<p>Каждая колонка отчета имеет свои особенности фильтрации по отчетному периоду.</p><p>Колонки \"Контроль входящих/исходящих\" и \"Продажи за период\" строятся строго по отчетному периоду: месяц, год, квартал и т.д.</p><p>Колонки \"Лиды ждут обработки\" и \"Заявок в работе\" выбирают данные на конец указанного периода.</p><p>Если выбрать заявки в работе за прошлый месяц, это будут все заявки, которые к последней дате прошлого месяца находились в работе, начало периода в этом случае не учитывается.</p><p>Данные в этом отчете собираются только с момента появления СтартCRM в вашем Битрикс24. За предыдущий период отчет не будет построен.</p>";
+$MESS["CRM_CH_TRACKER_WGT_RATING_BY_SUCCESSFUL_DEALS"] = "Рейтинг по сумме выигранных сделок";
+$MESS["CRM_CH_TRACKER_WGT_ANIMATION_1"] = "Вы можете в любой момент вернуться к обзорному видео, кликнув на значок.";
+$MESS["CRM_CH_TRACKER_WGT_ANIMATION_2"] = "Запускаете CRM?<br><br>
+Возьмите под контроль каналы, через которые клиенты обращаются к вам: подключите к CRM Телефонию, Электронную почту, Открытые линии, CRM-формы.<br><br>
+Чем больше каналов вы подключите для работы с вашими клиентами, тем больше сделок будет создано и доведено до успеха.";
+$MESS["CRM_CH_TRACKER_WGT_ANIMATION_3_SUPERVISOR"] = "Хотите держать руку на пульсе вашей CRM системы? Контролировать ваших менеджеров?<br><br> 
+Тогда этот отчет для вас! Счетчики показывают текущую работу менеджеров с клиентами – CRM покажет вам количество выполненных дел и сколько осталось задач на текущий день.<br><br>
+Цель ваших менеджеров уменьшать количество счетчиков, чтобы к концу рабочего дня не осталось ни одного! Это значит, они все делают вовремя и ни один клиент не останется без внимания. А это — залог доведенных до успеха сделок. ";
+$MESS["CRM_CH_TRACKER_WGT_ANIMATION_3_EMPLOYEE"] = "Хотите быть успешным менеджером и с легкостью выполнять план продаж?<br><br>
+Тогда эти счетчики для вас! Счетчики показывают вашу текущую работу с клиентами – CRM сама подсказывает вам количество выполненных дел и сколько осталось задач на текущий день.<br><br> 
+Уменьшайте количество счетчиков, чтобы к концу рабочего дня у вас не осталось ни одного! Это значит, вы все делаете вовремя и ни один клиент не останется без внимания. А это — залог доведенных до успеха сделок.";
+$MESS["CRM_CH_TRACKER_WGT_ANIMATION_4"] = "В любой момент посмотрите демо-режим.";
 ?>
