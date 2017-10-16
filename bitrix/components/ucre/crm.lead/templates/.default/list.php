ca22035965e4409baac54e93ad0218c337ec7b11
Andrew Abalakov <a.s.abalakov@ucre.ru>
2017-10-16 08:37:06 +0500

    Переезд

--- bitrix/components/ucre/crm.lead/templates/.default/list.php
+++ bitrix/components/ucre/crm.lead/templates/.default/list.php
@@ -20,22 +20,6 @@ $APPLICATION->IncludeComponent(
 	array(
 		'ID' => 'LEAD_LIST',
 		'ACTIVE_ITEM_ID' => 'LEAD_UCRE',
-		'PATH_TO_COMPANY_LIST' => isset($arResult['PATH_TO_COMPANY_LIST']) ? $arResult['PATH_TO_COMPANY_LIST'] : '',
-		'PATH_TO_COMPANY_EDIT' => isset($arResult['PATH_TO_COMPANY_EDIT']) ? $arResult['PATH_TO_COMPANY_EDIT'] : '',
-		'PATH_TO_CONTACT_LIST' => isset($arResult['PATH_TO_CONTACT_LIST']) ? $arResult['PATH_TO_CONTACT_LIST'] : '',
-		'PATH_TO_CONTACT_EDIT' => isset($arResult['PATH_TO_CONTACT_EDIT']) ? $arResult['PATH_TO_CONTACT_EDIT'] : '',
-		'PATH_TO_DEAL_LIST' => isset($arResult['PATH_TO_DEAL_LIST']) ? $arResult['PATH_TO_DEAL_LIST'] : '',
-		'PATH_TO_DEAL_EDIT' => isset($arResult['PATH_TO_DEAL_EDIT']) ? $arResult['PATH_TO_DEAL_EDIT'] : '',
-		'PATH_TO_LEAD_LIST' => isset($arResult['PATH_TO_LEAD_LIST']) ? $arResult['PATH_TO_LEAD_LIST'] : '',
-		'PATH_TO_LEAD_EDIT' => isset($arResult['PATH_TO_LEAD_EDIT']) ? $arResult['PATH_TO_LEAD_EDIT'] : '',
-		'PATH_TO_QUOTE_LIST' => isset($arResult['PATH_TO_QUOTE_LIST']) ? $arResult['PATH_TO_QUOTE_LIST'] : '',
-		'PATH_TO_QUOTE_EDIT' => isset($arResult['PATH_TO_QUOTE_EDIT']) ? $arResult['PATH_TO_QUOTE_EDIT'] : '',
-		'PATH_TO_INVOICE_LIST' => isset($arResult['PATH_TO_INVOICE_LIST']) ? $arResult['PATH_TO_INVOICE_LIST'] : '',
-		'PATH_TO_INVOICE_EDIT' => isset($arResult['PATH_TO_INVOICE_EDIT']) ? $arResult['PATH_TO_INVOICE_EDIT'] : '',
-		'PATH_TO_REPORT_LIST' => isset($arResult['PATH_TO_REPORT_LIST']) ? $arResult['PATH_TO_REPORT_LIST'] : '',
-		'PATH_TO_DEAL_FUNNEL' => isset($arResult['PATH_TO_DEAL_FUNNEL']) ? $arResult['PATH_TO_DEAL_FUNNEL'] : '',
-		'PATH_TO_EVENT_LIST' => isset($arResult['PATH_TO_EVENT_LIST']) ? $arResult['PATH_TO_EVENT_LIST'] : '',
-		'PATH_TO_PRODUCT_LIST' => isset($arResult['PATH_TO_PRODUCT_LIST']) ? $arResult['PATH_TO_PRODUCT_LIST'] : ''
 	),
 	$component
 );
@@ -52,8 +36,8 @@ else
 		$this->SetViewTarget('below_pagetitle', 0);
 	}
 
-	?><div class="pagetitle-container" style="overflow: hidden;"><?
-	$APPLICATION->IncludeComponent(
+	?><div class="pagetitle-container" style="overflow: hidden;">Тут какбэ панелька со счетчиками<?
+	/*$APPLICATION->IncludeComponent(
 		'bitrix:crm.entity.counter.panel',
 		'',
 		array(
@@ -61,7 +45,7 @@ else
 			'EXTRAS' => array(),
 			'PATH_TO_ENTITY_LIST' => $arResult['PATH_TO_LEAD_LIST']
 		)
-	);
+	);*/
 	?></div><?
 
 	if($isBitrix24Template)
@@ -70,8 +54,8 @@ else
 	}
 
 	$APPLICATION->ShowViewContent('crm-grid-filter');
-
-	$APPLICATION->IncludeComponent(
+	?>Тут какбэ меню лидов<?
+	/*$APPLICATION->IncludeComponent(
 		'bitrix:crm.lead.menu',
 		'',
 		array(
@@ -85,25 +69,7 @@ else
 			'TYPE' => 'list'
 		),
 		$component
-	);
-
-	if(\Bitrix\Main\ModuleManager::isModuleInstalled('rest'))
-	{
-		$APPLICATION->IncludeComponent(
-			'bitrix:app.placement',
-			'menu',
-			array(
-				'PLACEMENT' => "CRM_LEAD_LIST_MENU",
-				"PLACEMENT_OPTIONS" => array(),
-				'INTERFACE_EVENT' => 'onCrmLeadListInterfaceInit',
-				'MENU_EVENT_MODULE' => 'crm',
-				'MENU_EVENT' => 'onCrmLeadListItemBuildMenu',
-			),
-			null,
-			array('HIDE_ICONS' => 'Y')
-		);
-	}
-
+	);*/
 	$APPLICATION->IncludeComponent(
 		'bitrix:crm.lead.list',
 		'',
--- bitrix/components/ucre/crm.lead/templates/.default/widget.php
+++ bitrix/components/ucre/crm.lead/templates/.default/widget.php
@@ -16,24 +16,7 @@ $APPLICATION->IncludeComponent(
 	'',
 	array(
 		'ID' => 'DEAL_WIDGET',
-		'ACTIVE_ITEM_ID' => 'LEAD',
-		'PATH_TO_COMPANY_LIST' => isset($arResult['PATH_TO_COMPANY_LIST']) ? $arResult['PATH_TO_COMPANY_LIST'] : '',
-		'PATH_TO_COMPANY_EDIT' => isset($arResult['PATH_TO_COMPANY_EDIT']) ? $arResult['PATH_TO_COMPANY_EDIT'] : '',
-		'PATH_TO_CONTACT_LIST' => isset($arResult['PATH_TO_CONTACT_LIST']) ? $arResult['PATH_TO_CONTACT_LIST'] : '',
-		'PATH_TO_DEAL_WIDGET' => isset($arResult['PATH_TO_DEAL_WIDGET']) ? $arResult['PATH_TO_DEAL_WIDGET'] : '',
-		'PATH_TO_DEAL_INDEX' => isset($arResult['PATH_TO_DEAL_INDEX']) ? $arResult['PATH_TO_DEAL_INDEX'] : '',
-		'PATH_TO_DEAL_LIST' => isset($arResult['PATH_TO_DEAL_LIST']) ? $arResult['PATH_TO_DEAL_LIST'] : '',
-		'PATH_TO_DEAL_EDIT' => isset($arResult['PATH_TO_DEAL_EDIT']) ? $arResult['PATH_TO_DEAL_EDIT'] : '',
-		'PATH_TO_LEAD_LIST' => isset($arResult['PATH_TO_LEAD_LIST']) ? $arResult['PATH_TO_LEAD_LIST'] : '',
-		'PATH_TO_LEAD_EDIT' => isset($arResult['PATH_TO_LEAD_EDIT']) ? $arResult['PATH_TO_LEAD_EDIT'] : '',
-		'PATH_TO_QUOTE_LIST' => isset($arResult['PATH_TO_QUOTE_LIST']) ? $arResult['PATH_TO_QUOTE_LIST'] : '',
-		'PATH_TO_QUOTE_EDIT' => isset($arResult['PATH_TO_QUOTE_EDIT']) ? $arResult['PATH_TO_QUOTE_EDIT'] : '',
-		'PATH_TO_INVOICE_LIST' => isset($arResult['PATH_TO_INVOICE_LIST']) ? $arResult['PATH_TO_INVOICE_LIST'] : '',
-		'PATH_TO_INVOICE_EDIT' => isset($arResult['PATH_TO_INVOICE_EDIT']) ? $arResult['PATH_TO_INVOICE_EDIT'] : '',
-		'PATH_TO_REPORT_LIST' => isset($arResult['PATH_TO_REPORT_LIST']) ? $arResult['PATH_TO_REPORT_LIST'] : '',
-		'PATH_TO_DEAL_FUNNEL' => isset($arResult['PATH_TO_DEAL_FUNNEL']) ? $arResult['PATH_TO_DEAL_FUNNEL'] : '',
-		'PATH_TO_EVENT_LIST' => isset($arResult['PATH_TO_EVENT_LIST']) ? $arResult['PATH_TO_EVENT_LIST'] : '',
-		'PATH_TO_PRODUCT_LIST' => isset($arResult['PATH_TO_PRODUCT_LIST']) ? $arResult['PATH_TO_PRODUCT_LIST'] : ''
+		'ACTIVE_ITEM_ID' => 'LEAD_UCRE',
 	),
 	$component
 );
