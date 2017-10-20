8c9389bd96d1760381275a97270c377f7d943b85
Andrew Abalakov <a.s.abalakov@ucre.ru>
2017-10-20 15:05:57 +0500

    +Свои пункты меню

--- bitrix/components/bitrix/crm.control_panel/component.php
+++ bitrix/components/bitrix/crm.control_panel/component.php
@@ -245,6 +245,45 @@ if($isAdmin || CCrmLead::CheckReadPermission(0, $userPermissions))
 	);
 }
 
+$actions = array();
+ if($isAdmin){
+ 	$createUrl = CComponentEngine::MakePathFromTemplate(
+ 				/*$arParams['PATH_TO_LEAD_EDIT']*/'/crm/lead_ucre/edit/#lead_id#/',
+ 				array('lead_id' => 0)
+ 			);
+ 	$actions[] = array('ID' => 'CREATE', 'URL' => $createUrl);
+ 	$stdItems['LEAD_UCRE'] = array(
+ 		'ID' => 'LEAD_UCRE',
+ 		'MENU_ID' => 'menu_crm_lead_ucre',
+ 		'NAME' => "Новые лиды",
+ 		'TITLE' => "Новые лиды",
+ 		'URL' => "/crm/lead_ucre/",
+ 		'ICON' => 'lead',
+ 		'COUNTER' => $counter->getValue(),
+ 		'COUNTER_ID' => $counter->getCode(),
+ 		'ACTIONS' => $actions,
+ 		'IS_DISABLED' => !\Bitrix\Crm\Settings\LeadSettings::isEnabled()
+ 	);
+ }
+ $actions = array();
+ if($isAdmin){
+ 	$createUrl = CComponentEngine::MakePathFromTemplate(
+ 				/*$arParams['PATH_TO_LEAD_EDIT']*/'/crm/client_deal/edit/#cdeal_id#/',
+ 				array('cdeal_id' => 0)
+ 			);
+ 	$actions[] = array('ID' => 'CREATE', 'URL' => $createUrl);
+ 	$stdItems['CDEAL_UCRE'] = array(
+ 		'ID' => 'CDEAL_UCRE',
+ 		'MENU_ID' => 'menu_crm_cdeal_ucre',
+ 		'NAME' => "Сделки",
+ 		'TITLE' => "Сделки",
+ 		'URL' => "/crm/client_deal/",
+ 		'ICON' => 'deal',
+ 		'COUNTER' => $counter->getValue(),
+ 		'COUNTER_ID' => $counter->getCode(),
+ 		'ACTIONS' => $actions
+ 	);
+ }
 
 if($isAdmin || CCrmDeal::CheckReadPermission(0, $userPermissions))
 {
