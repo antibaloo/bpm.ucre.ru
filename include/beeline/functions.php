86bf4580afa246a748951cf67e055820030b67a6
Andrew Abalakov <a.s.abalakov@ucre.ru>
2017-10-25 17:11:20 +0500

    Набор функция для обработки сообщений от ОАТС Билайн. Добавлена функция конвертирования xml соощения о событии ОАТС в JSON или ассоциированный масиив (второй параметр - true)

---
+++ include/beeline/functions.php
@@ -0,0 +1,31 @@
+<?
+function xmlToJSON($xmlString, $assoc = false){
+  $reader = new XMLReader();
+  $reader->XML($xmlString);
+  $jsonString = "{";
+  while ($reader->read()) {
+    switch ($reader->nodeType){
+      case 1:
+        $jsonString .= '"'.$reader->localName.'": {';
+        if ($reader->hasAttributes){
+          while($reader->moveToNextAttribute()){
+            $jsonString .= '"'.$reader->localName.'": "'.$reader->value.'",';
+          }
+        }
+        break;
+      case 3:
+        $jsonString .= '"'.value.'": "'.$reader->value.'"';
+        break;
+      case 15:
+        if ($lastType == 15) $jsonString = mb_substr($jsonString, 0, -1);
+        $jsonString .= '},';
+        break;
+    }
+    $lastType = $reader->nodeType;
+  }
+  $reader->close();
+  $jsonString = mb_substr($jsonString, 0, -1);
+  $jsonString .= "}";
+  return json_decode($jsonString, $assoc);
+}
+?>
