42b0b90b65c9d2e58a1d62e57e12ee27dfcc0e4d
Andrew Abalakov <a.s.abalakov@ucre.ru>
2017-11-08 11:48:08 +0500

    Комплексный компонент список зданий с постраничкой

---
+++ bitrix/components/ucre/building.list/templates/.default/template.php
@@ -0,0 +1,52 @@
+<?php
+if(!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED!==true) die();
+?>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+<pre>
+<?//print_r($arResult);?>
+</pre>
+<form id="buildingList" method="POST" action="<?=$_SERVER["REQUEST_URI"]?>" enctype="multipart/formdata">
+  <div class="gridWrapper">
+    <div class="gridHeader textCenter"></div>
+    <div class="gridHeader textCenter">ID</div>
+    <div class="gridHeader textCenter">Address</div>
+    <div class="gridHeader textCenter">kadNamber</div>
+    <div class="gridHeader textCenter">Square</div>
+    <div class="gridHeader textCenter">placementCount</div>
+    <div class="gridHeader textCenter">liveCount</div>
+    <div class="gridHeader textCenter">minFloors</div>
+    <div class="gridHeader textCenter">maxFloors</div>
+    <div class="gridHeader textCenter">accessCount</div>
+    <?foreach ($arResult['DATA'] as $building){?>
+    <div class="gridCell textCenter">
+      <a href="/townbase/building/show/<?=$building['ID']?>/"><i class="fa fa-eye" title="Смотреть"></i></a>&nbsp;
+      <a href="/townbase/building/edit/<?=$building['ID']?>/"><i class="fa fa-pencil-square-o" title="Редактировать"></i></a>&nbsp;
+      <a href="/townbase/building/chess/<?=$building['ID']?>/"><i class="fa fa-table" aria-hidden="true" title="Шахматка"></i></a>
+    </div>
+    <div class="gridCell textCenter"><?=$building['ID']?></div>
+    <div class="gridCell textRight"><?=$building['UF_BUILDING_ADDRESS']?></div>
+    <div class="gridCell textCenter"><?=$building['UF_BUILDING_KAD_NUM']?></div>
+    <div class="gridCell textRight"><?=$building['UF_B_SQUARE']?></div>
+    <div class="gridCell textRight"><?=$building['UF_PLACEMENT_COUNT']?></div>
+    <div class="gridCell textRight"><?=$building['UF_LIVE_COUNT']?></div>
+    <div class="gridCell textRight"><?=$building['UF_FLOORS_MIN']?></div>
+    <div class="gridCell textRight"><?=$building['UF_FLOORS_MAX']?></div>
+    <div class="gridCell textRight"><?=$building['UF_ACCESSES']?></div>
+    <?}?>
+  </div>
+  <div class="gridFooter">
+  <?for ($i=1;$i<=$arResult['PAGES'];$i++){?>
+    <span class="pagesList <?=($arResult['ACTIVE_PAGE'] == $i)?"active":""?>"><?=$i?></span>
+  <?}?>
+  </div>
+  <input id="page" name="page" type="hidden" value="<?=$arResult['ACTIVE_PAGE']?>">
+  <button id="submit" type="submit" style="display:none;"></button>
+</form>
+<script>
+   $(".pagesList").click(function(){
+   if ($(this).html() != $("#page").val()){
+       $("#page").val($(this).html())
+     $('#submit').trigger('click');
+   }
+  });
+</script>
