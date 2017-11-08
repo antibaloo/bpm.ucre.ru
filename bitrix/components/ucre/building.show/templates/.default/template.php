992e900f379c9a1698bb78d1f988d6faacf15870
Andrew Abalakov <a.s.abalakov@ucre.ru>
2017-11-08 15:33:27 +0500

    Удаление шаблона по-умолчанию

--- bitrix/components/ucre/building.show/templates/.default/template.php
+++
@@ -1,8 +0,0 @@
-<?php
-if(!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED!==true) die();
-$APPLICATION->SetTitle("Карточка здания");
-echo "<pre>";
-print_r($arResult);
-echo "</pre>";
-?>
-<a href="/townbase/building/list/">К списку</a>
