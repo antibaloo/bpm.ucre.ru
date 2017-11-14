735af6e78aa72bf7c663e0728eb739764ba850c9
Andrew Abalakov <a.s.abalakov@ucre.ru>
2017-11-14 11:03:38 +0500

    Итоговый шаблон многоквартирного дома

--- bitrix/components/ucre/building.edit/templates/.default/multiflat.php
+++ bitrix/components/ucre/building.edit/templates/.default/multiflat.php
@@ -15,7 +15,7 @@ $APPLICATION->SetTitle("Заполните параметры здания");
   </div>
   <div class="addressWrapper">
     <div class="empty"></div>
-    <div class="label addressBox">Адрес здания</div>
+    <div class="label addressBox <?=($arResult['errors']['UF_BUILDING_ADDRESS'])?"fieldError":""?>" title="<?=($arResult['errors']['UF_BUILDING_ADDRESS'])?$arResult['errors']['UF_BUILDING_ADDRESS']:""?>">Адрес здания *</div>
     <div class="param addressBox">
       <input class="block-input" type="search" id="UF_BUILDING_ADDRESS" name="UF_BUILDING_ADDRESS" placeholder="Введите адрес здания" value="<?=$arResult['UF_BUILDING_ADDRESS']?>">
     </div>
@@ -29,39 +29,39 @@ $APPLICATION->SetTitle("Заполните параметры здания");
   </div>
   <div class="paramsWrapper">
     <div class="empty"></div>
-    <div class="label paramBox">Страна</div>
+    <div class="label paramBox <?=($arResult['errors']['UF_COUNTRY'])?"fieldError":""?>" title="<?=($arResult['errors']['UF_COUNTRY'])?$arResult['errors']['UF_COUNTRY']:""?>">Страна *</div>
     <div class="param paramBox"><input class="block-input" type="search" id="UF_COUNTRY" name="UF_COUNTRY" placeholder="страна" value="<?=$arResult['UF_COUNTRY']?>"></div>
-    <div class="label paramBox">Федеральный округ</div>
+    <div class="label paramBox <?=($arResult['errors']['UF_FED_DISTRICT'])?"fieldError":""?>" title="<?=($arResult['errors']['UF_FED_DISTRICT'])?$arResult['errors']['UF_FED_DISTRICT']:""?>">Федеральный округ *</div>
     <div class="param paramBox"><input class="block-input" type="search" id="UF_FED_DISTRICT" name="UF_FED_DISTRICT" placeholder="федеральный округ" value="<?=$arResult['UF_FED_DISTRICT']?>"></div>
     <div class="empty"></div>
   </div>
   <div class="paramsWrapper">
     <div class="empty"></div>
-    <div class="label paramBox">Субъект федерации</div>
+    <div class="label paramBox <?=($arResult['errors']['UF_PROVINCE'])?"fieldError":""?>" title="<?=($arResult['errors']['UF_PROVINCE'])?$arResult['errors']['UF_PROVINCE']:""?>">Субъект федерации *</div>
     <div class="param paramBox"><input class="block-input" type="search" id="UF_PROVINCE" name="UF_PROVINCE" placeholder="cубъект федерации: область, край, республика" value="<?=$arResult['UF_PROVINCE']?>"></div>
-    <div class="label paramBox">Район субъекта</div>
+    <div class="label paramBox <?=($arResult['errors']['UF_AREA'])?"fieldError":""?>" title="<?=($arResult['errors']['UF_AREA'])?$arResult['errors']['UF_AREA']:""?>">Район субъекта *</div>
     <div class="param paramBox"><input class="block-input" type="search" id="UF_AREA" name="UF_AREA" placeholder="район населенного пункта" value="<?=$arResult['UF_AREA']?>"></div>
     <div class="empty"></div>
   </div>
   <div class="paramsWrapper">
     <div class="empty"></div>
-    <div class="label paramBox">Населенный пункт</div>
+    <div class="label paramBox <?=($arResult['errors']['UF_LOCALITY'])?"fieldError":""?>" title="<?=($arResult['errors']['UF_LOCALITY'])?$arResult['errors']['UF_LOCALITY']:""?>">Населенный пункт *</div>
     <div class="param paramBox"><input class="block-input" type="search" id="UF_LOCALITY" name="UF_LOCALITY" placeholder="населенный пункт без топонимов" value="<?=$arResult['UF_LOCALITY']?>"></div>
-    <div class="label paramBox">Район НП</div>
+    <div class="label paramBox <?=($arResult['errors']['UF_DISTRICT'])?"fieldError":""?>" title="<?=($arResult['errors']['UF_DISTRICT'])?$arResult['errors']['UF_DISTRICT']:""?>">Район НП</div>
     <div class="param paramBox"><input class="block-input" type="search" id="UF_DISTRICT" name="UF_DISTRICT" placeholder="район населенного пункта" value="<?=$arResult['UF_DISTRICT']?>"></div>
     <div class="empty"></div>
   </div>
   <div class="paramsWrapper">
     <div class="empty"></div>
-    <div class="label paramBox">Улица</div>
+    <div class="label paramBox <?=($arResult['errors']['UF_STREET'])?"fieldError":""?>" title="<?=($arResult['errors']['UF_STREET'])?$arResult['errors']['UF_STREET']:""?>">Улица *</div>
     <div class="param paramBox"><input class="block-input" type="search" id="UF_STREET" name="UF_STREET" placeholder="улица, проспект, проезд, переулок ..." value="<?=$arResult['UF_STREET']?>"></div>
-    <div class="label paramBox">№ дома</div>
+    <div class="label paramBox <?=($arResult['errors']['UF_HOUSE'])?"fieldError":""?>" title="<?=($arResult['errors']['UF_HOUSE'])?$arResult['errors']['UF_HOUSE']:""?>">№ дома *</div>
     <div class="param paramBox"><input class="block-input" type="search" id="UF_HOUSE" name="UF_HOUSE" placeholder="номер дома одной строкой" value="<?=$arResult['UF_HOUSE']?>"></div>
     <div class="empty"></div>
   </div>
    <div class="paramsWrapper">
     <div class="empty"></div>
-    <div class="label paramBox">Индекс</div>
+    <div class="label paramBox <?=($arResult['errors']['UF_POSTAL'])?"fieldError":""?>" title="<?=($arResult['errors']['UF_POSTAL'])?$arResult['errors']['UF_POSTAL']:""?>">Индекс *</div>
     <div class="param paramBox"><input class="block-input" type="search" id="UF_POSTAL" name="UF_POSTAL" placeholder="индекс" value="<?=$arResult['UF_POSTAL']?>"></div>
     <div class="empty"></div>
     <div class="empty"></div>
@@ -69,59 +69,61 @@ $APPLICATION->SetTitle("Заполните параметры здания");
   </div>
   <div class="paramsWrapper">
     <div class="empty"></div>
-    <div class="label paramBox">Широта</div>
+    <div class="label paramBox <?=($arResult['errors']['UF_LATITUDE'])?"fieldError":""?>" title="<?=($arResult['errors']['UF_LATITUDE'])?$arResult['errors']['UF_LATITUDE']:""?>">Широта *</div>
     <div class="param paramBox"><input class="block-input" type="search" id="UF_LATITUDE" name="UF_LATITUDE" placeholder="широта" value="<?=$arResult['UF_LATITUDE']?>"></div>
-    <div class="label paramBox">Долгота</div>
+    <div class="label paramBox <?=($arResult['errors']['UF_LONGITUDE'])?"fieldError":""?>" title="<?=($arResult['errors']['UF_LONGITUDE'])?$arResult['errors']['UF_LONGITUDE']:""?>">Долгота *</div>
     <div class="param paramBox"><input class="block-input" type="search" id="UF_LONGITUDE" name="UF_LONGITUDE" placeholder="долгота" value="<?=$arResult['UF_LONGITUDE']?>"></div>
     <div class="empty"></div>
   </div>
   <div class="paramsWrapper">
     <div class="empty"></div>
-    <div class="label paramBox">Кадастровый номер</div>
+    <div class="label paramBox <?=($arResult['errors']['UF_BUILDING_KAD_NUM'])?"fieldError":""?>" title="<?=($arResult['errors']['UF_BUILDING_KAD_NUM'])?$arResult['errors']['UF_BUILDING_KAD_NUM']:""?>">Кадастровый номер *</div>
     <div class="param paramBox"><input class="block-input" type="search" id="UF_BUILDING_KAD_NUM" name="UF_BUILDING_KAD_NUM" placeholder="кадастровый номер" value="<?=$arResult['UF_BUILDING_KAD_NUM']?>"></div>
-    <div class="label paramBox">Общая площадь</div>
+    <div class="label paramBox <?=($arResult['errors']['UF_B_SQUARE'])?"fieldError":""?>" title="<?=($arResult['errors']['UF_B_SQUARE'])?$arResult['errors']['UF_B_SQUARE']:""?>">Общая площадь *</div>
     <div class="param paramBox"><input class="block-input" type="search" id="UF_B_SQUARE" name="UF_B_SQUARE" placeholder="общая площадь" value="<?=$arResult['UF_B_SQUARE']?>"></div>
     <div class="empty"></div>
   </div>
   <div class="paramsWrapper">
     <div class="empty"></div>
-    <div class="label paramBox">Количество помещений</div>
+    <div class="label paramBox <?=($arResult['errors']['UF_PLACEMENT_COUNT'])?"fieldError":""?>" title="<?=($arResult['errors']['UF_PLACEMENT_COUNT'])?$arResult['errors']['UF_PLACEMENT_COUNT']:""?>">Количество помещений *</div>
     <div class="param paramBox"><input class="block-input" type="search" id="UF_PLACEMENT_COUNT" name="UF_PLACEMENT_COUNT" placeholder="количеств опомещений" value="<?=$arResult['UF_PLACEMENT_COUNT']?>"></div>
-    <div class="label paramBox">из них жилые</div>
+    <div class="label paramBox <?=($arResult['errors']['UF_LIVE_COUNT'])?"fieldError":""?>" title="<?=($arResult['errors']['UF_LIVE_COUNT'])?$arResult['errors']['UF_LIVE_COUNT']:""?>">из них жилые *</div>
     <div class="param paramBox"><input class="block-input" type="search" id="UF_LIVE_COUNT" name="UF_LIVE_COUNT" placeholder="из них жилые" value="<?=$arResult['UF_LIVE_COUNT']?>"></div>
     <div class="empty"></div>
   </div>
   <div class="paramsWrapper">
     <div class="empty"></div>
-    <div class="label paramBox">Минимальная этажность</div>
+    <div class="label paramBox <?=($arResult['errors']['UF_FLOORS_MIN'])?"fieldError":""?>" title="<?=($arResult['errors']['UF_FLOORS_MIN'])?$arResult['errors']['UF_FLOORS_MIN']:""?>">Этажность <sub>min</sub> *</div>
     <div class="param paramBox"><input class="block-input" type="search" id="UF_FLOORS_MIN" name="UF_FLOORS_MIN" placeholder="минимальная этажность" value="<?=$arResult['UF_FLOORS_MIN']?>"></div>
-    <div class="label paramBox">Максимальная этажность</div>
+    <div class="label paramBox <?=($arResult['errors']['UF_FLOORS_MAX'])?"fieldError":""?>" title="<?=($arResult['errors']['UF_FLOORS_MAX'])?$arResult['errors']['UF_FLOORS_MAX']:""?>">Этажность <sub>max</sub> *</div>
     <div class="param paramBox"><input class="block-input" type="search" id="UF_FLOORS_MAX" name="UF_FLOORS_MAX" placeholder="максимальная этажность" value="<?=$arResult['UF_FLOORS_MAX']?>"></div>
     <div class="empty"></div>
   </div>
   <div class="paramsWrapper">
     <div class="empty"></div>
-    <div class="label paramBox">Подземных этажей</div>
+    <div class="label paramBox <?=($arResult['errors']['UF_UNDER_FLOORS'])?"fieldError":""?>" title="<?=($arResult['errors']['UF_UNDER_FLOORS'])?$arResult['errors']['UF_UNDER_FLOORS']:""?>">Подземных этажей *</div>
     <div class="param paramBox"><input class="block-input" type="search" id="UF_UNDER_FLOORS" name="UF_UNDER_FLOORS" placeholder="подземных этажей" value="<?=$arResult['UF_UNDER_FLOORS']?>"></div>
-    <div class="empty"></div>
-    <div class="empty"></div>
+    <div class="label paramBox <?=($arResult['errors']['UF_MATERIAL'])?"fieldError":""?>" title="<?=($arResult['errors']['UF_MATERIAL'])?$arResult['errors']['UF_MATERIAL']:""?>">Тип здания *</div>
+    <div class="param paramBox">
+      <select class="selectList" id="UF_MATERIAL" name="UF_MATERIAL"></select>
+    </div>
     <div class="empty"></div>
   </div>
   <div class="paramsWrapper">
     <div class="empty"></div>
-    <div class="label paramBox">Количество секций</div>
+    <div class="label paramBox <?=($arResult['errors']['UF_SECTIONS'])?"fieldError":""?>" title="<?=($arResult['errors']['UF_SECTIONS'])?$arResult['errors']['UF_SECTIONS']:""?>">Количество секций *</div>
     <div class="param paramBox"><input class="block-input" type="search" id="UF_SECTIONS" name="UF_SECTIONS" placeholder="количество секций" value="<?=$arResult['UF_SECTIONS']?>"></div>
-    <div class="label paramBox">Количество лифтов</div>
+    <div class="label paramBox <?=($arResult['errors']['UF_ELEVATORS'])?"fieldError":""?>" title="<?=($arResult['errors']['UF_ELEVATORS'])?$arResult['errors']['UF_ELEVATORS']:""?>">Количество лифтов *</div>
     <div class="param paramBox"><input class="block-input" type="search" id="UF_ELEVATORS" name="UF_ELEVATORS" placeholder="количество лифтов" value="<?=$arResult['UF_ELEVATORS']?>"></div>
     <div class="empty"></div>
   </div>
   <div class="paramsWrapper">
     <div class="empty"></div>
-    <div class="label paramBox">Земельный участок</div>
+    <div class="label paramBox <?=($arResult['errors']['UF_B_PLOT_ID'])?"fieldError":""?>" title="<?=($arResult['errors']['UF_B_PLOT_ID'])?$arResult['errors']['UF_B_PLOT_ID']:""?>">Земельный участок *</div>
     <div class="param paramBox">
       <select class="selectList" id="UF_B_PLOT_ID" name="UF_B_PLOT_ID"></select>
     </div>
-    <div class="label paramBox">Входит в состав ЖК</div>
+    <div class="label paramBox <?=($arResult['errors']['UF_RS'])?"fieldError":""?>" title="<?=($arResult['errors']['UF_RS'])?$arResult['errors']['UF_RS']:""?>">ЖК</div>
     <div class="param paramBox">
       <select class="selectList" id="UF_RS" name="UF_RS"></select>
     </div>
@@ -145,8 +147,9 @@ $APPLICATION->SetTitle("Заполните параметры здания");
 <?echo "<pre>";print_r($arResult);echo "</pre>";?>
 <script>
   $(document).ready(function() {
-    $('#UF_B_PLOT_ID').select2({data: <?=$arResult['PLOTS']?>});//ZHKS
+    $('#UF_B_PLOT_ID').select2({data: <?=$arResult['PLOTS']?>});
     $('#UF_RS').select2({data: <?=$arResult['ZHKS']?>});
+    $('#UF_MATERIAL').select2({data: <?=$arResult['MATERIALS']?>});
   });
   
   $(".formButton").click(function (){
