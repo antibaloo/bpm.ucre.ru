a9b86a1bca9b86df47d44232adac5e7e050d6941
Andrew Abalakov <a.s.abalakov@ucre.ru>
2017-11-16 11:27:51 +0500

    Раздел помещений в БГ ссылается на компонент ucre:placement

--- townbase/placement/index.php
+++ townbase/placement/index.php
@@ -2,4 +2,25 @@
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 $APPLICATION->SetTitle("Помещения: комнаты, квартиры, нежилые");
 ?>
+<?$APPLICATION->IncludeComponent(
+	"ucre:placement", 
+	"", 
+	array(
+		"SEF_FOLDER" => "/townbase/placement/",// Каталог ЧПУ (относительно корня сайта)
+		"SEF_URL_TEMPLATES" => array(
+			"index" => "index.php",
+			"list" => "list/",
+			"edit" => "edit/#element_id#/",
+      "show" => "show/#element_id#/"
+		),
+		"VARIABLE_ALIASES" => array(
+			"index" => "",
+			"list" => "",
+			"edit" => "",
+      "show" => ""
+		)
+	),
+	false
+);
+?>
 <?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
