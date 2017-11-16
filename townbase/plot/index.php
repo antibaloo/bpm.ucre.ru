0b6d77cd2a09be2ef1d579ef8bb172e6be3ec34e
Andrew Abalakov <a.s.abalakov@ucre.ru>
2017-11-16 11:28:25 +0500

    Раздел участков в БГ ссылается на компонент ucre:plot

--- townbase/plot/index.php
+++ townbase/plot/index.php
@@ -2,4 +2,25 @@
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 $APPLICATION->SetTitle("Земельные участки");
 ?>
+<?$APPLICATION->IncludeComponent(
+	"ucre:plot", 
+	"", 
+	array(
+		"SEF_FOLDER" => "/townbase/plot/",// Каталог ЧПУ (относительно корня сайта)
+		"SEF_URL_TEMPLATES" => array(
+			"index" => "index.php",
+			"list" => "list/",
+			"edit" => "edit/#element_id#/",
+      "show" => "show/#element_id#/"
+		),
+		"VARIABLE_ALIASES" => array(
+			"index" => "",
+			"list" => "",
+			"edit" => "",
+      "show" => ""
+		)
+	),
+	false
+);
+?>
 <?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
