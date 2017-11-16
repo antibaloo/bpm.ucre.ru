e60dd0d8c192c859425fff279eea0ce5a347818f
Andrew Abalakov <a.s.abalakov@ucre.ru>
2017-11-16 09:32:05 +0500

    Новый клмплексный компонент ucre:chess для просмотра и редактирования шахматок

---
+++ bitrix/components/ucre/chess/component.php
@@ -0,0 +1,21 @@
+<?
+if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED!==true)die();
+// подключаем модули
+CModule::IncludeModule('iblock');
+CModule::IncludeModule('highloadblock');
+
+// необходимые классы
+use Bitrix\Highloadblock as HL;
+use Bitrix\Main\Entity;
+
+// $hlblock - это массив, 2 - hl блок Building
+$hlblock   = HL\HighloadBlockTable::getById(2)->fetch();
+$Building   = HL\HighloadBlockTable::compileEntity( $hlblock );
+$BuildingDataClass = $Building->getDataClass();
+$arResult['DATA'] = $BuildingDataClass::getRowById($arParams['ELEMENT_ID']);
+$flatFloorCount = 3;
+echo "<pre>"; print_r($arParams);echo "</pre>";
+echo "<pre>"; print_r($_POST);echo "</pre>";
+$template = $arParams['ACTION'];
+$this->IncludeComponentTemplate($template);
+?>
