47aaa730b18078d9d183ead3bb8db1b4dbf75d7b
Andrew Abalakov <a.s.abalakov@ucre.ru>
2017-12-01 12:07:27 +0500

    Оригинальный компнент новой формы заявки(сделки)

---
+++ bitrix/components/bitrix/crm.deal.details/templates/.default/lang/de/template.php
@@ -0,0 +1,11 @@
+<?
+$MESS["CRM_DEAL_DETAIL_HISTORY_STUB"] = "Sie fügen gerade einen Auftrag hinzu...";
+$MESS["CRM_DEAL_CONV_ACCESS_DENIED"] = "Sie benötigen Zugriffsrechte zur Erstellung von Kontakten, Unternehmen und Aufträgen, um diese Operation abzuschließen.";
+$MESS["CRM_DEAL_CONV_GENERAL_ERROR"] = "Allgemeiner Konvertierungsfehler.";
+$MESS["CRM_DEAL_CONV_DIALOG_TITLE"] = "Ein Element aus dem Auftrag erstellen";
+$MESS["CRM_DEAL_CONV_DIALOG_CONTINUE_BTN"] = "Fortfahren";
+$MESS["CRM_DEAL_CONV_DIALOG_CANCEL_BTN"] = "Abbrechen";
+$MESS["CRM_DEAL_CONV_DIALOG_SYNC_LEGEND"] = "Ausgewählte Elemente haben keine Felder, wo Auftragsdaten gespeichert werden können. Wählen Sie bitte Elemente aus, in denen fehlende Felder erstellt werden, damit alle verfügbaren Informationen gespeichert werden können.";
+$MESS["CRM_DEAL_CONV_DIALOG_SYNC_FILED_LIST_TITLE"] = "Diese Felder werden erstellt";
+$MESS["CRM_DEAL_CONV_DIALOG_SYNC_ENTITY_LIST_TITLE"] = "Wählen Sie Elemente aus, in denen fehlende Felder erstellt werden";
+?>
---
+++ bitrix/components/bitrix/crm.deal.details/templates/.default/lang/ua/template.php
@@ -0,0 +1,11 @@
+<?
+$MESS["CRM_DEAL_DETAIL_HISTORY_STUB"] = "Просто зараз ви створюєте угоду ...";
+$MESS["CRM_DEAL_CONV_ACCESS_DENIED"] = "Для виконання операції необхідні дозволи на створення контактів, компаній та угод.";
+$MESS["CRM_DEAL_CONV_GENERAL_ERROR"] = "Загальна помилка конвертування.";
+$MESS["CRM_DEAL_CONV_DIALOG_TITLE"] = "Створення сутність на підставі угоди";
+$MESS["CRM_DEAL_CONV_DIALOG_CONTINUE_BTN"] = "Продовжити";
+$MESS["CRM_DEAL_CONV_DIALOG_CANCEL_BTN"] = "Скасувати";
+$MESS["CRM_DEAL_CONV_DIALOG_SYNC_LEGEND"] = "В обраних сутностях немає полів, в які можна передати дані з угоди. Будь ласка, оберіть сутність, в яких будуть створені відсутні поля, щоб зберегти всю доступну інформацію.";
+$MESS["CRM_DEAL_CONV_DIALOG_SYNC_FILED_LIST_TITLE"] = "Які поля будуть створені";
+$MESS["CRM_DEAL_CONV_DIALOG_SYNC_ENTITY_LIST_TITLE"] = "Оберіть сутності, в яких будуть створені відсутні поля";
+?>
