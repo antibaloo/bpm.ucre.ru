cac6b1df71583f3071d4c4c307d42264db7bea38
Andrew Abalakov <a.s.abalakov@ucre.ru>
2017-12-05 15:18:13 +0500

    -ненужный запрос

--- bitrix/components/ucre/crm.deal.geo/component.php
+++ bitrix/components/ucre/crm.deal.geo/component.php
@@ -1,19 +1,10 @@
 <?
 CModule::IncludeModule('crm');
 if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED!==true) die();//Запрет вызова из адресной строки браузера
-$rsDeal = CCrmDeal::GetListEx(
-	array(),
-	array("ID" => $arParams['DEAL_ID']),
-	false,
-	false,
-	array("CATEGORY_ID"),
-	array()
-);
 $arResult['COMPONENT_PATH'] = $this->GetPath();
 $rsData = $DB->Query("select polygonArray from b_crm_search_polygon where dealId=".$arParams['DEAL_ID']);
 if ($aData = $rsData->Fetch()) $arResult['POLYGON'] = $aData['polygonArray'];
 else $arResult['POLYGON'] = "";
 $arResult['DEAL_ID'] = $arParams['DEAL_ID'];
-$mainDeal = $rsDeal->Fetch();
 $this->IncludeComponentTemplate();
 ?>
