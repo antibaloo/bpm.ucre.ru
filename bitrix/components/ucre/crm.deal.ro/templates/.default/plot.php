8fbad272ad3707177bb4bd9be3d7434ab521ff7d
Andrew Abalakov <a.s.abalakov@ucre.ru>
2017-12-08 09:52:24 +0500

    Кадастровый номер в карточке объекта недвижимости. Адрес -> в подвал

--- bitrix/components/ucre/crm.deal.ro/templates/.default/plot.php
+++ bitrix/components/ucre/crm.deal.ro/templates/.default/plot.php
@@ -3,7 +3,7 @@ if(!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED!==true) die();
 ?>
 <table class="crm-offer-info-table crm-offer-main-info-text">
   <tbody>
-    <tr><td colspan="5"><div class="crm-offer-title">Характеристики объекта по адресу: <?=$arResult['ADDRESS']?>, <?=$arResult['LINK']?></div></td></tr>
+    <tr><td colspan="5"><div class="crm-offer-title">Характеристики объекта по адресу: <?=$arResult['ADDRESS']?></div></td></tr>
     <tr class="crm-offer-row">
       <td class="crm-offer-info-drg-btn"></td>
       <td class="crm-offer-info-left">
@@ -44,6 +44,20 @@ if(!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED!==true) die();
         <div class="crm-offer-info-label-wrap"><span class="crm-offer-info-label">Район:</span></div>
       </td><td class="crm-offer-info-right">
       <div class="crm-offer-info-label-wrap"><span class="crm-offer-info-label"><?=$arResult['LOCALITY']?></span></div>
+      </td>
+    </tr>
+    <tr class="crm-offer-row">
+      <td class="crm-offer-info-drg-btn"></td>
+      <td class="crm-offer-info-left">
+        <div class="crm-offer-info-label-wrap"><span class="crm-offer-info-label">Кадастровый номер:</span></div>
+      </td>
+      <td class="crm-offer-info-right">
+        <div class="crm-offer-info-label-wrap"><span class="crm-offer-info-label"><?=$arResult['KAD_NUMBER']?></span></div>
+      </td>
+      <td class="crm-offer-info-left">
+        <div class="crm-offer-info-label-wrap"><span class="crm-offer-info-label">Ссылка на сайт:</span></div>
+      </td><td class="crm-offer-info-right">
+      <div class="crm-offer-info-label-wrap"><span class="crm-offer-info-label"><?=$arResult['LINK']?></span></div>
       </td>
     </tr>
   </tbody>
