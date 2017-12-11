01273f3fda0fc439701a1f024dea1f23df703437
Andrew Abalakov <a.s.abalakov@ucre.ru>
2017-12-11 15:18:08 +0500

    Добавлена внешня фильтрация через параметры компонента или POST параметры, добавлен параметр запрета меню создания дял вывода списка дубликатов

--- bitrix/components/ucre/plot.list/component.php
+++ bitrix/components/ucre/plot.list/component.php
@@ -12,10 +12,11 @@ $hlblock   = HL\HighloadBlockTable::getById(6)->fetch();
 $Plot   = HL\HighloadBlockTable::compileEntity($hlblock);
 $PlotDataClass = $Plot->getDataClass();
 
+$arResult['FILTER'] = (isset($_POST['filter']))?array_merge( $_POST['filter'],$arParams['FILTER']):$arParams['FILTER'];
 $rsData = $PlotDataClass::getList(
   array(
     "select" => array('*'), //выбираем все поля
-    "filter" => array(),
+    "filter" => $arResult['FILTER'],
     "order" => array("ID"=>"ASC"), // сортировка по полю ID, будет работать только, если вы завели такое поле в hl'блоке
   )
 );
@@ -24,8 +25,7 @@ $arResult['ON_PAGE'] = ($arParams['ON_PAGE']>0)?$arParams['ON_PAGE']:25;
 $arResult['ACTIVE_PAGE'] = ($_POST['page'] >0)?$_POST['page']:1;
 $arResult['COUNT'] = $rsData->getSelectedRowsCount();
 $arResult['PAGES'] = ($arResult['COUNT'] / $arResult['ON_PAGE'] > 1)?ceil($arResult['COUNT'] / $arResult['ON_PAGE']):1;
-
 $arResult['DATA'] = array_slice ($rsData->FetchAll(),($arResult['ACTIVE_PAGE']-1)*$arResult['ON_PAGE'],$arResult['ON_PAGE']);//Вырезаем часть результата в соответствии с активной страницей и кол-вом записей на странице
-
+$arResult['NO_MENU'] = ($arParams['NO_MENU'])?$arParams['NO_MENU']: false;
 $this->IncludeComponentTemplate();
 ?>
