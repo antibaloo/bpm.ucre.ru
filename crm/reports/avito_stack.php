a9a81da69de904721ddb531e8460fd6c5145d127
Andrew Abalakov <a.s.abalakov@ucre.ru>
2017-12-20 13:52:13 +0500

    Поправил отчет об очереди выгрузки на Авито в соответствии с действующим алгоритмом отчета

--- crm/reports/avito_stack.php
+++ crm/reports/avito_stack.php
@@ -3,16 +3,20 @@
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 $APPLICATION->SetTitle("Очередь выгрузки на Авито");
 ?>
-  <p>Выгрузка на авито происходит 3 раза в день в <b>00:15, 8:15 и 16:15</b>. Формирование реальных пакетов происходит в <b>7:36, 15:35 и 23:35.</b></p>
-  <p>Т.е. если вы хотите внести изменения в уже выгружаемые заявки, то это нужно сделать до этого времени.</p>
-  <p>Попадание заявок на выгрузку происходит как в ручном режиме (за счет сотрудинка и с его контактными данными) через задачу поставленную на Абалакова А.С с Н.П. Молчановой в качестве наблюдателя (она сделает отметку об оплате), так и в автоматическом
-    режиме.
-  </p>
-  <p>Автоматический отбор проводится 2 раза в сутки (<b>7:25 и 15:25</b>) по следующим критериям: активные заявки на продажу с не пустой ценой и описанием, с привязанным объектом, с фото или планировками, не относящиеся к категории "земельные участки" или
-    "коммерческая недвижимость". Сотрировка этой очереди проходит по убыванию даты изменения, т.е. новые и недавно отредактированные заявки находятся в начале очереди</p>
-  <p>
-    На текущий момент отбор производится по 5 первых за раз, таким образом в день отбирается 10 новых заявок на выгрузку.
-  </p>
+<p>Выгрузка на авито происходит 3 раза в день в <b>00:15, 8:15 и 16:15</b>. Формирование реальных пакетов происходит в <b>7:36, 15:35 и 23:35.</b></p>
+<p>Т.е. если вы хотите внести изменения в уже выгружаемые заявки, то это нужно сделать до этого времени.</p>
+<p>Попадание заявок на выгрузку происходит как в ручном режиме (за счет сотрудинка и с его контактными данными) через задачу поставленную на Абалакова А.С с Н.П. Молчановой в качестве наблюдателя (она сделает отметку об оплате), так и в автоматическом
+  режиме.
+</p>
+<p>Автоматический отбор проводится 2 раза в сутки (<b>7:25 и 15:25</b>) по следующим критериям: 
+<ul>
+  <li>5 заявок на поиск покупателя в стадии "Агентский договор подписан" с непустой ценой, описанием, с фото и/или планировкой, не относящиеся к категории "земельные участки" или
+    "коммерческая недвижимость". Сотрировка этой очереди проходит по убыванию даты изменения, т.е. новые и недавно отредактированные заявки находятся в начале очереди
+  </li>
+  <li>
+    если потребность в 5 объявлений не была удовлетворена за счет договорных объектов, то производится аналогичный поиск среди заявок в статусе "Заявка на поиск покупателя подписана"
+  </li>
+</ul>
   <p>
     <b>Месячный лимит составляет 330 объектов. При текущем объеме потерянных лидов и заявок на покупку расширять этот пакет нецелесообразно.</b>
   </p>
@@ -21,7 +25,11 @@ $APPLICATION->SetTitle("Очередь выгрузки на Авито");
   </div>
   <br>
   <?
-$rsData = $DB->Query("select b_crm_deal.ID,b_crm_deal.TITLE,b_crm_deal.ASSIGNED_BY_ID, b_crm_deal.COMMENTS,b_uts_crm_deal.UF_CRM_58958B5734602, b_uts_crm_deal.UF_CRM_1472038962, b_uts_crm_deal.UF_CRM_1476517423,b_iblock_element.ID as ELEMENT_ID, b_iblock_element.CODE, b_iblock_element_prop_s42.PROPERTY_210, b_iblock_element_prop_s42.PROPERTY_300, b_iblock_element_prop_s42.PROPERTY_213, b_iblock_element_prop_s42.PROPERTY_214, b_iblock_element_prop_s42.PROPERTY_215,b_iblock_element_prop_s42.PROPERTY_216,b_iblock_element_prop_s42.PROPERTY_217,b_iblock_element_prop_s42.PROPERTY_218, b_iblock_element_prop_s42.PROPERTY_298, b_iblock_element_prop_s42.PROPERTY_299, b_iblock_element_prop_s42.PROPERTY_229, b_iblock_element_prop_s42.PROPERTY_228, b_iblock_element_prop_s42.PROPERTY_224, b_iblock_element_prop_s42.PROPERTY_292, b_iblock_element_prop_s42.PROPERTY_225, b_iblock_element_prop_s42.PROPERTY_226, b_iblock_element_prop_s42.PROPERTY_221, b_iblock_element_prop_s42.PROPERTY_222, b_iblock_element_prop_s42.PROPERTY_242, b_iblock_element_prop_s42.PROPERTY_243, b_iblock_element_prop_s42.PROPERTY_238, b_iblock_element_prop_s42.PROPERTY_295 from b_crm_deal LEFT JOIN b_uts_crm_deal ON b_crm_deal.ID = b_uts_crm_deal.VALUE_ID LEFT JOIN b_iblock_element ON b_uts_crm_deal.UF_CRM_1469534140 = b_iblock_element.ID LEFT JOIN b_iblock_element_prop_s42 ON b_uts_crm_deal.UF_CRM_1469534140 = b_iblock_element_prop_s42.IBLOCK_ELEMENT_ID where b_crm_deal.CATEGORY_ID = 0 and b_uts_crm_deal.UF_CRM_1469534140 <> '' and b_crm_deal.STAGE_ID = 'PROPOSAL' AND (TIMESTAMP(b_iblock_element_prop_s42.PROPERTY_260) < NOW() OR b_iblock_element_prop_s42.PROPERTY_260 is null) AND b_crm_deal.COMMENTS<>'' AND b_uts_crm_deal.UF_CRM_58958B5734602 > 0 AND (b_uts_crm_deal.UF_CRM_1472038962<>'a:0:{}' OR b_uts_crm_deal.UF_CRM_1476517423 <> 'a:0:{}') AND b_iblock_element_prop_s42.PROPERTY_210<>387 AND b_iblock_element_prop_s42.PROPERTY_210<>386 ORDER BY b_crm_deal.DATE_MODIFY DESC");
+$statuses = array(
+  '1' => 'Агентский договор подписан',
+  '13' => 'Заявка на поиск покупателя подписана'
+);
+$rsData = $DB->Query("select b_crm_deal.ID,b_crm_deal.TITLE, b_crm_deal.STAGE_ID, b_crm_deal.ASSIGNED_BY_ID, b_crm_deal.COMMENTS,b_uts_crm_deal.UF_CRM_58958B5734602, b_uts_crm_deal.UF_CRM_1472038962, b_uts_crm_deal.UF_CRM_1476517423,b_iblock_element.ID as ELEMENT_ID, b_iblock_element.CODE, b_iblock_element_prop_s42.PROPERTY_210, b_iblock_element_prop_s42.PROPERTY_300, b_iblock_element_prop_s42.PROPERTY_213, b_iblock_element_prop_s42.PROPERTY_214, b_iblock_element_prop_s42.PROPERTY_215,b_iblock_element_prop_s42.PROPERTY_216,b_iblock_element_prop_s42.PROPERTY_217,b_iblock_element_prop_s42.PROPERTY_218, b_iblock_element_prop_s42.PROPERTY_298, b_iblock_element_prop_s42.PROPERTY_299, b_iblock_element_prop_s42.PROPERTY_229, b_iblock_element_prop_s42.PROPERTY_228, b_iblock_element_prop_s42.PROPERTY_224, b_iblock_element_prop_s42.PROPERTY_292, b_iblock_element_prop_s42.PROPERTY_225, b_iblock_element_prop_s42.PROPERTY_226, b_iblock_element_prop_s42.PROPERTY_221, b_iblock_element_prop_s42.PROPERTY_222, b_iblock_element_prop_s42.PROPERTY_242, b_iblock_element_prop_s42.PROPERTY_243, b_iblock_element_prop_s42.PROPERTY_238, b_iblock_element_prop_s42.PROPERTY_295 from b_crm_deal LEFT JOIN b_uts_crm_deal ON b_crm_deal.ID = b_uts_crm_deal.VALUE_ID LEFT JOIN b_iblock_element ON b_uts_crm_deal.UF_CRM_1469534140 = b_iblock_element.ID LEFT JOIN b_iblock_element_prop_s42 ON b_uts_crm_deal.UF_CRM_1469534140 = b_iblock_element_prop_s42.IBLOCK_ELEMENT_ID where b_crm_deal.CATEGORY_ID = 0 and b_uts_crm_deal.UF_CRM_1469534140 <> '' and (b_crm_deal.STAGE_ID = '1' OR b_crm_deal.STAGE_ID = '13') AND (TIMESTAMP(b_iblock_element_prop_s42.PROPERTY_260) < NOW() OR b_iblock_element_prop_s42.PROPERTY_260 is null) AND b_crm_deal.COMMENTS<>'' AND b_uts_crm_deal.UF_CRM_58958B5734602 > 0 AND (b_uts_crm_deal.UF_CRM_1472038962<>'a:0:{}' OR b_uts_crm_deal.UF_CRM_1476517423 <> 'a:0:{}') AND b_iblock_element_prop_s42.PROPERTY_210<>387 AND b_iblock_element_prop_s42.PROPERTY_210<>386 ORDER BY b_crm_deal.STAGE_ID ASC,b_crm_deal.DATE_MODIFY DESC");
 $count = $rsData->SelectedRowsCount();
 $rows = 20;
 $pages = ($count % $rows)?intval($count/$rows)+1:$count/$rows;
@@ -36,6 +44,7 @@ for ($i=1;$i<=$pages;$i++){
           <th width="5%">Очередь</th>
           <th width="5%">id</th>
           <th width="30%">Название заявки</th>
+          <th width="5%">Статус</th>
           <th width="8%">Цена, руб.</th>
           <th width="15%">Ответственный</th>
         </tr>
@@ -57,6 +66,7 @@ for ($i=1;$i<=$pages;$i++){
                 <?=$aRes['TITLE']?>
               </a>
             </td>
+            <td title="<?=$statuses[$aRes['STAGE_ID']]?>"><?=$statuses[$aRes['STAGE_ID']]?></td>
             <td style="text-align: right; padding-right: 5px;" title="<?=($aRes['UF_CRM_58958B5734602'])?number_format($aRes['UF_CRM_58958B5734602'],0," . "," "):"цена не указана "?>">
               <?=($aRes['UF_CRM_58958B5734602'])?number_format($aRes['UF_CRM_58958B5734602'],0,"."," "):"<span style='color:red;'>цена не указана</span>"?></td>
             <td>
