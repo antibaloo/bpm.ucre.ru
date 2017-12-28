561468e9c563208eb53d5044f09d0971f449367c
Andrew Abalakov <a.s.abalakov@ucre.ru>
2017-12-28 10:04:40 +0500

    Обновление

--- bitrix/components/bitrix/crm.deal.details/lang/de/class.php
+++ bitrix/components/bitrix/crm.deal.details/lang/de/class.php
@@ -1,6 +1,9 @@
 <?
+$MESS["CRM_MODULE_NOT_INSTALLED"] = "Das Modul CRM ist nicht installiert.";
+$MESS["CRM_PERMISSION_DENIED"] = "Zugriff verweigert";
 $MESS["CRM_DEAL_FIELD_TITLE"] = "Name";
 $MESS["CRM_DEAL_FIELD_TYPE_ID"] = "Auftragstyp";
+$MESS["CRM_DEAL_FIELD_STAGE_ID"] = "Phase";
 $MESS["CRM_DEAL_FIELD_OPPORTUNITY_WITH_CURRENCY"] = "Betrag und Währung";
 $MESS["CRM_DEAL_FIELD_CLOSEDATE"] = "Abschlussdatum";
 $MESS["CRM_DEAL_FIELD_BEGINDATE"] = "Anfangsdatum";
@@ -13,23 +16,22 @@ $MESS["CRM_DEAL_FIELD_RECURRING"] = "Wiederholung";
 $MESS["CRM_DEAL_FIELD_CATEGORY_RECURRING"] = "Auftragspipeline";
 $MESS["CRM_DEAL_FIELD_OPPORTUNITY"] = "Betrag";
 $MESS["CRM_DEAL_FIELD_CURRENCY_ID"] = "Währung";
+$MESS["CRM_DEAL_FIELD_PRODUCTS"] = "Produkte";
 $MESS["CRM_DEAL_SECTION_MAIN"] = "Zum Auftrag";
 $MESS["CRM_DEAL_SECTION_ADDITIONAL"] = "Mehr";
 $MESS["CRM_DEAL_SECTION_RECURRING"] = "Wiederkehrender Auftrag";
+$MESS["CRM_DEAL_SECTION_PRODUCTS"] = "Produkte";
 $MESS["CRM_DEAL_TAB_PRODUCTS"] = "Produkte";
+$MESS["CRM_DEAL_TAB_INVOICES"] = "Rechnungen";
+$MESS["CRM_DEAL_TAB_AUTOMATION"] = "Automatisierung";
 $MESS["CRM_DEAL_TAB_EVENT"] = "History";
 $MESS["CRM_DEAL_TAB_QUOTE"] = "Angebote";
-$MESS["CRM_DEAL_TAB_AUTOMATION"] = "Automatisierung";
-$MESS["CRM_MODULE_NOT_INSTALLED"] = "Das Modul CRM ist nicht installiert.";
-$MESS["CRM_PERMISSION_DENIED"] = "Zugriff verweigert";
-$MESS["CRM_DEAL_FIELD_STAGE_ID"] = "Phase";
-$MESS["CRM_DEAL_FIELD_PRODUCTS"] = "Produkte";
-$MESS["CRM_DEAL_SECTION_PRODUCTS"] = "Produkte";
-$MESS["CRM_DEAL_TAB_INVOICES"] = "Rechnungen";
+$MESS["CRM_DEAL_TAB_BIZPROC"] = "Workflows";
 $MESS["CRM_DEAL_FIELD_RECURRING_NOTHING_SELECTED"] = "Nichts ausgewählt";
+$MESS["CRM_DEAL_FIELD_RECURRING_RESTRICTED"] = "Sie haben nicht genügend Rechte";
 $MESS["CRM_DEAL_FIELD_RECURRING_CREATED_FROM_CURRENT"] = "Eine Vorlage wurde für den wiederkehrenden Auftrag ##RECURRING_ID# erstellt";
 $MESS["CRM_DEAL_FIELD_RECURRING_CREATED_MANY_FROM_CURRENT"] = "Vorlagen wurden für die wiederkehrenden Aufträge #RECURRING_LIST# erstellt";
-$MESS["CRM_DEAL_FIELD_RECURRING_DATE_NEXT_EXECUTION"] = "Nächste Erstellung erfolgt am #NEXT_DATE#";
+$MESS["CRM_DEAL_FIELD_RECURRING_DATE_NEXT_EXECUTION"] = "Nächster Auftrag wird erstellt am #NEXT_DATE#";
 $MESS["CRM_DEAL_FIELD_RECURRING_TITLE"] = "Auftragsvorlage: #TITLE#";
 $MESS["CRM_DEAL_FIELD_RECURRING_DAY"] = "Tage";
 $MESS["CRM_DEAL_FIELD_RECURRING_WEEK"] = "Wochen";
@@ -43,8 +45,6 @@ $MESS["CRM_DEAL_CREATION_PAGE_TITLE"] = "Neuer Auftrag";
 $MESS["CRM_DEAL_COPY_PAGE_TITLE"] = "Auftrag kopieren";
 $MESS["CRM_DEAL_FIELD_NUM_SIGN"] = "##DEAL_ID#";
 $MESS["CRM_DEAL_FIELD_CONTACT_LEGEND"] = "Mit dem Auftrag verknüpfte Kontakte";
-$MESS["CRM_DEAL_TAB_BIZPROC"] = "Workflows";
-$MESS["CRM_DEAL_FIELD_RECURRING_RESTRICTED"] = "Sie haben nicht genügend Rechte";
 $MESS["CRM_DEAL_NOT_FOUND"] = "Auftrag wurde nicht gefunden.";
 $MESS["CRM_DEAL_FIELD_ID"] = "ID";
 $MESS["CRM_RECURRING_DEAL_B24_BLOCK_TITLE"] = "Wiederkehrende Aufträge sind in Tarifen \"Standard\" und \"Professional\" verfügbar";
@@ -59,4 +59,5 @@ Erstellen Sie einen wiederkehrenden Auftrag, legen Sie einen Zeitplan und eine P
 <strong>Wiederkehrende Aufträge sind in Tarifen \"Standard\" und \"Professional\" verfügbar</strong>
 ";
 $MESS["CRM_DEAL_QUOTE_LINK"] = "Auftrag wurde aus dem Angebot <a target='_blank' href='#URL#'>#TITLE#</a> erstellt";
+$MESS["CRM_DEAL_TAB_TREE"] = "Abhängigkeiten";
 ?>
--- bitrix/components/bitrix/crm.deal.details/lang/en/class.php
+++ bitrix/components/bitrix/crm.deal.details/lang/en/class.php
@@ -1,6 +1,9 @@
 <?
+$MESS["CRM_MODULE_NOT_INSTALLED"] = "The CRM module is not installed.";
+$MESS["CRM_PERMISSION_DENIED"] = "Access denied";
 $MESS["CRM_DEAL_FIELD_TITLE"] = "Name";
 $MESS["CRM_DEAL_FIELD_TYPE_ID"] = "Deal type";
+$MESS["CRM_DEAL_FIELD_STAGE_ID"] = "Stage";
 $MESS["CRM_DEAL_FIELD_OPPORTUNITY_WITH_CURRENCY"] = "Amount and currency";
 $MESS["CRM_DEAL_FIELD_CLOSEDATE"] = "End date";
 $MESS["CRM_DEAL_FIELD_BEGINDATE"] = "Start date";
@@ -13,23 +16,22 @@ $MESS["CRM_DEAL_FIELD_RECURRING"] = "Repeat";
 $MESS["CRM_DEAL_FIELD_CATEGORY_RECURRING"] = "Deal pipeline";
 $MESS["CRM_DEAL_FIELD_OPPORTUNITY"] = "Amount";
 $MESS["CRM_DEAL_FIELD_CURRENCY_ID"] = "Currency";
+$MESS["CRM_DEAL_FIELD_PRODUCTS"] = "Products";
 $MESS["CRM_DEAL_SECTION_MAIN"] = "About deal";
 $MESS["CRM_DEAL_SECTION_ADDITIONAL"] = "More";
 $MESS["CRM_DEAL_SECTION_RECURRING"] = "Recurring deal";
+$MESS["CRM_DEAL_SECTION_PRODUCTS"] = "Products";
 $MESS["CRM_DEAL_TAB_PRODUCTS"] = "Products";
+$MESS["CRM_DEAL_TAB_INVOICES"] = "Invoices";
+$MESS["CRM_DEAL_TAB_AUTOMATION"] = "Automation";
 $MESS["CRM_DEAL_TAB_EVENT"] = "History";
 $MESS["CRM_DEAL_TAB_QUOTE"] = "Quotes";
-$MESS["CRM_DEAL_TAB_AUTOMATION"] = "Automation";
-$MESS["CRM_MODULE_NOT_INSTALLED"] = "The CRM module is not installed.";
-$MESS["CRM_PERMISSION_DENIED"] = "Access denied";
-$MESS["CRM_DEAL_FIELD_STAGE_ID"] = "Stage";
-$MESS["CRM_DEAL_FIELD_PRODUCTS"] = "Products";
-$MESS["CRM_DEAL_SECTION_PRODUCTS"] = "Products";
-$MESS["CRM_DEAL_TAB_INVOICES"] = "Invoices";
+$MESS["CRM_DEAL_TAB_BIZPROC"] = "Workflows";
 $MESS["CRM_DEAL_FIELD_RECURRING_NOTHING_SELECTED"] = "No selection";
+$MESS["CRM_DEAL_FIELD_RECURRING_RESTRICTED"] = "Insufficient rights";
 $MESS["CRM_DEAL_FIELD_RECURRING_CREATED_FROM_CURRENT"] = "Template has been created for recurring deal ##RECURRING_ID#";
 $MESS["CRM_DEAL_FIELD_RECURRING_CREATED_MANY_FROM_CURRENT"] = "Template has been created for recurring deals: #RECURRING_LIST#";
-$MESS["CRM_DEAL_FIELD_RECURRING_DATE_NEXT_EXECUTION"] = "Next pass will occur on #NEXT_DATE#";
+$MESS["CRM_DEAL_FIELD_RECURRING_DATE_NEXT_EXECUTION"] = "Next deal will be generated on #NEXT_DATE#";
 $MESS["CRM_DEAL_FIELD_RECURRING_TITLE"] = "Deal template: #TITLE#";
 $MESS["CRM_DEAL_FIELD_RECURRING_DAY"] = "days";
 $MESS["CRM_DEAL_FIELD_RECURRING_WEEK"] = "weeks";
@@ -43,8 +45,6 @@ $MESS["CRM_DEAL_CREATION_PAGE_TITLE"] = "New deal";
 $MESS["CRM_DEAL_COPY_PAGE_TITLE"] = "Copy deal";
 $MESS["CRM_DEAL_FIELD_NUM_SIGN"] = "##DEAL_ID#";
 $MESS["CRM_DEAL_FIELD_CONTACT_LEGEND"] = "Contacts linked to deal";
-$MESS["CRM_DEAL_TAB_BIZPROC"] = "Workflows";
-$MESS["CRM_DEAL_FIELD_RECURRING_RESTRICTED"] = "Insufficient rights";
 $MESS["CRM_DEAL_NOT_FOUND"] = "Deal was not found.";
 $MESS["CRM_DEAL_FIELD_ID"] = "ID";
 $MESS["CRM_RECURRING_DEAL_B24_BLOCK_TITLE"] = "Recurring deals are available in \"Standard\" and \"Professional\" plans";
@@ -58,4 +58,5 @@ $MESS["CRM_RECURRING_DEAL_B24_BLOCK_TEXT"] = "Let Bitrix24 create deals for you!
 		</ul>		
 		<strong>Recurring Deals Are Available in \"Standard\" And \"Professional\" Plans</strong>";
 $MESS["CRM_DEAL_QUOTE_LINK"] = "Deal was created based on the quote <a target='_blank' href='#URL#'>#TITLE#</a>";
+$MESS["CRM_DEAL_TAB_TREE"] = "Dependencies";
 ?>
