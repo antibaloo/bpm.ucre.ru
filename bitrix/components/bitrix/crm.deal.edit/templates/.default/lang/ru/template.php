fd33098b2ff68d5d48a3ef5e567ba4579389ff52
Andrew Abalakov <a.s.abalakov@ucre.ru>
2018-01-05 13:46:19 +0500

    Правильный перевод

---
+++ bitrix/components/bitrix/crm.deal.edit/templates/.default/lang/ru/template.php
@@ -0,0 +1,19 @@
+<?
+$MESS["CRM_TAB_1"] = "Заявка";
+$MESS["CRM_TAB_1_TITLE"] = "Свойства заявки";
+$MESS["CRM_TAB_2"] = "События";
+$MESS["CRM_TAB_2_TITLE"] = "События заявки";
+$MESS["CRM_TAB_3"] = "Бизнес-процессы";
+$MESS["CRM_TAB_3_TITLE"] = "Бизнес-процессы заявки";
+$MESS["CRM_DEAL_EDIT_TITLE"] = "Заявка №#ID# &mdash; #TITLE#";
+$MESS["CRM_DEAL_CREATE_TITLE"] = "Новая заявка";
+$MESS["CRM_DEAL_CONV_ACCESS_DENIED"] = "Для выполнения операции необходимы разрешения на создание счетов и предложений.";
+$MESS["CRM_DEAL_CONV_GENERAL_ERROR"] = "Общая ошибка.";
+$MESS["CRM_DEAL_CONV_DIALOG_TITLE"] = "Создание сущности на основании заявки";
+$MESS["CRM_DEAL_CONV_DIALOG_CONTINUE_BTN"] = "Продолжить";
+$MESS["CRM_DEAL_CONV_DIALOG_CANCEL_BTN"] = "Отмена";
+$MESS["CRM_DEAL_CONV_DIALOG_SYNC_LEGEND"] = "В выбранных сущностях нет полей, в которые можно передать данные из заявки. Пожалуйста, выберите сущности, в которых будут созданы недостающие поля, чтобы сохранить всю доступную информацию.";
+$MESS["CRM_DEAL_CONV_DIALOG_SYNC_FILED_LIST_TITLE"] = "Какие поля будут созданы";
+$MESS["CRM_DEAL_CONV_DIALOG_SYNC_ENTITY_LIST_TITLE"] = "Выберите сущности, в которых будут созданы недостающие поля";
+$MESS["CRM_DEAL_RECUR_SHOW_TITLE"] = "Шаблон заявки №#ID# &mdash; #TITLE#";
+?>
