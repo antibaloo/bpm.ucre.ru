e478c13a0b7c346f957baf4f51f0b2840f92c2d3
Andrew Abalakov <a.s.abalakov@ucre.ru>
2018-01-11 11:01:53 +0500

    Сделка -> заявка

---
+++ bitrix/components/bitrix/crm.lead.details/lang/ru/ajax.php
@@ -0,0 +1,7 @@
+<?
+$MESS["CRM_LEAD_PRODUCT_ROWS_SAVING_ERROR"] = "Во время сохранения товаров произошла ошибка.";
+$MESS["CRM_LEAD_DEAULT_TITLE"] = "Новый лид";
+$MESS["CRM_LEAD_NOT_FOUND"] = "Лид не найден.";
+$MESS["CRM_LEAD_ACCESS_DENIED"] = "Доступ запрещён.";
+$MESS["CRM_LEAD_DELETION_ERROR"] = "При удалении заявки произошла ошибка.";
+?>
