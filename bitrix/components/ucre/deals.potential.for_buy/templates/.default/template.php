df2c2587a57f5a54b149c57c3da5b34909e9dec4
Andrew Abalakov <a.s.abalakov@ucre.ru>
2018-01-25 14:27:35 +0500

    Для корректной работы в новой карточке заявки в шаблон компонента добавлен напрозрачный фон и внутренние отступы в основном блоке шаблона

--- bitrix/components/ucre/deals.potential.for_buy/templates/.default/template.php
+++ bitrix/components/ucre/deals.potential.for_buy/templates/.default/template.php
@@ -2,7 +2,7 @@
 <?php
 if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED!==true)die();
 ?>
-<div id="potentials">
+<div id="potentials" style="background-color:white; padding: 5px;">
 </div>
 <div id="markdialog" style="text-align:center; box-shadow: 0px 0px 100px 0px #000000; width: 400px;height: 250px;	margin: auto;display: none;background: #fff;z-index: 200;	position: fixed;left: 0;right: 0;top: 0;bottom: 0;padding: 10px;">
   <div id="markcaption"></div>
