6a1fb3917295ce6f27b84ce67befa5137e546439
Andrew Abalakov <a.s.abalakov@ucre.ru>
2018-01-25 15:42:00 +0500

    Непрозрачный фон и отступы в шаблон

--- bitrix/components/ucre/crm.avito.log/templates/.default/nolog.php
+++ bitrix/components/ucre/crm.avito.log/templates/.default/nolog.php
@@ -1,4 +1,6 @@
 <?php
 if(!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED!==true) die();
 ?>
-<h2>Выгрузка объекта не производилась!<h2>
+<div style="background-color: white; padding: 5px;">
+  <h2>Выгрузка объекта не производилась!</h2>
+</div>
