a54b7d3434343e122e1dff21757085da47bb5583
Andrew Abalakov <a.s.abalakov@ucre.ru>
2018-01-30 11:33:16 +0500

    Релизована структура галереи с шаблоном tilesgrid в рабочей версии

--- bitrix/components/ucre/gallery/templates/.default/show.php
+++ bitrix/components/ucre/gallery/templates/.default/show.php
@@ -1,4 +1,27 @@
 <?php
 if(!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED!==true) die();
-echo "<pre>";print_r($arResult);echo "</pre>";
+//echo "<pre>";print_r($arResult);echo "</pre>";
 ?>
+<!-- Собственно структура галереи -->
+<?foreach ($arResult['FIELDS'] as $header=>$field){
+  if (count($arResult[$field])){?>
+<center><h2><?=$header?></h2></center>
+<div id="<?=$field?>" style="display:none;">
+  <?foreach($arResult[$field] as $fileId=>$link){
+      $file = CFile::ResizeImageGet($fileId, array('width'=>150, 'height'=>150), BX_RESIZE_IMAGE_EXACT, true);
+  ?>
+  <img alt="<?$fileId?>" src="<?=$file['src']?>" data-image="<?=$link?>" data-description="<?$fileId?>">
+  <?}?>
+</div>
+<?}
+}?>
+<!-- Инициирующий js -->
+<script type="text/javascript">
+$(document).ready(function(){
+<?foreach ($arResult['FIELDS'] as $header=>$field){
+  if (count($arResult[$field])){?>
+ $("#<?=$field?>").unitegallery({gallery_theme: "tilesgrid"}); 
+<?}
+}?>
+});
+</script>
