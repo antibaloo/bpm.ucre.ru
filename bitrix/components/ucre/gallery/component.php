455612bf52c9d9ace3111b54209bf2253810c8f9
Andrew Abalakov <a.s.abalakov@ucre.ru>
2018-01-30 11:32:31 +0500

    Мелкие правки

--- bitrix/components/ucre/gallery/component.php
+++ bitrix/components/ucre/gallery/component.php
@@ -1,7 +1,11 @@
 <?
 if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED!==true)die();
 $arResult['FIELDS'] = $arParams['FIELDS'];
-foreach ($arResult['FIELDS'] as $field){$arResult[$field] = $arParams['ENTITY'][$field];}
+foreach ($arResult['FIELDS'] as $field){
+  foreach ( $arParams['ENTITY'][$field]['VALUE'] as $fileId){
+    $arResult[$field][$fileId] = CFile::GetPath($fileId);
+  }
+}
 $template = 'show';
 $this->IncludeComponentTemplate($template);
 ?>
