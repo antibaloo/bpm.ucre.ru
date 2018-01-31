1209f7739d26174c80fdd498d3a30a57ce820b8c
Andrew Abalakov <a.s.abalakov@ucre.ru>
2018-01-31 10:50:55 +0500

    E,hfy if,kjy htlfrnhjdfybz

--- bitrix/components/ucre/gallery/templates/.default/edit.php
+++
@@ -1,9 +0,0 @@
-<?php
-if(!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED!==true) die();
-?>
-<div id="<?=$arResult['TAB_ID']?>">
-  <center><input id="save_<?=$arResult['TAB_ID']?>" type="button" value="Сохранить">&nsbp;<input id="cancel_<?=$arResult['TAB_ID']?>" type="button" value="Отменить"></center>
-</div>
-<script type="text/javascript">
-  
-</script>
