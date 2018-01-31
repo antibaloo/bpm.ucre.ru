44749abb6134d8e9960db2a1a2638719aeff1930
Andrew Abalakov <a.s.abalakov@ucre.ru>
2018-01-31 10:09:02 +0500

    Использование параметра TAB_ID для идентификации блока шаблона и обработки ajax вызова

--- bitrix/components/ucre/gallery/templates/.default/edit.php
+++ bitrix/components/ucre/gallery/templates/.default/edit.php
@@ -1,3 +1,9 @@
 <?php
 if(!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED!==true) die();
 ?>
+<div id="<?=$arResult['TAB_ID']?>">
+  <center><input id="save_<?=$arResult['TAB_ID']?>" type="button" value="Сохранить">&nsbp;<input id="cancel_<?=$arResult['TAB_ID']?>" type="button" value="Отменить"></center>
+</div>
+<script type="text/javascript">
+  
+</script>
