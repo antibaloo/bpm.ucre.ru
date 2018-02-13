6ed20d0064ac064b52f7f5c0400cf75d1b8162c1
Andrew Abalakov <a.s.abalakov@ucre.ru>
2018-02-13 10:03:53 +0500

    Сделки --> Заявки

---
+++ bitrix/modules/crm/lang/ru/lib/widget/data/dealidle.php
@@ -0,0 +1,5 @@
+<?
+$MESS["CRM_DEAL_IDLE_PRESET_OVERALL_COUNT"] = "Количество забытых заявок";
+$MESS["CRM_DEAL_IDLE_PRESET_OVERALL_COUNT_SHORT"] = "Количество заявок";
+$MESS["CRM_DEAL_IDLE_CATEGORY"] = "Забытые заявки";
+?>
---
+++ bitrix/modules/crm/lang/ru/lib/widget/data/dealinwork.php
@@ -0,0 +1,5 @@
+<?php
+$MESS ['CRM_DEAL_IN_WORK_PRESET_OVERALL_COUNT'] = 'Количество активных заявок';
+$MESS ['CRM_DEAL_IN_WORK_PRESET_OVERALL_COUNT_SHORT'] = 'Количество заявок';
+$MESS ['CRM_DEAL_IN_WORK_CATEGORY'] = 'Активные заявки';
+?>
