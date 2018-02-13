af8d666c25ee674fe450934193989e2be04904bd
Andrew Abalakov <a.s.abalakov@ucre.ru>
2018-02-13 09:54:56 +0500

    Сделки --> Заявки

---
+++ bitrix/modules/crm/lang/ru/kanban.php
@@ -0,0 +1,22 @@
+<?
+$MESS['CRM_KANBAN_NOTIFY_TITLE'] = 'Недостаточно прав';
+$MESS['CRM_KANBAN_NOTIFY_HEADER'] = 'Недостаточно прав на изменение стадий';
+$MESS['CRM_KANBAN_NOTIFY_TEXT'] = 'Создать новую стадию может администратор вашего Битрикс24. Отправьте уведомление или обратитесь лично и ваши коллеги добавят нужные стадии.';
+$MESS['CRM_KANBAN_NOTIFY_BUTTON'] = 'Отправить уведомление';
+$MESS['CRM_KANBAN_ACTIVITY_MY'] = 'Дела';
+$MESS['CRM_KANBAN_ACTIVITY_PLAN'] = 'План';
+$MESS['CRM_KANBAN_ACTIVITY_PLAN_CALL'] = 'Звонок';
+$MESS['CRM_KANBAN_ACTIVITY_PLAN_MEETING'] = 'Встреча';
+$MESS['CRM_KANBAN_ACTIVITY_PLAN_VISIT'] = 'Визит';
+$MESS['CRM_KANBAN_ACTIVITY_PLAN_TASK'] = 'Задача';
+$MESS['CRM_KANBAN_ACTIVITY_TO_PLAN'] = 'Запланировать';
+$MESS['CRM_KANBAN_ACTIVITY_MORE'] = 'Еще';
+$MESS['CRM_KANBAN_ACTIVITY_LETSGO_LEAD'] = 'Запланируйте дела, чтобы продвинуть лид вперед по воронке.';
+$MESS['CRM_KANBAN_ACTIVITY_LETSGO_DEAL'] = 'Запланируйте дела, чтобы продвинуть заявку вперед по воронке.';
+$MESS['CRM_KANBAN_ACTIVITY_CHANGE_LEAD'] = 'У вас нет запланированных дел. Измените статус лида, <span class="crm-kanban-item-activity-link">запланируйте</span> дело или поставьте ожидание.';
+$MESS['CRM_KANBAN_ACTIVITY_CHANGE_DEAL'] = 'У вас нет запланированных дел. Передвиньте стадию заявки, <span class="crm-kanban-item-activity-link">запланируйте</span> дело или поставьте ожидание.';
+$MESS['CRM_KANBAN_NO_EMAIL'] = 'E-mail отсутствует';
+$MESS['CRM_KANBAN_NO_PHONE'] = 'Телефон отсутствует';
+$MESS['CRM_KANBAN_NO_IM'] = 'Общение в чате невозможно';
+$MESS['MAIN_KANBAN_COLUMN_TITLE_PLACEHOLDER'] = 'Название';
+$MESS['MAIN_KANBAN_DROPZONE_CANCEL'] = 'Отменить';
