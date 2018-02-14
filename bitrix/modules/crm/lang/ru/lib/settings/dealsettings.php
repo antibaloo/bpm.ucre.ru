4b5a3c023555a82e79babbb6fd52f8190aaac3b0
Andrew Abalakov <a.s.abalakov@ucre.ru>
2018-02-14 11:20:16 +0500

    Сделки --> Заявки

---
+++ bitrix/modules/crm/lang/ru/lib/settings/dealsettings.php
@@ -0,0 +1,5 @@
+<?
+$MESS["CRM_DEAL_SETTINGS_VIEW_WIDGET"] = "Аналитика по заявкам";
+$MESS["CRM_DEAL_SETTINGS_VIEW_LIST"] = "Обычный список заявок";
+$MESS["CRM_DEAL_SETTINGS_VIEW_KANBAN"] = "Канбан заявок";
+?>
---
+++ bitrix/modules/crm/lang/ru/lib/timeline/dealcontroller.php
@@ -0,0 +1,7 @@
+<?php
+$MESS["CRM_DEAL_MODIFICATION_STAGE"] = "Стадия изменена";
+$MESS["CRM_DEAL_CREATION"] = "Создана заявка";
+$MESS["CRM_DEAL_BASE_CAPTION_LEAD"] = "На основании лида";
+$MESS["CRM_DEAL_BASE_CAPTION_DEAL_RECURRING"] = "На основании шаблона";
+$MESS["CRM_DEAL_CREATION_BASED_ON"] = "Создано на основании";
+?>
