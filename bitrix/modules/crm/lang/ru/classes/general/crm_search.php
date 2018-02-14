d884b85f4569ab801ec58eb468032aa595eb3592
Andrew Abalakov <a.s.abalakov@ucre.ru>
2018-02-14 13:05:05 +0500

    Сделки --> Заявки

---
+++ bitrix/modules/crm/lang/ru/classes/general/crm_search.php
@@ -0,0 +1,10 @@
+<?
+$MESS["CRM_LEAD"] = "Лид";
+$MESS["CRM_CONTACT"] = "Контакт";
+$MESS["CRM_COMPANY"] = "Компания";
+$MESS["CRM_DEAL"] = "Заявка";
+$MESS["CRM_INVOICE"] = "Счёт";
+$MESS["CRM_QUOTE"] = "Предложение";
+$MESS["CRM_EMAILS"] = "E-mail";
+$MESS["CRM_PHONES"] = "Телефоны";
+?>
