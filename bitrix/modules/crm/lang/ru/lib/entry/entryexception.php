000273f48dc95f0a43de902eb268c35c8a8bbfc2
Andrew Abalakov <a.s.abalakov@ucre.ru>
2018-02-14 10:42:07 +0500

    Сделки --> Заявки

---
+++ bitrix/modules/crm/lang/ru/lib/entry/entryexception.php
@@ -0,0 +1,4 @@
+<?
+$MESS["CRM_ENTRY_EX_DEAL_CATEGORY_NOT_FOUND"] = "Направление заявки не найдено";
+$MESS["CRM_ENTRY_EX_DEAL_CATEGORY_DEPENDENCIES_FOUND"] = "Найдены заявки, относящиеся к направлению \"#CAPTION#\"";
+?>
---
+++ bitrix/modules/crm/lang/ru/lib/timeline/dealrecurringcontroller.php
@@ -0,0 +1,10 @@
+<?php
+$MESS["CRM_DEAL_RECURRING_NEXT_EXECUTION"] = "Дата следующего создания";
+$MESS["CRM_DEAL_RECURRING_NEXT_EXECUTION_CHANGED"] = "Дата следующего создания изменена";
+$MESS["CRM_DEAL_RECURRING_NOT_ACTIVE"] = "Постоянная заявка не активна";
+$MESS["CRM_DEAL_RECURRING_ACTIVE"] = "Постоянная заявка активна";
+$MESS["CRM_DEAL_CREATION"] = "Создана заявка";
+$MESS["CRM_DEAL_RECURRING_CREATION"] = "Создан шаблон заявки";
+$MESS["CRM_RECURRING_CREATION_BASED_ON"] = "Создано на основании";
+$MESS["CRM_DEAL_BASE_CAPTION_BASED_ON_DEAL"] = "На основании заявки";
+?>
