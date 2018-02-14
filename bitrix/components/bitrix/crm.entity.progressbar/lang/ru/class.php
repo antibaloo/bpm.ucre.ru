1ab35e82491a0aaa5af4284bd26bd26ec977684f
Andrew Abalakov <a.s.abalakov@ucre.ru>
2018-02-14 15:06:21 +0500

    Сделки --> Заявки

---
+++ bitrix/components/bitrix/crm.entity.progressbar/lang/ru/class.php
@@ -0,0 +1,5 @@
+<?
+$MESS["CRM_ENTITY_ED_PROG_CLOSE"] = "Завершить обработку";
+$MESS["CRM_ENTITY_ED_PROG_DEAL_CLOSE"] = "Завершить заявку";
+$MESS["CRM_ENTITY_ED_PROG_LEAD_CLOSE"] = "Завершить обработку лида";
+?>
