1e8a829a630686d80a9bc296075e5f07a56e9e6a
Andrew Abalakov <a.s.abalakov@ucre.ru>
2018-03-11 15:47:13 +0500

    Лиды создаются, звонки создаютсяи прикрепляются, телефон у ОМ один на двоих, служебные звонки не создают лидов, но фиксируются, пока не т записи разговором

--- beelinePbx/index.php
+++ beelinePbx/index.php
@@ -1,8 +1,16 @@
 <?php
 require ($_SERVER['DOCUMENT_ROOT'] . "/bitrix/modules/main/include/prolog_before.php");
+CModule::IncludeModule('pull');
+CModule::IncludeModule('voximplant');
+CModule::IncludeModule("im");
+CModule::IncludeModule("timeman");
+CModule::IncludeModule('crm');
+CModule::IncludeModule('search');
 include('../include/beeline/functions.php');
+if ($_SERVER['CONTENT_TYPE'] != 'application/xml; charset=UTF-8') exit;
 $xml = file_get_contents("php://input");
-Bitrix\Main\Diag\Debug::writeToFile(array('DATE' => date("c"),'xml' => $xml, 'json' => xmlToJSON($xml)),"","/beelinePbx/beeline.log");
+//Bitrix\Main\Diag\Debug::writeToFile(array('DATE' => date("c"),'$_SERVER' => $_SERVER, '$_POST' => $_POST), "","/beelinePbx/beeline.log");
+//Bitrix\Main\Diag\Debug::writeToFile(array('DATE' => date("c"),'xml' => $xml), "","/beelinePbx/beeline.log");
 $dom = new DOMDocument;
 $dom->loadXML($xml);
 
@@ -13,7 +21,7 @@ if ($dom->getElementsByTagName("eventData")->item(0)->attributes->getNamedItem("
 $DB->PrepareFields("b_beelinepbx_mess");
 $lenRedirect = $dom->getElementsByTagName("redirections")->length;
 $lenAddress = $dom->getElementsByTagName("address")->length;
-$arFields = array(
+$beelineCall = array(
   "b_time"                  => $DB->GetNowFunction(),
   "eventID"                 => "'".trim($dom->getElementsByTagName("eventID")->item(0)->nodeValue)."'",
   "eventType"               => "'".trim($dom->getElementsByTagName("eventData")->item(0)->attributes->getNamedItem("type")->nodeValue)."'",
@@ -54,10 +62,200 @@ $arFields = array(
 );
 $DB->StartTransaction();
 
-$ID = $DB->Insert("b_beelinepbx_mess", $arFields, $err_mess.__LINE__);
+$ID = $DB->Insert("b_beelinepbx_mess", $beelineCall, $err_mess.__LINE__);
 $ID = intval($ID);
 
 if (strlen($strError)<=0) $DB->Commit();
 else $DB->Rollback();
+
+
+$callId = str_replace("'","",$beelineCall['callId']);
+$targetId = str_replace("'","",$beelineCall['targetId']);
+$remotePartyCallType = str_replace("'","",$beelineCall['remotePartyCallType']);
+$remotePartyUserId = str_replace("'","",$beelineCall['remotePartyUserId']);
+$remotePartyAddress = str_replace("'","",$beelineCall['remotePartyAddress']);
+$eventType = str_replace("'","",$beelineCall['eventType']);
+$source = str_replace("'","",$beelineCall['source']);
+$internalReleaseCause = str_replace("'","",$beelineCall['internalReleaseCause']);
+$startTime = str_replace("'","",$beelineCall['startTime']);
+$answerTime = str_replace("'","",$beelineCall['answerTime']);
+$releaseTime = str_replace("'","",$beelineCall['releaseTime']);
+$addressOfRecord = str_replace("'","",$beelineCall['addressOfRecord']);
+
+
+
+
+//Разбор сообщения
+
+$assignedById = getUserByTargetId($targetId); //Определяем ответственного
+
+if ($remotePartyCallType == 'Group'){
+  $phone_res = array(
+    'FOUND'    => 'Y',
+    'EMLOYEE'  => array('ID' =>  getUserByTargetId($remotePartyUserId))
+  );
+}else{
+  $phone_res = findByPhoneNumber(substr($remotePartyAddress,5));
+}
+
+
+
+switch ($eventType){
+  case "xsi:CallReceivedEvent":
+    $callDirection = 2;
+    $callDirectionText = 'входящему звонку с номера ';
+    break;
+  case "xsi:CallOriginatedEvent":
+    $callDirection = 1;
+    $callDirectionText = 'исходящему звонку на номер ';
+    break;
+}
+switch (substr($source,5)){
+  case '79325360157':
+    $SOURCE_ID = '79325360157';
+    break;
+  case '79228090357':
+    $SOURCE_ID = '79228090357';
+    break;
+  case '79325360657':
+    $SOURCE_ID = '79325360657';
+    break;
+  case '79877959090':
+    $SOURCE_ID = '79877959090';
+    break;
+  case '532675700':
+  case '79619475700':
+    $SOURCE_ID = '79619475700';
+    break;
+  case '79878473434':
+    $SOURCE_ID = '79878473434';
+    break;
+  case '79878479292':
+    $SOURCE_ID = '79878479292';
+    break;
+  default:
+    default:
+      if ($eventType =="xsi:CallOriginatedEvent"){
+        $SOURCE_ID = "3";
+      }else{
+        $SOURCE_ID = "CALL";
+      }
+      break;
+}
+
+if ($phone_res['FOUND'] == 'N'){//Если не найдена сущность, то ее надо создать
+  $entityType = 'LEAD';
+  $oLead = new CCrmLead;
+  $arFields = array(
+    "TITLE" => "Название лида",
+    "NAME" => "Переименовать",
+    "COMMENTS" => "Лид по ".$callDirectionText."+".substr($remotePartyAddress,5,1)."(".substr($remotePartyAddress,6,3).")".substr($remotePartyAddress,9,3)."-".substr($remotePartyAddress,12,2)."-".substr($remotePartyAddress,14)." на ВАТС Билайн",
+    "SOURCE_ID" => $SOURCE_ID,
+    "OPPORTUNITY" =>0,
+    "CURRENCY_ID" => "RUB",
+    "OPPORTUNITY_ACCOUNT" => 0,
+    "ACCOUNT_CURRENCY_ID" => "RUB",
+    "LAST_NAME" => "",
+    "SECOND_NAME" => "",
+    "COMPANY_TITLE" => "",
+    "POST" => "",
+    "SOURCE_DESCRIPTION" =>"Создан модулем сопряжения ВАТС Билайн",
+    "STATUS_ID" => "NEW",
+    "UF_CRM_1486022615" => 1317,
+    "ASSIGNED_BY_ID" => $assignedById,
+    "FM" => array(
+      "PHONE" => array(
+        "n0" => array(
+          "VALUE" => "+".substr($remotePartyAddress,5,1)."(".substr($remotePartyAddress,6,3).")".substr($remotePartyAddress,9,3)."-".substr($remotePartyAddress,12,2)."-".substr($remotePartyAddress,14),
+          "VALUE_TYPE" => "OTHER"
+        )
+      )
+    ),
+  );
+  $entityId = $oLead->Add($arFields, true, array('CURRENT_USER' => 24));
+}else{//А если найдена, запоминаем тип и идентификатор
+  if ($phone_res['LEAD']['ID'] > 0) {
+    $entityType = 'LEAD';
+    $entityId = $phone_res['LEAD']['ID'];
+  }elseif ($phone_res['CONTACT']['ID'] > 0){
+    $entityType = 'CONTACT';
+    $entityId = $phone_res['CONTACT']['ID'];
+  }elseif ($phone_res['COMPANY']['ID'] > 0){
+    $entityType = 'COMPANY';
+    $entityId = $phone_res['COMPANY']['ID'];
+  }
+}
+
+//Bitrix\Main\Diag\Debug::writeToFile(array('DATE' => date("c"),'eventType' => $eventType, 'targetId' => $targetId, 'remotePartyAddress' =>$remotePartyAddress, 'remotePartyCallType' => $remotePartyCallType,'assignedById' => $assignedById,'phone_res' => $phone_res, 'entityType' => $entityType, 'entityId' => $entityId), "","/beelinePbx/beeline.log");
+
+$restHelper = new \Bitrix\Voximplant\Rest\Helper;
+
+if (!getBitrixByBeelinepbx($callId)){//Если не найдено соответствие, то регистрируем звонок
+  if ($remotePartyCallType == 'Group'){
+    $callParams = array(
+      'USER_ID' =>$assignedById,
+      'PHONE_NUMBER'   => substr($remotePartyAddress,4),
+      'CRM_CREATE' => false,
+      'TYPE' => $callDirection,
+      'SHOW' => false,
+    );
+  }else{
+    $callParams = array(
+      'USER_ID' =>$assignedById,
+      'PHONE_NUMBER'   => substr($remotePartyAddress,5),
+      'CRM_ENTITY_TYPE' => $entityType,
+      'CRM_ENTITY_ID' => $entityId,
+      'CRM_CREATE' => true,
+      'TYPE' => $callDirection,
+      'CRM_SOURCE' => $SOURCE_ID,
+      'SHOW' => true,
+    );
+  }
+  $registerResults = $restHelper-> registerExternalCall($callParams);
+  $registerData = $registerResults->getData();
+  if($registerData['CALL_ID']) linkCallIds($callId,$registerData['CALL_ID']);//Запоминаем соответствие callid
+}
+if ($eventType == 'xsi:CallReleasedEvent') {
+  switch ($internalReleaseCause){
+    case "Busy":
+      $statusCode = 486;
+      break;
+    case "Temporarily Unavailable":
+      $statusCode = 480;
+      break;
+    case "Forbidden":
+      $statusCode = 403;
+      break;
+    case "":
+      if ($answerTime == "") $statusCode = 304;
+      else $statusCode = 200;
+      break;
+  }
+  if ($answerTime == "") {
+    $duration = 0;
+    $waiting = ($releaseTime - $startTime)/1000;
+  }else{
+    $duration = ($releaseTime - $answerTime)/1000;
+    $waiting = ($answerTime - $startTime)/1000;
+  }
+  
+  $callParams = array(
+    'CALL_ID' => getBitrixByBeelinepbx($callId),
+    'DURATION' => $duration,
+    'USER_ID' => $assignedById,
+    'ADD_TO_CHAT' => true,
+    'RECORD_URL' => '',
+    'STATUS_CODE' => $statusCode,
+  );
+  $finishResults = $restHelper->finishExternalCall($callParams);
+  $finishData = $finishResults->getData();
+  if ($statusCode != 200){
+    $oActivity = new CCrmActivity;
+    $acFields = array('COMPLETED' => 'N');
+    $oActivity->Update($finishData['CRM_ACTIVITY_ID'], $acFields,true, true, array('CURRENT_USER' => 24));
+  }
+  
+  
+}
 
 ?>
