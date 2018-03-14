390c55a084fa6b9eb4a814eecf53c328f5368752
Andrew Abalakov <a.s.abalakov@ucre.ru>
2018-03-14 09:14:03 +0500

    отложенный обработчик таблицы записей телефонных разговоров Билай ВАТС

---
+++ pub/beelineRecordUpdate.php
@@ -0,0 +1,34 @@
+<?
+require ($_SERVER['DOCUMENT_ROOT'] . "/bitrix/modules/main/include/prolog_before.php");
+CModule::IncludeModule('voximplant');
+$rsConfig = $DB->Query("select * from b_beelinepbx_config");
+while ($arConfig = $rsConfig->Fetch()){$config[$arConfig['param']] = $arConfig['value'];}
+
+$rsRecords = $DB->Query("select * from b_beeline_record_log where recordState = 'RecordNotFound' order by id DESC");
+while ($arRecord = $rsRecords->Fetch()){
+  $client = curl_init('https://cloudpbx.beeline.ru/apis/portal/v2/records/'.$arRecord['extTrackingId'].'/'.$arRecord['targetId'].'/download');
+  curl_setopt($client, CURLOPT_CUSTOMREQUEST, "GET");
+  curl_setopt($client, CURLOPT_HTTPHEADER, array('X-MPBX-API-AUTH-TOKEN: '.$config['token']));
+  curl_setopt($client, CURLOPT_RETURNTRANSFER, 1);
+  $responce = curl_exec($client);
+  $json = json_decode($responce, true);
+  if (isset($json['errorCode'])){
+    echo $arRecord['logTime']." - ".$arRecord['extTrackingId'].": Запись не найдена ". date("H:i:s d-m-Y")."<br>";
+  }else{
+    
+    
+    file_put_contents('/home/bitrix/www_bpm/callRecordTmp/'.$arRecord['bitrixCallId'].'.wav', $responce);
+    exec('lame -V 1 /home/bitrix/www_bpm/callRecordTmp/'.$arRecord['bitrixCallId'].'.wav /home/bitrix/www_bpm/callRecordTmp/'.$arRecord['bitrixCallId'].'.mp3');
+    unlink('/home/bitrix/www_bpm/callRecordTmp/'.$arRecord['bitrixCallId'].'.wav');
+    $restHelper = new \Bitrix\Voximplant\Rest\Helper;
+    $result = $restHelper->attachRecordWithUrl($arRecord['bitrixCallId'],'https://bpm.ucre.ru/callRecordTmp/'.$arRecord['bitrixCallId'].'.mp3');
+    unlink('/home/bitrix/www_bpm/callRecordTmp/'.$arRecord['bitrixCallId'].'.mp3');
+    
+    
+    echo $arRecord['logTime']." - ".$arRecord['extTrackingId']." Запись найдена ". date("H:i:s d-m-Y")."<br>";
+    
+    $DB->Query("update b_beeline_record_log set recordState='Uploaded', uploadTime = ".$DB->GetNowFunction()."  where id = '".$arRecord['id']."'");
+  }
+}
+
+?>
