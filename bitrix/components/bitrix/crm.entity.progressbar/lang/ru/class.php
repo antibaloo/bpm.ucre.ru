9398bf320d3eb0bad779b95af5244c41c0c4f063
Andrew Abalakov <a.s.abalakov@ucre.ru>
2018-03-19 10:01:12 +0500

    обновление

--- bitrix/components/bitrix/crm.entity.progressbar/lang/ru/class.php
+++ bitrix/components/bitrix/crm.entity.progressbar/lang/ru/class.php
@@ -1,5 +1,6 @@
 <?
 $MESS["CRM_ENTITY_ED_PROG_CLOSE"] = "Завершить обработку";
-$MESS["CRM_ENTITY_ED_PROG_DEAL_CLOSE"] = "Завершить заявку";
+$MESS["CRM_ENTITY_ED_PROG_DEAL_CLOSE"] = "Завершить сделку";
 $MESS["CRM_ENTITY_ED_PROG_LEAD_CLOSE"] = "Завершить обработку лида";
+$MESS["CRM_ENTITY_ED_PROG_QUOTE_CLOSE"] = "Завершить обработку предложения";
 ?>
