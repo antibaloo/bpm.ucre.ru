fcbb4a98ba419b363233bb6fce471e789f6b2318
Andrew Abalakov <a.s.abalakov@ucre.ru>
2018-03-19 09:55:48 +0500

    обновление

--- bitrix/components/bitrix/crm.entity.details/templates/.default/lang/ru/template.php
+++ bitrix/components/bitrix/crm.entity.details/templates/.default/lang/ru/template.php
@@ -1,21 +1,30 @@
 <?
 $MESS['CRM_ENT_DETAIL_MAIN_TAB'] = "Общие";
 $MESS['CRM_ENT_DETAIL_COPY_LEAD_URL'] = "Скопировать ссылку на лид в буфер обмена";
-$MESS['CRM_ENT_DETAIL_COPY_DEAL_URL'] = "Скопировать ссылку на заявку в буфер обмена";
-$MESS['CRM_ENT_DETAIL_COPY_DEAL_RECURRING_URL'] = "Скопировать ссылку на постоянную заявку в буфер обмена";
+$MESS['CRM_ENT_DETAIL_COPY_DEAL_URL'] = "Скопировать ссылку на сделку в буфер обмена";
+$MESS['CRM_ENT_DETAIL_COPY_DEAL_RECURRING_URL'] = "Скопировать ссылку на постоянную сделку в буфер обмена";
 $MESS['CRM_ENT_DETAIL_COPY_CONTACT_URL'] = "Скопировать ссылку на контакт в буфер обмена";
 $MESS['CRM_ENT_DETAIL_COPY_COMPANY_URL'] = "Скопировать ссылку на компанию в буфер обмена";
 $MESS['CRM_ENT_DETAIL_LEAD_URL_COPIED'] = "Ссылка на лид скопирована в буфер обмена";
-$MESS['CRM_ENT_DETAIL_DEAL_URL_COPIED'] = "Ссылка на заявку скопирована в буфер обмена";
-$MESS['CRM_ENT_DETAIL_DEAL_RECURRING_URL_COPIED'] = "Ссылка на постоянную заявку скопирована в буфер обмена";
+$MESS['CRM_ENT_DETAIL_DEAL_URL_COPIED'] = "Ссылка на сделку скопирована в буфер обмена";
+$MESS['CRM_ENT_DETAIL_DEAL_RECURRING_URL_COPIED'] = "Ссылка на постоянную сделку скопирована в буфер обмена";
 $MESS['CRM_ENT_DETAIL_CONTACT_URL_COPIED'] = "Ссылка на контакт скопирована в буфер обмена";
 $MESS['CRM_ENT_DETAIL_COMPANY_URL_COPIED'] = "Ссылка на компанию скопирована в буфер обмена";
-$MESS["CRM_ENT_DETAIL_DEAL_DELETE_DIALOG_TITLE"] = "Удаление заявки";
+$MESS["CRM_ENT_DETAIL_DEAL_DELETE_DIALOG_TITLE"] = "Удаление сделки";
 $MESS["CRM_ENT_DETAIL_LEAD_DELETE_DIALOG_TITLE"] = "Удаление лида";
 $MESS["CRM_ENT_DETAIL_CONTACT_DELETE_DIALOG_TITLE"] = "Удаление контакта";
 $MESS["CRM_ENT_DETAIL_COMPANY_DELETE_DIALOG_TITLE"] = "Удаление компании";
-$MESS["CRM_ENT_DETAIL_DEAL_DELETE_DIALOG_MESSAGE"] = "Вы уверены, что хотите удалить эту заявку?";
+$MESS["CRM_ENT_DETAIL_DEAL_DELETE_DIALOG_MESSAGE"] = "Вы уверены, что хотите удалить эту сделку?";
 $MESS["CRM_ENT_DETAIL_LEAD_DELETE_DIALOG_MESSAGE"] = "Вы уверены, что хотите удалить этот лид?";
 $MESS["CRM_ENT_DETAIL_CONTACT_DELETE_DIALOG_MESSAGE"] = "Вы уверены, что хотите удалить этот контакт?";
 $MESS["CRM_ENT_DETAIL_COMPANY_DELETE_DIALOG_MESSAGE"] = "Вы уверены, что хотите удалить эту компанию?";
 $MESS["CRM_ENT_DETAIL_REST_BUTTON"] = "Приложения";
+$MESS['CRM_ENT_DETAIL_COPY_QUOTE_URL'] = "Скопировать ссылку на предложение в буфер обмена";
+$MESS['CRM_ENT_DETAIL_QUOTE_URL_COPIED'] = "Ссылка на предложение скопирована в буфер обмена";
+$MESS["CRM_ENT_DETAIL_QUOTE_DELETE_DIALOG_TITLE"] = "Удаление предложения";
+$MESS["CRM_ENT_DETAIL_QUOTE_DELETE_DIALOG_MESSAGE"] = "Вы уверены, что хотите удалить это предложение?";
+$MESS["CRM_ENT_DETAIL_LEAD_EXCLUDE_DIALOG_TITLE"] = "Добавление лида в исключения";
+$MESS["CRM_ENT_DETAIL_LEAD_EXCLUDE_DIALOG_MESSAGE"] = "Вы уверены, что хотите добавить этот лид в список исключений?";
+$MESS["CRM_ENT_DETAIL_DEAL_EXCLUDE_DIALOG_TITLE"] = "Добавление сделки в исключения";
+$MESS["CRM_ENT_DETAIL_DEAL_EXCLUDE_DIALOG_MESSAGE"] = "Вы уверены, что хотите добавить эту сделку в список исключений?";
+
