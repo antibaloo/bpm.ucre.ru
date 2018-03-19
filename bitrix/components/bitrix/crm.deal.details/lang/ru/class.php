681ec7d57b7a8c2e9de81f8f218403cbd25a66f9
Andrew Abalakov <a.s.abalakov@ucre.ru>
2018-03-19 09:42:32 +0500

    Правильный перевод

--- bitrix/components/bitrix/crm.deal.details/lang/ru/class.php
+++ bitrix/components/bitrix/crm.deal.details/lang/ru/class.php
@@ -2,7 +2,7 @@
 $MESS["CRM_MODULE_NOT_INSTALLED"] = "Модуль CRM не установлен.";
 $MESS["CRM_PERMISSION_DENIED"] = "Доступ запрещен";
 $MESS["CRM_DEAL_FIELD_TITLE"] = "Название";
-$MESS["CRM_DEAL_FIELD_TYPE_ID"] = "Тип сделки";
+$MESS["CRM_DEAL_FIELD_TYPE_ID"] = "Тип заявки";
 $MESS["CRM_DEAL_FIELD_STAGE_ID"] = "Стадия";
 $MESS["CRM_DEAL_FIELD_OPPORTUNITY_WITH_CURRENCY"] = "Сумма и валюта";
 $MESS["CRM_DEAL_FIELD_CLOSEDATE"] = "Дата завершения";
@@ -13,13 +13,13 @@ $MESS["CRM_DEAL_FIELD_COMMENTS"] = "Комментарий";
 $MESS["CRM_DEAL_FIELD_CLIENT"] = "Клиент";
 $MESS["CRM_DEAL_FIELD_ASSIGNED_BY_ID"] = "Ответственный";
 $MESS["CRM_DEAL_FIELD_RECURRING"] = "Повторяемость";
-$MESS["CRM_DEAL_FIELD_CATEGORY_RECURRING"] = "Направление сделки";
+$MESS["CRM_DEAL_FIELD_CATEGORY_RECURRING"] = "Направление заявки";
 $MESS["CRM_DEAL_FIELD_OPPORTUNITY"] = "Сумма";
 $MESS["CRM_DEAL_FIELD_CURRENCY_ID"] = "Валюта";
 $MESS["CRM_DEAL_FIELD_PRODUCTS"] = "Товары";
-$MESS["CRM_DEAL_SECTION_MAIN"] = "О сделке";
+$MESS["CRM_DEAL_SECTION_MAIN"] = "О заявке";
 $MESS["CRM_DEAL_SECTION_ADDITIONAL"] = "Дополнительно";
-$MESS["CRM_DEAL_SECTION_RECURRING"] = "Регулярная сделка";
+$MESS["CRM_DEAL_SECTION_RECURRING"] = "Регулярная заявка";
 $MESS["CRM_DEAL_SECTION_PRODUCTS"] = "Товары";
 $MESS["CRM_DEAL_TAB_PRODUCTS"] = "Товары";
 $MESS["CRM_DEAL_TAB_INVOICES"] = "Счета";
@@ -30,10 +30,10 @@ $MESS["CRM_DEAL_TAB_BIZPROC"] = "Бизнес-процессы";
 $MESS["CRM_DEAL_TAB_TREE"] = "Связи";
 $MESS["CRM_DEAL_FIELD_RECURRING_NOTHING_SELECTED"] = "Ничего не выбрано";
 $MESS["CRM_DEAL_FIELD_RECURRING_RESTRICTED"] = "Ограничено правами";
-$MESS["CRM_DEAL_FIELD_RECURRING_CREATED_FROM_CURRENT"] = "На основании создан шаблон постоянной сделки №#RECURRING_ID#";
-$MESS["CRM_DEAL_FIELD_RECURRING_CREATED_MANY_FROM_CURRENT"] = "На основании созданы шаблоны постоянных сделок: #RECURRING_LIST#";
+$MESS["CRM_DEAL_FIELD_RECURRING_CREATED_FROM_CURRENT"] = "На основании создан шаблон постоянной заявки №#RECURRING_ID#";
+$MESS["CRM_DEAL_FIELD_RECURRING_CREATED_MANY_FROM_CURRENT"] = "На основании созданы шаблоны постоянных заявок: #RECURRING_LIST#";
 $MESS["CRM_DEAL_FIELD_RECURRING_DATE_NEXT_EXECUTION"] = "Дата следующего создания #NEXT_DATE#";
-$MESS["CRM_DEAL_FIELD_RECURRING_TITLE"] = "Шаблон сделки: #TITLE#";
+$MESS["CRM_DEAL_FIELD_RECURRING_TITLE"] = "Шаблон заявки: #TITLE#";
 $MESS["CRM_DEAL_FIELD_RECURRING_DAY"] = "дней";
 $MESS["CRM_DEAL_FIELD_RECURRING_WEEK"] = "недель";
 $MESS["CRM_DEAL_FIELD_RECURRING_MONTH"] = "месяцев";
@@ -42,24 +42,24 @@ $MESS["CRM_DEAL_FIELD_RECURRING_EVERYDAY"] = "Ежедневно";
 $MESS["CRM_DEAL_FIELD_RECURRING_EVERY_WEEK"] = "Еженедельно";
 $MESS["CRM_DEAL_FIELD_RECURRING_EVERY_MONTH"] = "Ежемесячно";
 $MESS["CRM_DEAL_FIELD_RECURRING_EVERY_YEAR"] = "Ежегодно";
-$MESS["CRM_DEAL_CREATION_PAGE_TITLE"] = "Создание сделки";
-$MESS["CRM_DEAL_COPY_PAGE_TITLE"] = "Копирование сделки";
+$MESS["CRM_DEAL_CREATION_PAGE_TITLE"] = "Создание заявки";
+$MESS["CRM_DEAL_COPY_PAGE_TITLE"] = "Копирование заявки";
 $MESS["CRM_DEAL_FIELD_NUM_SIGN"] = "№#DEAL_ID#";
-$MESS["CRM_DEAL_FIELD_CONTACT_LEGEND"] = "Контакты, участвующие в сделке";
-$MESS["CRM_DEAL_NOT_FOUND"] = "Сделка не найдена";
+$MESS["CRM_DEAL_FIELD_CONTACT_LEGEND"] = "Контакты, участвующие в заявке";
+$MESS["CRM_DEAL_NOT_FOUND"] = "Заявка не найдена";
 $MESS["CRM_DEAL_FIELD_ID"] = "ID";
-$MESS["CRM_RECURRING_DEAL_B24_BLOCK_TITLE"] = "Регулярные сделки доступны в тарифах \"Команда\" и \"Компания\"";
-$MESS["CRM_RECURRING_DEAL_B24_BLOCK_TEXT"] = "Создавать сделки в Битрикс24 можно автоматически! 
-	Если вы, например, целый год планируете еженедельно заключать договор с каким-то клиентом, то регулярные сделки сэкономят ваше время. 
-	Заведите сделку, укажите ее регулярность и направление. А в указанное время она будет создана автоматически без участия менеджера.
+$MESS["CRM_RECURRING_DEAL_B24_BLOCK_TITLE"] = "Регулярные заявки доступны в тарифах \"Команда\" и \"Компания\"";
+$MESS["CRM_RECURRING_DEAL_B24_BLOCK_TEXT"] = "Создавать заявки в Битрикс24 можно автоматически! 
+	Если вы, например, целый год планируете еженедельно заключать договор с каким-то клиентом, то регулярные заявки сэкономят ваше время. 
+	Заведите заявку, укажите ее регулярность и направление. А в указанное время она будет создана автоматически без участия менеджера.
 		<ul class=\"hide-features-list\">
-			<li class=\"hide-features-list-item\">Автоматическое создание сделок</li>
+			<li class=\"hide-features-list-item\">Автоматическое создание заявок</li>
 			<li class=\"hide-features-list-item\">Повышение эффективности работы менеджеров</li>
-			<li class=\"hide-features-list-item\">Быстрый доступ к списку регулярных сделок <a href=\"#LINK#\" target=\"_blank\" class=\"hide-features-more\">Узнать подробнее</a></li>
+			<li class=\"hide-features-list-item\">Быстрый доступ к списку регулярных заявок <a href=\"#LINK#\" target=\"_blank\" class=\"hide-features-more\">Узнать подробнее</a></li>
 		</ul>		
-		<strong>Регулярные сделки доступны в тарифах \"Команда\" и \"Компания\"</strong>";
-$MESS["CRM_DEAL_QUOTE_LINK"] = "Сделка создана на основании предложения <a target='_blank' href='#URL#'>#TITLE#</a>";
+		<strong>Регулярные заявки доступны в тарифах \"Команда\" и \"Компания\"</strong>";
+$MESS["CRM_DEAL_QUOTE_LINK"] = "Заявка создана на основании предложения <a target='_blank' href='#URL#'>#TITLE#</a>";
 $MESS["CRM_DEAL_FIELD_LOCATION_ID"] = "Местоположение";
 $MESS["CRM_DEAL_FIELD_UTM"] = "UTM-метки";
-$MESS["CRM_DEAL_RETURNING"] = "Повторная сделка";
+$MESS["CRM_DEAL_RETURNING"] = "Повторная заявка";
 ?>
