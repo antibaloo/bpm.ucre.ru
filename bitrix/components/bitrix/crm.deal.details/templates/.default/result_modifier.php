5d1f6b0727b8922d7ecc4727d14d83a276334fa3
Andrew Abalakov <a.s.abalakov@ucre.ru>
2018-03-21 17:32:45 +0500

    Создание всех кастомных вкладок перенесено из компонента ядра в модификатор шаблона для заявок

--- bitrix/components/bitrix/crm.deal.details/templates/.default/result_modifier.php
+++ bitrix/components/bitrix/crm.deal.details/templates/.default/result_modifier.php
@@ -5,11 +5,144 @@ foreach ($arResult['TABS'] as $key=>$tab){
 }
 /*----------------------------------------------------------------*/
 if ($arResult['ENTITY_ID']>0){
+  //Фиктивный вызов без отображения. Вкладка загрузки отображается только для админов, АУП и ответственных
+  if (CCrmDeal::CheckUpdatePermission($arResult['ENTITY_ID'], CCrmPerms::GetUserPermissions($USER->GetID()))){
+    ob_start();
+    $APPLICATION->IncludeComponent(
+      'ucre:gallery.upload',
+      '',
+      array('ENTITY' => $arResult['ENTITY_DATA'],
+            'FIELDS' => array(
+              'Фотографии' => 'UF_CRM_1472038962',
+              'Планировки' => 'UF_CRM_1476517423',
+              'Фото для внутреннего пользования' => 'UF_CRM_1513322128',
+              'Документы по заявке' => 'UF_CRM_1472704376',
+              'Подписанная заявка' => 'UF_CRM_1512462544',
+              'Скан агентского договора' => 'UF_CRM_1512462594',
+            )
+           )
+    );
+    $html = ob_get_contents();
+    ob_end_clean();
+  }	
   if ($arResult['CATEGORY_ID'] == 0 || $arResult['CATEGORY_ID'] == 4){
+    ob_start();
+    /*Общий компонент для отображения данных связанного объекта*/
+    $APPLICATION->IncludeComponent(
+      'ucre:crm.deal.ro',
+      '',
+      array('DEAL_ID' => $arResult['ENTITY_ID'])
+    );
+    /*----------------------------------------------------------*/
+    $html = ob_get_contents();
+    ob_end_clean();
+    $object = array(
+      'id' => 'tab_roObject',
+      'name' => 'Объект',
+      'html' =>$html
+    );
     
+    ob_start();
+    /*Общий компонент для отображения галереи изображений из обозначенных полей*/
+    $APPLICATION->IncludeComponent(
+      'ucre:gallery',
+      '',
+      array('ENTITY' => $arResult['ENTITY_DATA'],
+            'FIELDS' => array(
+              'Фотографии' => 'UF_CRM_1472038962',
+              'Планировки' => 'UF_CRM_1476517423',
+              'Фото для внутреннего пользования' => 'UF_CRM_1513322128',
+            )
+           )
+    );
+    
+    $rsUser = $USER->GetByID($USER->GetID()); $arUser = $rsUser->Fetch();
+    if ($USER->IsAdmin() || $arUser['WORK_DEPARTMENT'] == 'АУП' || $USER->GetID() == $arResult['ENTITY_DATA']['ASSIGNED_BY_ID']){
+      $APPLICATION->IncludeComponent(
+        'ucre:gallery',
+        '',
+        array('ENTITY' => $arResult['ENTITY_DATA'],
+              'FIELDS' => array(
+                'Документы по заявке' => 'UF_CRM_1472704376',
+                'Подписанная заявка' => 'UF_CRM_1512462544',
+                'Скан агентского договора' => 'UF_CRM_1512462594',
+              )
+             )
+      );
+    }
+    /*----------------------------------------------------------*/
+    $html = ob_get_contents();
+    ob_end_clean();
+    
+ 		//Добавление кнопки редактировать по условию
+    $edit = CCrmDeal::CheckUpdatePermission($arResult['ENTITY_ID'], CCrmPerms::GetUserPermissions($USER->GetID()))?'<div class="galleryUploadWrapper"><div></div><div></div><div></div><div id="galleryUpload">Редактировать</div><div></div><div></div><div></div></div>':"";
+    $gallery = array(
+      'id' => 'tabImg',
+      'name' => 'Изображения',
+      'html' =>'<div id="ucreImageDiv">'.$edit.$html.'</div>'
+    );
+    
+    ob_start();
+    /*Общий компонент для отображения лога выгрузки на Авито*/
+    $APPLICATION->IncludeComponent(
+      'ucre:crm.avito.log',
+      '',
+      array(
+        'OBJECT_ID' =>$arResult['ENTITY_DATA']['UF_CRM_1469534140']['VALUE'],
+        'COUNT' => 42
+      )
+    );
+    /*----------------------------------------------------------*/
+    $html = ob_get_contents();
+    ob_end_clean();
+    $avitoLog = array(
+      'id' => 'tab_avitoLog',
+      'name' => 'Лог Авито',
+      'html' =>$html
+    );	
+    array_unshift($arResult['TABS'], $object, $gallery, $avitoLog);
   }
   if ($arResult['CATEGORY_ID'] == 3 || $arResult['CATEGORY_ID'] == 9){
-    
+    ob_start();
+    /*Общий компонент для отображения галереи изображений из обозначенных полей*/
+    $APPLICATION->IncludeComponent(
+      'ucre:gallery',
+      '',
+      array('ENTITY' => $arResult['ENTITY_DATA'],
+            'FIELDS' => array(
+              'Фотографии' => 'UF_CRM_1472038962',
+              'Планировки' => 'UF_CRM_1476517423',
+              'Фото для внутреннего пользования' => 'UF_CRM_1513322128',
+            )
+           )
+    );
+    /*----------------------------------------------------------*/
+    /*Общий компонент для отображения галереи изображений из обозначенных полей*/
+    $rsUser = $USER->GetByID($USER->GetID()); $arUser = $rsUser->Fetch();
+    if ($USER->IsAdmin() || $arUser['WORK_DEPARTMENT'] == 'АУП' || $USER->GetID() == $arResult['ENTITY_DATA']['ASSIGNED_BY_ID']){
+      $APPLICATION->IncludeComponent(
+        'ucre:gallery',
+        '',
+        array('ENTITY' => $arResult['ENTITY_DATA'],
+              'FIELDS' => array(
+                'Документы по заявке' => 'UF_CRM_1472704376',
+                'Подписанная заявка' => 'UF_CRM_1512462544',
+                'Скан агентского договора' => 'UF_CRM_1512462594',
+              )
+             )
+      );
+    }
+    /*----------------------------------------------------------*/
+    $html = ob_get_contents();
+    ob_end_clean();
+    //Добавление кнопки редактировать по условию
+    $edit = CCrmDeal::CheckUpdatePermission($arResult['ENTITY_ID'], CCrmPerms::GetUserPermissions($USER->GetID()))?'<div class="galleryUploadWrapper"><div></div><div></div><div></div><div id="galleryUpload">Редактировать</div><div></div><div></div><div></div></div>':"";
+    $gallery = array(
+      'id' => 'tabImg',
+      'name' => 'Изображения',
+      'html' =>'<div id="ucreImageDiv">'.$edit.$html.'</div>'
+    );
+    array_unshift($arResult['TABS'], $gallery);
   }
   if ($arResult['CATEGORY_ID'] == 2){
     ob_start();
